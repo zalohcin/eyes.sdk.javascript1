3b0a6be6070c12be29178a0b8492fe4be7e655e8
Daniel Puterman <daniel.puterman@applitools.com>
2018-02-10 10:31:24 +0200

    Inline documentation fix.

--- eyes.sdk.core.javascript/src/server/SessionStartInfo.js
+++ eyes.sdk.core.javascript/src/server/SessionStartInfo.js
@@ -132,7 +132,7 @@ class SessionStartInfo {
     }
 
     // noinspection JSUnusedGlobalSymbols
-    /** @return {String} */
+    /** @return {Boolean} */
     getIgnoreBaseline() {
         return this._ignoreBaseline;
     }
