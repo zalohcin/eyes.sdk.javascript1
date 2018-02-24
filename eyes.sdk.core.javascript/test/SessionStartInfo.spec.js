aa0535b90af59936b958c1c70b134b02824919ec
Oleh Astappiev <astappev@gmail.com>
2018-02-24 21:21:00 +0100

    Add checkSingleWindowBase method to EyesBase and related changes

--- eyes.sdk.core.javascript/test/SessionStartInfo.spec.js
+++ eyes.sdk.core.javascript/test/SessionStartInfo.spec.js
@@ -20,11 +20,11 @@ describe('SessionStartInfo', () => {
             "some agent", SessionType.SEQUENTIAL,
             "my app", "1.0.0", "some scenario", batchInfo,
             "some baseline name", "env name", new AppEnvironment(), new ImageMatchSettings(),
-            "some branch name", "some parent branch name", properties
+            "some branch name", "some parent branch name", false, false, properties, false
         );
 
         const actualSerialization = GeneralUtils.toJson(ssi);
-        const expectedSerialization = `{"agentId":"some agent","sessionType":"SEQUENTIAL","appIdOrName":"my app","verId":"1.0.0","scenarioIdOrName":"some scenario","batchInfo":${GeneralUtils.toJson(batchInfo)},"baselineEnvName":"some baseline name","environmentName":"env name","environment":{"inferred":null},"defaultMatchSettings":{"matchLevel":"Strict","ignore":[],"floating":[]},"branchName":"some branch name","parentBranchName":"some parent branch name","properties":[{"name":"property name","value":"property value"},{"name":null,"value":null}]}`;
-        assert.equal(expectedSerialization, actualSerialization, "SessionStartInfo serialization does not match!");
+        const expectedSerialization = `{"agentId":"some agent","sessionType":"SEQUENTIAL","appIdOrName":"my app","verId":"1.0.0","scenarioIdOrName":"some scenario","batchInfo":${GeneralUtils.toJson(batchInfo)},"baselineEnvName":"some baseline name","environmentName":"env name","environment":{"inferred":null},"defaultMatchSettings":{"matchLevel":"Strict","ignore":[],"floating":[]},"branchName":"some branch name","parentBranchName":"some parent branch name","compareWithParentBranch":false,"ignoreBaseline":false,"properties":[{"name":"property name","value":"property value"},{"name":null,"value":null}],"render":false}`;
+        assert.equal(actualSerialization, expectedSerialization, "SessionStartInfo serialization does not match!");
     });
 });
