13a7b99ab253b994ed436ea56dbeacb7e4a46ca4
Oleh Astappiev <astappev@gmail.com>
2018-03-16 15:40:46 +0100

    Rename browser packages

--- eyes.images.browser/index.js
+++ eyes.images.browser/index.js
@@ -6,6 +6,6 @@
  * This file is meant to be used as an input for Browserify for creating a browser version of the SDK, by adding
  * the EyesImages object to the global "window" instance.
  */
-window.EyesImages = require('../eyes.images.javascript/index');
+window.EyesImages = require('@applitools/eyes.images');
 
 console.log("EyesImages loaded into the 'window' object");
