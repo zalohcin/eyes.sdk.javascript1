293c3910065d2ea7d81f94229c7ce2d2efb013d3
Oleh Astappiev <astappev@gmail.com>
2018-07-25 18:35:52 +0200

    Improve setBatch method, add test for it

--- eyes.sdk.core/test/unit/EyesBase.spec.js
+++ eyes.sdk.core/test/unit/EyesBase.spec.js
@@ -1,84 +1,80 @@
 'use strict';
 
-var assert = require('assert');
-var EyesSDK = require('../../index');
-var PromiseFactory = require('eyes.utils').PromiseFactory;
+const assert = require('assert');
 
-var EyesBase = EyesSDK.EyesBase;
+const { EyesBase } = require('../../index');
 
-describe('EyesBase', function () {
+describe('EyesBase', () => {
+  /** @type {EyesBase} */ let eyes;
 
-    /** @type {EyesBase} */ var eyes;
-    before(function () {
-        eyes = new EyesBase(new PromiseFactory(), EyesBase.DEFAULT_EYES_SERVER, false);
-        eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
-        eyes._promiseFactory.setFactoryMethods(function (asyncAction) {
-            return new Promise(asyncAction);
-        }, undefined);
+  before(() => {
+    eyes = new EyesBase();
+  });
+
+  describe('setBatch()', () => {
+    it('should create an default batch', () => {
+      const batch = eyes.getBatch();
+      assert.strictEqual(typeof batch.getId(), 'string');
+      assert.strictEqual(typeof batch.getName(), 'undefined');
+      assert.strictEqual(typeof batch.getStartedAt(), 'object');
     });
 
-    describe('setBatch()', function () {
-        it('should create an default batch', function () {
-            var batch = eyes.getBatch();
-            assert.strictEqual(typeof batch.id, 'string');
-            assert.strictEqual(typeof batch.name, 'undefined');
-            assert.strictEqual(typeof batch.startedAt, 'string');
-        });
+    it('should create batch with name', () => {
+      eyes.setBatch('batch name');
 
-        it('should create batch with name', function () {
-            eyes.setBatch('batch name');
-
-            var batch = eyes.getBatch();
-            assert.strictEqual(typeof batch.id, 'string');
-            assert.strictEqual(batch.name, 'batch name');
-            assert.strictEqual(typeof batch.startedAt, 'string');
-        });
-
-        it('should create batch with name, id', function () {
-            eyes.setBatch('batch name', 'fake batch id');
-
-            var batch = eyes.getBatch();
-            assert.strictEqual(batch.id, 'fake batch id');
-            assert.strictEqual(batch.name, 'batch name');
-            assert.strictEqual(typeof batch.startedAt, 'string');
-        });
-
-        it('should create batch with name, id, time', function () {
-            var time = new Date().toUTCString();
-            eyes.setBatch('batch name2', 'fake batch id2', time);
-
-            var batch = eyes.getBatch();
-            assert.strictEqual(batch.id, 'fake batch id2');
-            assert.strictEqual(batch.name, 'batch name2');
-            assert.strictEqual(batch.startedAt, time);
-        });
-
-        it('should create batch from BatchInfo', function () {
-            var defaultBatch = eyes.getBatch();
-
-            eyes.setBatch('batch name', 'fake batch id');
-
-            var batch = eyes.getBatch();
-            assert.strictEqual(batch.id, 'fake batch id');
-            assert.strictEqual(batch.name, 'batch name');
-            assert.notDeepEqual(batch, defaultBatch);
-            assert.deepEqual(eyes.getBatch(), batch);
-
-            eyes.setBatch(defaultBatch);
-            assert.deepEqual(eyes.getBatch(), defaultBatch);
-        });
-
-        it('should create batch by default using values from env', function () {
-            process.env.APPLITOOLS_BATCH_ID = 'fake id in env';
-            process.env.APPLITOOLS_BATCH_NAME = 'fake batch name in env';
-
-            var batch = eyes.getBatch();
-            assert.strictEqual(batch.id, 'fake id in env');
-            assert.strictEqual(batch.name, 'fake batch name in env');
-        });
-
-        afterEach(function () {
-            eyes._batch = undefined;
-        })
+      const batch = eyes.getBatch();
+      assert.strictEqual(typeof batch.getId(), 'string');
+      assert.strictEqual(batch.getName(), 'batch name');
+      assert.strictEqual(typeof batch.getStartedAt(), 'object');
     });
+
+    it('should create batch with name, id', () => {
+      eyes.setBatch('batch name', 'fake batch id');
+
+      const batch = eyes.getBatch();
+      assert.strictEqual(batch.getId(), 'fake batch id');
+      assert.strictEqual(batch.getName(), 'batch name');
+      assert.strictEqual(typeof batch.getStartedAt(), 'object');
+    });
+
+    it('should create batch with name, id, time', () => {
+      const time = new Date();
+      time.setMilliseconds(0);
+
+      eyes.setBatch('batch name2', 'fake batch id2', time);
+
+      const batch = eyes.getBatch();
+      assert.strictEqual(batch.getId(), 'fake batch id2');
+      assert.strictEqual(batch.getName(), 'batch name2');
+      assert.equal(batch.getStartedAt().getTime(), time.getTime());
+    });
+
+    it('should create batch from BatchInfo', () => {
+      const defaultBatch = eyes.getBatch();
+
+      eyes.setBatch('batch name', 'fake batch id');
+
+      const batch = eyes.getBatch();
+      assert.strictEqual(batch.getId(), 'fake batch id');
+      assert.strictEqual(batch.getName(), 'batch name');
+      assert.notDeepEqual(batch, defaultBatch);
+      assert.deepEqual(eyes.getBatch(), batch);
+
+      eyes.setBatch(defaultBatch);
+      assert.deepEqual(eyes.getBatch(), defaultBatch);
+    });
+
+    it('should create batch by default using values from env', () => {
+      process.env.APPLITOOLS_BATCH_ID = 'fake id in env';
+      process.env.APPLITOOLS_BATCH_NAME = 'fake batch name in env';
+
+      const batch = eyes.getBatch();
+      assert.strictEqual(batch.getId(), 'fake id in env');
+      assert.strictEqual(batch.getName(), 'fake batch name in env');
+    });
+
+    afterEach(() => {
+      eyes._batch = undefined;
+    });
+  });
 });
