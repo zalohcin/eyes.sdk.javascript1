5452dc08e200ea4e953a2f18ff1cc5ff23073bfd
Amit Zur <sendwithchibo@gmail.com>
2018-08-22 17:49:25 +0300

    add retry to fetchResrouces

--- src/sdk/fetchResource.js
+++ src/sdk/fetchResource.js
@@ -1,15 +1,20 @@
 'use strict';
 const fetch = require('node-fetch');
+const {retryFetch} = require('@applitools/http-commons');
 
 function makeFetchResource(logger) {
   return url => {
     logger.log(`fetching ${url}`);
-    return fetch(url).then(resp =>
-      resp.buffer().then(buff => ({
-        url,
-        type: resp.headers.get('Content-Type'),
-        value: buff,
-      })),
+    return retryFetch(
+      _retry =>
+        fetch(url).then(resp =>
+          resp.buffer().then(buff => ({
+            url,
+            type: resp.headers.get('Content-Type'),
+            value: buff,
+          })),
+        ),
+      {retries: 5},
     );
   };
 }
