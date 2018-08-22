3d064b4fb8d5b6a434771ca99a9b54673fb25892
Amit Zur <sendwithchibo@gmail.com>
2018-08-22 17:49:14 +0300

    make domNodesToCdt serializable

--- src/browser-util/domNodesToCdt.js
+++ src/browser-util/domNodesToCdt.js
@@ -1,13 +1,14 @@
 /* eslint-disable no-use-before-define */
 'use strict';
-const NODE_TYPES = {
-  ELEMENT: 1,
-  TEXT: 3,
-  DOCUMENT: 9,
-  DOCUMENT_TYPE: 10,
-};
 
 function domNodesToCdt(docNode) {
+  const NODE_TYPES = {
+    ELEMENT: 1,
+    TEXT: 3,
+    DOCUMENT: 9,
+    DOCUMENT_TYPE: 10,
+  };
+
   const domNodes = [
     {
       nodeType: NODE_TYPES.DOCUMENT,
@@ -15,68 +16,73 @@ function domNodesToCdt(docNode) {
   ];
   domNodes[0].childNodeIndexes = childrenFactory(domNodes, docNode.childNodes);
   return domNodes;
-}
 
-const childrenFactory = (domNodes, elementNodes) => {
-  if (!elementNodes || elementNodes.length === 0) return null;
+  function childrenFactory(domNodes, elementNodes) {
+    if (!elementNodes || elementNodes.length === 0) return null;
 
-  const childIndexes = [];
-  elementNodes.forEach(elementNode => {
-    const index = elementNodeFactory(domNodes, elementNode);
-    if (index !== null) {
-      childIndexes.push(index);
-    }
-  });
+    const childIndexes = [];
+    elementNodes.forEach(elementNode => {
+      const index = elementNodeFactory(domNodes, elementNode);
+      if (index !== null) {
+        childIndexes.push(index);
+      }
+    });
 
-  return childIndexes;
-};
+    return childIndexes;
+  }
 
-const elementNodeFactory = (domNodes, elementNode) => {
-  let node;
-  const {nodeType} = elementNode;
-  if (nodeType === NODE_TYPES.ELEMENT) {
-    if (elementNode.nodeName !== 'SCRIPT') {
+  function elementNodeFactory(domNodes, elementNode) {
+    let node;
+    const {nodeType} = elementNode;
+    if (nodeType === NODE_TYPES.ELEMENT) {
+      if (elementNode.nodeName !== 'SCRIPT') {
+        node = {
+          nodeType: NODE_TYPES.ELEMENT,
+          nodeName: elementNode.nodeName,
+          attributes: Object.keys(elementNode.attributes).map(key => {
+            let value = elementNode.attributes[key].value;
+            const name = elementNode.attributes[key].localName;
+
+            if (/^blob:/.test(value)) {
+              value = value.replace(/^blob:http:\/\/localhost:\d+\/(.+)/, '$1'); // TODO don't replace localhost once render-grid implements absolute urls
+            }
+
+            return {
+              name,
+              value,
+            };
+          }),
+          childNodeIndexes: elementNode.childNodes.length
+            ? childrenFactory(domNodes, elementNode.childNodes)
+            : [],
+        };
+      }
+    } else if (nodeType === NODE_TYPES.TEXT) {
       node = {
-        nodeType: NODE_TYPES.ELEMENT,
+        nodeType: NODE_TYPES.TEXT,
+        nodeValue: elementNode.nodeValue,
+      };
+    } else if (nodeType === NODE_TYPES.DOCUMENT_TYPE) {
+      node = {
+        nodeType: NODE_TYPES.DOCUMENT_TYPE,
         nodeName: elementNode.nodeName,
-        attributes: Object.keys(elementNode.attributes).map(key => {
-          let value = elementNode.attributes[key].value;
-          const name = elementNode.attributes[key].localName;
-
-          if (/^blob:/.test(value)) {
-            value = value.replace(/^blob:http:\/\/localhost:\d+\/(.+)/, '$1'); // TODO don't replace localhost once render-grid implements absolute urls
-          }
-
-          return {
-            name,
-            value,
-          };
-        }),
-        childNodeIndexes: elementNode.childNodes.length
-          ? childrenFactory(domNodes, elementNode.childNodes)
-          : [],
       };
     }
-  } else if (nodeType === NODE_TYPES.TEXT) {
-    node = {
-      nodeType: NODE_TYPES.TEXT,
-      nodeValue: elementNode.nodeValue,
-    };
-  } else if (nodeType === NODE_TYPES.DOCUMENT_TYPE) {
-    node = {
-      nodeType: NODE_TYPES.DOCUMENT_TYPE,
-      nodeName: elementNode.nodeName,
-    };
-  }
 
-  if (node) {
-    domNodes.push(node);
-    return domNodes.length - 1;
-  } else {
-    // console.log(`Unknown nodeType: ${nodeType}`);
-    return null;
+    if (node) {
+      domNodes.push(node);
+      return domNodes.length - 1;
+    } else {
+      // console.log(`Unknown nodeType: ${nodeType}`);
+      return null;
+    }
   }
-};
+}
 
 module.exports = domNodesToCdt;
-module.exports.NODE_TYPES = NODE_TYPES;
+module.exports.NODE_TYPES = {
+  ELEMENT: 1,
+  TEXT: 3,
+  DOCUMENT: 9,
+  DOCUMENT_TYPE: 10,
+};
