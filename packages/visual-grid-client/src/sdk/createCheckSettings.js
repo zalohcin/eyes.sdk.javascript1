85d8ac858e85f3f69bef19610f0875eb7e0af1e9
Amit Zur <sendwithchibo@gmail.com>
2018-09-20 18:12:48 +0300

    add support for selectorRegions and ignore regions with selector

--- packages/visual-grid-client/src/sdk/createCheckSettings.js
+++ packages/visual-grid-client/src/sdk/createCheckSettings.js
@@ -4,9 +4,7 @@ const {CheckSettings, Region} = require('@applitools/eyes.sdk.core');
 function createCheckSettings({ignore}) {
   const checkSettings = new CheckSettings(0);
   if (ignore) {
-    if (!Array.isArray(ignore)) {
-      ignore = [ignore];
-    }
+    ignore = [].concat(ignore);
     for (const region of ignore) {
       checkSettings.ignoreRegions(Region.fromObject(region));
     }
