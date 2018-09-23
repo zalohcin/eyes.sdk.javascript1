20d873ad67435d2f81401c2170daac906fedea81
Amit Zur <sendwithchibo@gmail.com>
2018-09-23 14:42:59 +0300

    sensible default - create default batch for when not specified

---
+++ packages/visual-grid-client/test/it/renderingGridClient.it.test.js
@@ -0,0 +1,31 @@
+'use strict';
+const {describe, it, beforeEach, before} = require('mocha');
+const {expect} = require('chai');
+const makeRenderingGridClient = require('../../src/sdk/renderingGridClient');
+const createFakeWrapper = require('../util/createFakeWrapper');
+
+const apiKey = 'api key';
+
+describe('renderingGridClient', () => {
+  it('sets a new batch', async () => {
+    const {openEyes} = makeRenderingGridClient({
+      showLogs: process.env.APPLITOOLS_SHOW_LOGS,
+    });
+
+    const wrapper = createFakeWrapper('http://some_url');
+    await openEyes({
+      wrappers: [wrapper],
+      apiKey,
+    });
+
+    const batchId = wrapper.getBatch().toJSON().id;
+
+    await openEyes({
+      wrappers: [wrapper],
+      apiKey,
+    });
+
+    const batchId2 = wrapper.getBatch().toJSON().id;
+    expect(batchId).to.equal(batchId2);
+  });
+});
