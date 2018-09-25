5de6f65cda51e562f31f3b69e38f3c7b8d3995c6
Amit Zur <sendwithchibo@gmail.com>
2018-09-25 13:51:01 +0300

    support CSSOM in empty style tags

--- packages/visual-grid-client/test/unit/browser-util/domNodesToCdt.test.js
+++ packages/visual-grid-client/test/unit/browser-util/domNodesToCdt.test.js
@@ -2,16 +2,20 @@
 const {describe, it} = require('mocha');
 const {expect} = require('chai');
 const {JSDOM} = require('jsdom');
+const puppeteer = require('puppeteer');
+const testServer = require('../../util/testServer');
 const domNodesToCdt = require('../../../src/browser-util/domNodesToCdt');
 const {NODE_TYPES} = domNodesToCdt;
 const {loadFixture, loadJsonFixture} = require('../../util/loadFixture');
-const _fs = require('fs');
-const {resolve: _r} = require('path');
+const fs = require('fs');
+const {resolve} = require('path');
 
 function getDocNode(htmlStr) {
   const dom = new JSDOM(htmlStr, {url: 'http://something.org/'});
   return dom.window.document;
 }
+
+const domNodesToCdtInPuppeteer = new Function(`return (${domNodesToCdt})(document)`);
 
 describe('domNodesToCdt', () => {
   it('works for DOM with 1 element', () => {
@@ -57,8 +61,35 @@ describe('domNodesToCdt', () => {
   it('works for test.html', () => {
     const docNode = getDocNode(loadFixture('test.html'));
     const cdt = domNodesToCdt(docNode);
-    // _fs.writeFileSync(_r(__dirname, '../../fixtures/test.orig.cdt.json'), JSON.stringify(cdt, null, 2));
+    if (process.env.APPLITOOLS_UPDATE_FIXTURES) {
+      const cdtStr = JSON.stringify(cdt, null, 2);
+      fs.writeFileSync(resolve(__dirname, '../../fixtures/test.orig.cdt.json'), cdtStr);
+    }
     const expectedCdt = loadJsonFixture('test.orig.cdt.json');
     expect(cdt).to.deep.equal(expectedCdt);
+  });
+
+  it('handles empty style tags with CSSOM content', async () => {
+    const server = await testServer();
+    const browser = await puppeteer.launch();
+    try {
+      const page = await browser.newPage();
+      const url = `http://localhost:${server.port}/cssom.html`;
+
+      await page.goto(url);
+
+      const cdt = await page.evaluate(domNodesToCdtInPuppeteer);
+
+      if (process.env.APPLITOOLS_UPDATE_FIXTURES) {
+        const cdtStr = JSON.stringify(cdt, null, 2);
+        fs.writeFileSync(resolve(__dirname, '../../fixtures/cssom.cdt.json'), cdtStr);
+      }
+
+      const expectedCdt = loadJsonFixture('cssom.cdt.json');
+      expect(cdt).to.eql(expectedCdt);
+    } finally {
+      await browser.close();
+      await server.close();
+    }
   });
 });
