e93204c6b62dd95ad59e0a712d4453b3d0d9bbf3
Oleh Astappiev <astappev@gmail.com>
2018-10-15 21:55:22 +0200

    Update TestDomSending test, add more cases

--- eyes.selenium/test/integration/TestDomSending.spec.js
+++ eyes.selenium/test/integration/TestDomSending.spec.js
@@ -17,17 +17,26 @@ describe('Eyes.Selenium.JavaScript - send dom', function () {
     eyes = new Eyes();
     eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
     eyes.setLogHandler(new ConsoleLogHandler(true));
+    eyes.setSendDom(true);
     // eyes.setProxy('http://localhost:8888');
   });
 
-  it('should happen something', function () {
-    return eyes.open(driver, this.test.parent.title, this.test.title, new RectangleSize(800, 560)).then(driver => {
-      driver.get('http://applitools.github.io/demo/TestPages/DomTest/dom_capture.html');
+  it('should happen something', async function () {
+    driver = await eyes.open(driver, this.test.parent.title, this.test.title, new RectangleSize(800, 560));
 
-      eyes.check('A Window', Target.window().sendDom(true));
+    await driver.get('http://applitools.github.io/demo/TestPages/DomTest/dom_capture.html');
+    await eyes.check('A Test Window', Target.window().sendDom(true));
 
-      return eyes.close();
-    });
+    await driver.get('https://applitools.com');
+    await eyes.check('An Applitools Window', Target.window());
+
+    await driver.get('https://booking.com');
+    await driver.findElement(By.css('input.sb-searchbox__input.sb-destination__input')).sendKeys('Kiev, Ukraine');
+    await driver.findElement(By.css('button.sb-searchbox__button')).click();
+    await driver.sleep(200);
+    await eyes.check('A Booking Window', Target.window());
+
+    await eyes.close();
   });
 
   afterEach(function () {
