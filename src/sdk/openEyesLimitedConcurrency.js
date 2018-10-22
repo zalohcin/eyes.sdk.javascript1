1140ade6da5f75a88c806ffc8d239bcd310175b4
Amit Zur <sendwithchibo@gmail.com>
2018-10-22 09:27:50 +0300

    adding logs

--- src/sdk/openEyesLimitedConcurrency.js
+++ src/sdk/openEyesLimitedConcurrency.js
@@ -2,14 +2,20 @@
 const throatPkg = require('throat');
 const {presult} = require('@applitools/functional-commons');
 
-function makeOpenEyesLimitedConcurrency(openEyes, concurrency) {
+function makeOpenEyesLimitedConcurrency({openEyes, concurrency, logger}) {
   const throat = throatPkg(concurrency);
 
   return function openEyesLimitedConcurrency(args) {
+    const {testName} = args,
+      ts = Date.now();
+    logger.log(`openEyesLimitedConcurrency: inserting to throat, testName=${testName}, ts=${ts}`);
     return new Promise((resolveOpen, rejectOpen) => {
       throat(
         () =>
           new Promise(async resolveTest => {
+            logger.log(
+              `openEyesLimitedConcurrency: going to run the real openEyes, testName=${testName}, ts=${ts}`,
+            );
             const [openErr, openResult] = await presult(openEyes(args));
 
             if (openErr) {
