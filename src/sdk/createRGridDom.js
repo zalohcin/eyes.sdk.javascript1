ab413c2246497c4e62952d537325e618e5d2f6fc
Amit Zur <sendwithchibo@gmail.com>
2018-11-21 17:37:41 +0200

    update packages and freeze puppeteer on 1.9.0

--- src/sdk/createRGridDom.js
+++ src/sdk/createRGridDom.js
@@ -3,8 +3,8 @@
 const {RGridDom} = require('@applitools/eyes-sdk-core');
 
 function createRGridDom({cdt, resources}) {
-  const resourceArr = Object.values(resources).sort(
-    (r1, r2) => (r1.getUrl() > r2.getUrl() ? 1 : -1),
+  const resourceArr = Object.values(resources).sort((r1, r2) =>
+    r1.getUrl() > r2.getUrl() ? 1 : -1,
   );
   const rGridDom = new RGridDom();
   rGridDom.setDomNodes(cdt);
