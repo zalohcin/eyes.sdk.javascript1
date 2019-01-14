cbb8ad26361b067d4ea7fe3e038320008c4d5e91
Oleh Astappiev <astappev@gmail.com>
2019-01-14 19:17:39 +0100

    fix(visual-grid-client): when region object given in ignore/floating of checkWindow, use it without create new Region

--- packages/visual-grid-client/src/sdk/createCheckSettings.js
+++ packages/visual-grid-client/src/sdk/createCheckSettings.js
@@ -1,24 +1,33 @@
 'use strict';
-const {CheckSettings, Region} = require('@applitools/eyes-sdk-core');
+
+const {CheckSettings, Region, IgnoreRegionByRectangle, FloatingRegionByRectangle} = require('@applitools/eyes-sdk-core');
 
 function createCheckSettings({ignore, floating}) {
   const checkSettings = new CheckSettings(0);
   if (ignore) {
     ignore = [].concat(ignore);
     for (const region of ignore) {
-      checkSettings.ignoreRegions(new Region(region));
+      if (region instanceof IgnoreRegionByRectangle) {
+        checkSettings.ignoreRegions(region);
+      } else {
+        checkSettings.ignoreRegions(new Region(region));
+      }
     }
   }
   if (floating) {
     floating = [].concat(floating);
     for (const region of floating) {
-      checkSettings.floatingRegion(
-        new Region(region),
-        region.maxUpOffset,
-        region.maxDownOffset,
-        region.maxLeftOffset,
-        region.maxRightOffset,
-      );
+      if (region instanceof FloatingRegionByRectangle) {
+        checkSettings.floatingRegion(region);
+      } else {
+        checkSettings.floatingRegion(
+          new Region(region),
+          region.maxUpOffset,
+          region.maxDownOffset,
+          region.maxLeftOffset,
+          region.maxRightOffset,
+        );
+      }
     }
   }
   return checkSettings;
