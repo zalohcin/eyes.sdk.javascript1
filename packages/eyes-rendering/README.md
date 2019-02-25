cf4b2fc43d5e7dc3f0a8300a3b53da6350e96e24
Oleh Astappiev <astappev@gmail.com>
2019-02-25 21:58:02 +0100

    refactor: remove `eyes-rendering` as it was merged with `eyes-selenium`

--- packages/eyes-rendering/README.md
+++
@@ -1,6 +0,0 @@
-@applitools/eyes-rendering
-===================
-
-Applitools Eyes SDK for Visual Grid
-
-[![npm](https://img.shields.io/npm/v/@applitools/eyes-rendering.svg?style=for-the-badge)](https://www.npmjs.com/package/@applitools/eyes-rendering)
--- packages/eyes-rendering/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-exports.Target = require('./lib/fluent/Target').Target;
-exports.CheckRGSettings = require('./lib/fluent/CheckRGSettings').CheckRGSettings;
-
-exports.Eyes = require('./lib/Eyes').Eyes;
-exports.RenderingConfiguration = require('./lib/RenderingConfiguration').RenderingConfiguration;
