5e7174cc3a13ac63aec28539ae67e228df777da3
Oleh Astappiev <astappev@gmail.com>
2019-03-04 19:12:16 +0100

    feat(eyes-selenium): add common public classes from sdk-core to selenium

--- packages/eyes-selenium/index.js
+++ packages/eyes-selenium/index.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const common = require('@applitools/eyes-common');
+const core = require('@applitools/eyes-sdk-core');
+
 exports.SeleniumConfiguration = require('./lib/config/SeleniumConfiguration').SeleniumConfiguration;
 exports.BrowserType = require('./lib/config/BrowserType').BrowserType;
 exports.ScreenOrientation = require('./lib/config/ScreenOrientation').ScreenOrientation;
@@ -59,3 +62,54 @@ exports.SeleniumJavaScriptExecutor = require('./lib/SeleniumJavaScriptExecutor')
 exports.Eyes = require('./lib/Eyes').Eyes;
 exports.EyesSelenium = require('./lib/EyesSelenium').EyesSelenium;
 exports.EyesVisualGrid = require('./lib/EyesVisualGrid').EyesVisualGrid;
+
+
+// eyes-common
+exports.Configuration = common.Configuration;
+exports.BatchInfo = common.BatchInfo;
+exports.PropertyData = common.PropertyData;
+exports.ProxySettings = common.ProxySettings;
+exports.EyesError = common.EyesError;
+exports.DebugScreenshotsProvider = common.DebugScreenshotsProvider;
+exports.FileDebugScreenshotsProvider = common.FileDebugScreenshotsProvider;
+exports.NullDebugScreenshotProvider = common.NullDebugScreenshotProvider;
+exports.Location = common.Location;
+exports.RectangleSize = common.RectangleSize;
+exports.Region = common.Region;
+exports.MutableImage = common.MutableImage;
+exports.ConsoleLogHandler = common.ConsoleLogHandler;
+exports.FileLogHandler = common.FileLogHandler;
+exports.LogHandler = common.LogHandler;
+exports.NullLogHandler = common.NullLogHandler;
+
+
+// eyes-sdk-core
+exports.ImageProvider = core.ImageProvider;
+exports.CorsIframeHandle = core.CorsIframeHandle;
+exports.CutProvider = core.CutProvider;
+exports.FixedCutProvider = core.FixedCutProvider;
+exports.NullCutProvider = core.NullCutProvider;
+exports.UnscaledFixedCutProvider = core.UnscaledFixedCutProvider;
+exports.RemoteSessionEventHandler = core.RemoteSessionEventHandler;
+exports.SessionEventHandler = core.SessionEventHandler;
+exports.ValidationInfo = core.ValidationInfo;
+exports.ValidationResult = core.ValidationResult;
+exports.CoordinatesTypeConversionError = core.CoordinatesTypeConversionError;
+exports.DiffsFoundError = core.DiffsFoundError;
+exports.NewTestError = core.NewTestError;
+exports.OutOfBoundsError = core.OutOfBoundsError;
+exports.TestFailedError = core.TestFailedError;
+exports.ExactMatchSettings = core.ExactMatchSettings;
+exports.FloatingMatchSettings = core.FloatingMatchSettings;
+exports.ImageMatchSettings = core.ImageMatchSettings;
+exports.MatchLevel = core.MatchLevel;
+exports.MatchResult = core.MatchResult;
+exports.NullRegionProvider = core.NullRegionProvider;
+exports.RegionProvider = core.RegionProvider;
+exports.RenderingInfo = core.RenderingInfo;
+exports.RunningSession = core.RunningSession;
+exports.SessionType = core.SessionType;
+exports.FailureReports = core.FailureReports;
+exports.TestResults = core.TestResults;
+exports.TestResultsFormatter = core.TestResultsFormatter;
+exports.TestResultsStatus = core.TestResultsStatus;
