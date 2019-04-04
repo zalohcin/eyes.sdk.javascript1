799cf79c3953c2e72543ebb764b88152713784c1
Amit Zur <sendwithchibo@gmail.com>
2019-04-04 13:21:19 +0300

    take screenshot function

--- packages/visual-grid-client/src/visual-grid-client.js
+++ packages/visual-grid-client/src/visual-grid-client.js
@@ -9,10 +9,12 @@ const {
 
 const makeVisualGridClient = require('./sdk/renderingGridClient');
 const configParams = require('./sdk/configParams');
+const takeScreenshot = require('./sdk/takeScreenshot');
 
 module.exports = {
   configParams,
   makeVisualGridClient,
+  takeScreenshot,
   DiffsFoundError,
   TestResults,
   TestFailedError,
---
+++ packages/visual-grid-client/test/e2e/takeScreenshot.e2e.test.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const {describe, it, before, after, beforeEach} = require('mocha');
+const {expect} = require('chai');
+const puppeteer = require('puppeteer');
+const takeScreenshot = require('../../src/sdk/takeScreenshot');
+const {getProcessPageAndSerializeScript} = require('@applitools/dom-snapshot');
+const fetch = require('node-fetch');
+const {presult} = require('@applitools/functional-commons');
+const testServer = require('../util/testServer');
+
+describe('takeScreenshot e2e', () => {
+  let server, browser, page;
+
+  before(async () => {
+    server = await testServer({port: 3456});
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  after(async () => {
+    await server.close();
+    await browser.close();
+  });
+
+  it('returns url to screenshot', async () => {
+    const website = `http://localhost:${server.port}/test-iframe.html`;
+    const apiKey = process.env.APPLITOOLS_API_KEY;
+    const serverUrl = 'https://eyesapi.applitools.com';
+    const renderInfo = await fetch(`${serverUrl}/api/sessions/renderInfo?apiKey=${apiKey}`).then(
+      r => r.json(),
+    );
+
+    const processPageAndSerialize = `(${await getProcessPageAndSerializeScript()})()`;
+    await page.goto(website);
+    const {cdt, url, resourceUrls, blobs, frames} = await page.evaluate(processPageAndSerialize);
+    const [err, imageLocation] = await presult(
+      takeScreenshot({
+        apiKey,
+        showLogs: process.env.APPLITOOLS_SHOW_LOGS,
+        renderInfo,
+        cdt,
+        url,
+        resourceUrls,
+        blobs,
+        frames,
+        browsers: [{width: 1920, height: 1440}],
+      }),
+    );
+
+    err && console.log(err);
+    expect(err).to.be.undefined;
+
+    console.log(imageLocation);
+    expect(imageLocation).to.match(/https:\/\/.+/);
+  });
+});
