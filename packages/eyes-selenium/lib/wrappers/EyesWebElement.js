24507fcd8bd966ea05dfb2854f66f25e8063ea41
Oleh Astappiev <astappiev@gmail.com>
2019-05-03 15:15:40 +0200

    fix(eyes-selenium): when `Symbol.serialize` is not working, use `toJSON`

--- packages/eyes-selenium/lib/wrappers/EyesWebElement.js
+++ packages/eyes-selenium/lib/wrappers/EyesWebElement.js
@@ -97,6 +97,15 @@ class EyesWebElement extends WebElement {
   }
 
   /**
+   * Sometimes, Symbol('serialize') is not working (e.g. in Jest), therefore we need this to correctly unwrap it
+   *
+   * @return {!Object} Returns the serialized representation of this WebElement.
+   */
+  toJSON() {
+    return this.getId().then(WebElement.buildId);
+  }
+
+  /**
    * @return {Promise<Region>}
    */
   async getBounds() {
