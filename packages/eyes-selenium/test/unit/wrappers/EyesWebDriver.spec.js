268448eac08b3ed5df9e5afb63d067cc3afbc3ac
Oleh Astappiev <astappiev@gmail.com>
2019-05-23 17:13:42 +0200

    test(eyes-selenium): use headless browser in tests

--- packages/eyes-selenium/test/unit/wrappers/EyesWebDriver.spec.js
+++ packages/eyes-selenium/test/unit/wrappers/EyesWebDriver.spec.js
@@ -4,13 +4,17 @@ require('chromedriver');
 const assert = require('assert');
 
 const { Builder, Capabilities, By } = require('selenium-webdriver');
+const { Options: ChromeOptions } = require('selenium-webdriver/chrome');
 const { Eyes, EyesWebElement } = require('../../../index');
 
 describe('EyesWebDriver', function () {
   this.timeout(5 * 60 * 1000);
 
   it('findElement', async function () {
-    const driver = new Builder().withCapabilities(Capabilities.chrome()).build();
+    const driver = await new Builder()
+      .withCapabilities(Capabilities.chrome())
+      .setChromeOptions(new ChromeOptions().headless().addArguments('disable-infobars'))
+      .build();
 
     const eyes = new Eyes();
     await eyes.open(driver, 'EyesWebDriver test', 'test findElement');
