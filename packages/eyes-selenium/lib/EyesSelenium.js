31c1fe092fd5a3b8dbe94bc2dc4b622407ed1edc
Oleh Astappiev <astappiev@gmail.com>
2019-06-19 14:38:25 +0200

    fix(eyes-selenium): use `html` as default scrollRootElement (2)

--- packages/eyes-selenium/lib/EyesSelenium.js
+++ packages/eyes-selenium/lib/EyesSelenium.js
@@ -471,7 +471,7 @@ class EyesSelenium extends Eyes {
           scrollRootElement = await frame.getForceScrollRootElement(this._driver);
         }
         if (scrollRootElement == null) {
-          scrollRootElement = driver.findElement(By.tagName("html"));
+          scrollRootElement = this._driver.findElement(By.tagName("html"));
         }
       }
       this._logger.verbose("scrollRootElement:", scrollRootElement);
