ea098d7ca989a07c64be08123b6b428b00aba134
Oleh Astappiev <astappiev@gmail.com>
2019-07-02 20:17:17 +0200

    docs(eyes-common): add @signature for setProperties of Configuration
    Close #80

--- packages/eyes-common/lib/config/Configuration.js
+++ packages/eyes-common/lib/config/Configuration.js
@@ -391,6 +391,12 @@ class Configuration {
   }
 
   /**
+   * @signature `setProperties(properties)`
+   * @sigparam {PropertyData[]} properties - A list of PropertyData instances
+   *
+   * @signature `setProperties(properties)`
+   * @sigparam {PropertyDataObject[]} properties - A list of property data objects
+   *
    * @param {PropertyData[]|PropertyDataObject[]} value
    * @return {this}
    */
