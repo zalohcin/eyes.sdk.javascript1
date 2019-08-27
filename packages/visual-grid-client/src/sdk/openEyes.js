dfa2b93ba3ddbaac5c9dc44477831682cf7ea150
Oleh Astappiev <astappiev@gmail.com>
2019-08-27 11:15:58 +0200

    chore(visual-grid-client): fix eslint issue

--- packages/visual-grid-client/src/sdk/openEyes.js
+++ packages/visual-grid-client/src/sdk/openEyes.js
@@ -13,6 +13,8 @@ const {
   apiKeyFailMsg,
 } = require('./wrapperUtils');
 
+const SUPPORTED_BROWSERS = ['firefox', 'ie10', 'ie11', 'edge', 'chrome', 'ie'];
+
 function makeOpenEyes({
   appName: _appName,
   browser: _browser,
@@ -226,11 +228,8 @@ function makeOpenEyes({
     }
 
     function getBrowserError(browser) {
-      if (
-        browser.name &&
-        !['firefox', 'ie10', 'ie11', 'edge', 'chrome', 'ie'].includes(browser.name.replace(/-canary$/, ''))
-      ) {
-        return `browser name should be one of the following 'chrome', 'firefox', 'ie10', 'ie11' or 'edge' but recieved '${browser.name}'.`;
+      if (browser.name && !SUPPORTED_BROWSERS.includes(browser.name.replace(/-canary$/, ''))) {
+        return `browser name should be one of the following 'chrome', 'firefox', 'ie10', 'ie11' or 'edge' but received '${browser.name}'.`;
       }
       if (browser.name && !browser.deviceName && (!browser.height || !browser.width)) {
         return `browser '${browser.name}' should include 'height' and 'width' parameters.`;
