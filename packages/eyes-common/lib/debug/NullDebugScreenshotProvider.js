daceac10635c094e8ada38b6583131d6d3dbe2bd
Oleh Astappiev <astappiev@gmail.com>
2019-09-04 17:25:42 +0200

    chore(eyes-common): add TODO message

--- packages/eyes-common/lib/debug/NullDebugScreenshotProvider.js
+++ packages/eyes-common/lib/debug/NullDebugScreenshotProvider.js
@@ -3,6 +3,7 @@
 const { DebugScreenshotsProvider } = require('./DebugScreenshotsProvider');
 
 /**
+ * TODO: rename to NullDebugScreenshotsProvider, should be renamed in other SDKs as well (come from Java)
  * A mock debug screenshot provider.
  */
 class NullDebugScreenshotProvider extends DebugScreenshotsProvider {
