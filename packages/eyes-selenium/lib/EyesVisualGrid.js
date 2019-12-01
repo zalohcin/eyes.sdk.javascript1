096a5f104e88d84267cd254109594af044c35698
David Haeffner <dhaeffner@gmail.com>
2019-12-01 15:17:58 +0200

    refactor: (eyes-selenium) Created a generator function for the openEyes configuration in VisualGridRunner for easier integration testing.

--- packages/eyes-selenium/lib/EyesVisualGrid.js
+++ packages/eyes-selenium/lib/EyesVisualGrid.js
@@ -47,6 +47,44 @@ class EyesVisualGrid extends Eyes {
     return `eyes.selenium.visualgrid.javascript/${VERSION}`;
   }
 
+
+  /**
+   * @private
+   * @return {object}
+   */
+  _generateOpenEyesConfiguration() {
+    return {
+      appName: this._configuration.getAppName(),
+      testName: this._configuration.getTestName(),
+      displayName: this._configuration.getDisplayName(),
+      browser: this._configuration.getBrowsersInfo(),
+      properties: this._configuration.getProperties(),
+      batchSequenceName: this._configuration.getBatch() && this._configuration.getBatch().getSequenceName(),
+      batchName: this._configuration.getBatch() && this._configuration.getBatch().getName(),
+      batchId: this._configuration.getBatch() && this._configuration.getBatch().getId(),
+      batchNotify: this._configuration.getBatch() && this._configuration.getBatch().getNotifyOnCompletion(),
+      baselineBranchName: this._configuration.getBaselineBranchName(),
+      baselineEnvName: this._configuration.getBaselineEnvName(),
+      baselineName: this._configuration.getBaselineEnvName(),
+      envName: this._configuration.getEnvironmentName(),
+      branchName: this._configuration.getBranchName(),
+      saveFailedTests: this._configuration.getSaveFailedTests(),
+      saveNewTests: this._configuration.getSaveNewTests(),
+      compareWithParentBranch: this._configuration.getCompareWithParentBranch(),
+      ignoreBaseline: this._configuration.getIgnoreBaseline(),
+      parentBranchName: this._configuration.getParentBranchName(),
+      isDisabled: this._configuration.getIsDisabled(),
+      matchTimeout: this._configuration.getMatchTimeout(),
+
+      ignoreCaret: this._configuration.getIgnoreCaret(),
+      matchLevel: this._configuration.getMatchLevel(),
+      useDom: this._configuration.getUseDom(),
+      enablePatterns: this._configuration.getEnablePatterns(),
+      ignoreDisplacements: this._configuration.getIgnoreDisplacements(),
+      saveDebugData: this._configuration.getSaveDebugData(),
+    };
+  }
+
   /**
    * @inheritDoc
    */
@@ -94,36 +132,7 @@ class EyesVisualGrid extends Eyes {
       }
     }
 
-    const { checkWindow, close, abort } = await openEyes({
-      appName: this._configuration.getAppName(),
-      testName: this._configuration.getTestName(),
-      displayName: this._configuration.getDisplayName(),
-      browser: this._configuration.getBrowsersInfo(),
-      properties: this._configuration.getProperties(),
-      batchSequenceName: this._configuration.getBatch() && this._configuration.getBatch().getSequenceName(),
-      batchName: this._configuration.getBatch() && this._configuration.getBatch().getName(),
-      batchId: this._configuration.getBatch() && this._configuration.getBatch().getId(),
-      batchNotify: this._configuration.getBatch() && this._configuration.getBatch().getNotifyOnCompletion(),
-      baselineBranchName: this._configuration.getBaselineBranchName(),
-      baselineEnvName: this._configuration.getBaselineEnvName(),
-      baselineName: this._configuration.getBaselineEnvName(),
-      envName: this._configuration.getEnvironmentName(),
-      branchName: this._configuration.getBranchName(),
-      saveFailedTests: this._configuration.getSaveFailedTests(),
-      saveNewTests: this._configuration.getSaveNewTests(),
-      compareWithParentBranch: this._configuration.getCompareWithParentBranch(),
-      ignoreBaseline: this._configuration.getIgnoreBaseline(),
-      parentBranchName: this._configuration.getParentBranchName(),
-      isDisabled: this._configuration.getIsDisabled(),
-      matchTimeout: this._configuration.getMatchTimeout(),
-
-      ignoreCaret: this._configuration.getIgnoreCaret(),
-      matchLevel: this._configuration.getMatchLevel(),
-      useDom: this._configuration.getUseDom(),
-      enablePatterns: this._configuration.getEnablePatterns(),
-      ignoreDisplacements: this._configuration.getIgnoreDisplacements(),
-      saveDebugData: this._configuration.getSaveDebugData(),
-    });
+    const { checkWindow, close, abort } = await openEyes(this._generateOpenEyesConfiguration());
 
     this._isOpen = true;
     this._checkWindowCommand = checkWindow;
---
+++ packages/eyes-selenium/test/it/lib/runner/VisualGridRunner.spec.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const assert = require('assert');
+const path = require('path');
+const { readFileSync, writeFileSync } = require('fs');
+const { Eyes, VisualGridRunner } = require('../../../../index');
+
+function parseData(input) {
+  return JSON.stringify(
+    input,
+    null,
+    2
+  );
+}
+
+function getFixture(name) {
+  return JSON.parse(readFileSync(path.join(__dirname, '..', '..', '..', 'fixtures', name)));
+}
+
+function updateFixture(name, input) {
+  if (!process.env.APPLITOOLS_UPDATE_FIXTURES) return;
+  writeFileSync(
+    path.join(__dirname, '..', '..', '..', 'fixtures', name),
+    parseData(input)
+  );
+}
+
+describe('VisualGridRunner', function () {
+  const browsersInfo = [
+    {
+      width: 800,
+      height: 600,
+      name: 'firefox',
+    },
+    {
+      deviceName: 'iPhone 4',
+      screenOrientation: 'portrait',
+    },
+  ];
+
+  const conf = {
+    apiKey: process.env.APPLITOOLS_API_KEY,
+    appName: 'myAppName',
+    testName: 'myTestName',
+    setSendDom: false,
+    browsersInfo,
+    batch: {
+      id: 'myBatchId',
+      name: 'myBatchName',
+      notifyOnCompletion: true,
+    },
+  };
+
+  const eyes = new Eyes(new VisualGridRunner());
+  eyes.setConfiguration(conf);
+
+  it('should have a valid open configuration', () => {
+    const actual = parseData(eyes._generateOpenEyesConfiguration());
+
+    const fixtureName = 'VisualGridRunner.openEyesConfiguration.json';
+    updateFixture(fixtureName, actual);
+
+    assert.deepStrictEqual(actual, getFixture(fixtureName));
+  });
+});
