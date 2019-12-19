84d76e727c002621361ff564821c8a04da8efaba
David Haeffner <dhaeffner@gmail.com>
2019-12-19 13:49:53 +0200

    expose resource pool functions in sdk-test-kit index

--- packages/sdk-test-kit/src/index.js
+++ packages/sdk-test-kit/src/index.js
@@ -1,5 +1,8 @@
 const {makeRun} = require('./coverage-tests/index')
+const {makeResourcePool, findResourceInPool} = require('./resource-pool/index')
 
 module.exports = {
   makeRun,
+  makeResourcePool,
+  findResourceInPool,
 }
