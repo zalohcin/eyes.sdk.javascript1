fb2d8ab498428076351a813339eb1b7608b37438
David Haeffner <dhaeffner@gmail.com>
2019-12-19 20:48:27 +0200

    sdk-test-kit: removed outdated requires and exports

--- packages/sdk-test-kit/src/index.js
+++ packages/sdk-test-kit/src/index.js
@@ -1,8 +1,5 @@
 const {makeRun} = require('./coverage-tests/index')
-const {makeResourcePool, findResourceInPool} = require('./resource-pool/index')
 
 module.exports = {
   makeRun,
-  makeResourcePool,
-  findResourceInPool,
 }
