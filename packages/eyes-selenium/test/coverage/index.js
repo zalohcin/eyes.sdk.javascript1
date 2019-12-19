c13a50a9ebbbcc9b6904ba061005032e5bcd4674
David Haeffner <dhaeffner@gmail.com>
2019-12-19 13:51:30 +0200

    Added hooks to the eyes-selenium coverate test implementation and wired up resource pool creation

--- packages/eyes-selenium/test/coverage/index.js
+++ packages/eyes-selenium/test/coverage/index.js
@@ -9,20 +9,39 @@ const {
   StitchMode,
   VisualGridRunner,
 } = require('../../index')
-const {makeRun} = require('@applitools/sdk-test-kit')
+const {makeRun, makeResourcePool, findResourceInPool} = require('@applitools/sdk-test-kit')
 
 const sdkName = 'eyes-selenium'
 const batch = new BatchInfo(`JS Coverage Tests - ${sdkName}`)
 
-async function initialize(context) {
+function initialize(context) {
   let driver
   let eyes
+  let resource
 
-  async function setup() {
-    driver = await new Builder()
-      .forBrowser('chrome')
-      .setChromeOptions(new ChromeOptions().headless())
-      .build()
+  async function beforeAll() {
+    const resourcePool = await makeResourcePool(10, async () => {
+      const driver = await new Builder()
+        .forBrowser('chrome')
+        .setChromeOptions(new ChromeOptions().headless())
+        .build()
+      return {driver, isAvailable: true}
+    })
+    return {resourcePool}
+  }
+
+  async function afterAll(sharedContext) {
+    for (const index in sharedContext.resourcePool) {
+      await sharedContext.resourcePool[index].driver.close()
+    }
+  }
+
+  async function beforeEach() {
+    resource = await findResourceInPool(context.executionMode.resourcePool, resource => {
+      return resource.isAvailable
+    })
+    driver = resource.driver
+    resource.isAvailable = false
     if (context.executionMode.isVisualGrid) {
       eyes = new Eyes(new VisualGridRunner())
     } else if (context.executionMode.isCssStitching) {
@@ -35,7 +54,9 @@ async function initialize(context) {
     eyes.setBatch(batch)
   }
 
-  await setup()
+  function afterEach() {
+    resource.isAvailable = true
+  }
 
   async function visit(url) {
     await driver.get(url)
@@ -65,10 +86,9 @@ async function initialize(context) {
 
   async function close(options) {
     await eyes.close(options)
-    await driver.close()
   }
 
-  return {visit, open, check, close}
+  return {beforeAll, afterAll, beforeEach, afterEach, visit, open, check, close}
 }
 
 const supportedTests = [
