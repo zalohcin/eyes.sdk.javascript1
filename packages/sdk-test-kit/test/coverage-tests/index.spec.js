b8d83f09439dabd9d8d7b0c9d0483e5868e15ec3
David Haeffner <dhaeffner@gmail.com>
2019-12-19 21:13:45 +0200

    sdk-test-kit: added some initial unit tests for the coverage test runner

--- packages/sdk-test-kit/test/coverage-tests/index.spec.js
+++ packages/sdk-test-kit/test/coverage-tests/index.spec.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const {makeCoverageTests, _makeRun} = require('../../src/coverage-tests/index')
+const {makeCoverageTests, makeRun} = require('../../src/coverage-tests/index')
 
 const fakeSDK = {
   open: () => {},
@@ -16,9 +16,53 @@ describe('coverage-tests', () => {
   })
 
   describe('makeRun', () => {
-    it.skip('should return a run function', () => {})
-    it.skip('should run a test with the provided implementation', () => {})
-    it.skip('should call the hook methods in the correct order', () => {})
-    it.skip('should record and display errors correctly', () => {})
+    it('should return a run function', () => {
+      const {run} = makeRun()
+      assert.deepStrictEqual(typeof run, 'function')
+    })
+    it('should run tests with the provided implementation', async () => {
+      let count = 0
+      const name = 'blah'
+      const initialize = () => {
+        return {
+          setup: () => {
+            count++
+          },
+          visit: () => {},
+          open: () => {},
+          check: () => {},
+          close: () => {},
+          teardown: () => {
+            count++
+          },
+        }
+      }
+      const supportedTests = [
+        {name: 'checkRegionClassic', executionMode: {blah: true}},
+        {name: 'checkRegionClassic', executionMode: {blahblah: true}},
+      ]
+      const {run} = makeRun(name, initialize)
+      await run(supportedTests, false)
+      assert.deepStrictEqual(count, 4)
+    })
+    // TODO: add mocking for console.log, or test error/reporting functions directly
+    it.skip('should record and display errors from a run', async () => {
+      const name = 'blah'
+      const initialize = () => {
+        return {
+          setup: () => {},
+          visit: () => {},
+          open: () => {
+            throw 'blah error'
+          },
+          check: () => {},
+          close: () => {},
+          teardown: () => {},
+        }
+      }
+      const supportedTests = [{name: 'checkRegionClassic', executionMode: {blah: true}}]
+      const {run} = makeRun(name, initialize)
+      await run(supportedTests)
+    })
   })
 })
