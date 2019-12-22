9a368eff50d304e9f4e8a737e391c58d1465381c
David Haeffner <dhaeffner@gmail.com>
2019-12-22 11:39:50 +0200

    eyes-selenium: updated the coverage-test implementation to use the new check API and added to the list of supported tests

--- packages/eyes-selenium/test/coverage/index.js
+++ packages/eyes-selenium/test/coverage/index.js
@@ -40,19 +40,21 @@ async function initialize({displayName, executionMode}) {
     )
   }
 
-  async function checkRegion({isClassicApi = false, locator} = {}) {
+  async function checkRegion({isClassicApi = false, locator, tag, matchTimeout} = {}) {
     if (isClassicApi) {
-      await eyes.checkElementBy(By.css(locator))
+      await eyes.checkElementBy(By.css(locator), matchTimeout, tag)
     } else {
-      await eyes.check(undefined, Target.region(By.css(locator)).fully())
+      await eyes.check(tag, Target.region(By.css(locator)).fully())
     }
   }
 
-  async function checkWindow({isClassicApi = false} = {}) {
+  async function checkWindow({isClassicApi = false, isFully = false, tag, matchTimeout} = {}) {
     if (isClassicApi) {
-      await eyes.checkWindow()
+      await eyes.checkWindow(tag, matchTimeout, isFully)
     } else {
-      await eyes.check(undefined, Target.window().fully())
+      isFully
+        ? await eyes.check(undefined, Target.window().fully())
+        : await eyes.check(undefined, Target.window())
     }
   }
 
@@ -72,15 +74,27 @@ const supportedTests = [
   {name: 'checkRegionClassic', executionMode: {isVisualGrid: true}},
   {name: 'checkRegionClassic', executionMode: {isCssStitching: true}},
   {name: 'checkRegionClassic', executionMode: {isScrollStitching: true}},
+  {name: 'checkRegionClassicWithOverFlow', executionMode: {isVisualGrid: true}},
+  {name: 'checkRegionClassicWithOverFlow', executionMode: {isCssStitching: true}},
+  {name: 'checkRegionClassicWithOverFlow', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowClassicViewport', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowClassicViewport', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowClassicViewport', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowClassicFully', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowClassicFully', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowClassicFully', executionMode: {isScrollStitching: true}},
   {name: 'checkRegionFluent', executionMode: {isVisualGrid: true}},
   {name: 'checkRegionFluent', executionMode: {isCssStitching: true}},
   {name: 'checkRegionFluent', executionMode: {isScrollStitching: true}},
-  {name: 'checkWindowClassic', executionMode: {isVisualGrid: true}},
-  {name: 'checkWindowClassic', executionMode: {isCssStitching: true}},
-  {name: 'checkWindowClassic', executionMode: {isScrollStitching: true}},
-  {name: 'checkWindowFluent', executionMode: {isVisualGrid: true}},
-  {name: 'checkWindowFluent', executionMode: {isCssStitching: true}},
-  {name: 'checkWindowFluent', executionMode: {isScrollStitching: true}},
+  {name: 'checkRegionFluentWithOverFlow', executionMode: {isVisualGrid: true}},
+  {name: 'checkRegionFluentWithOverFlow', executionMode: {isCssStitching: true}},
+  {name: 'checkRegionFluentWithOverFlow', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowFluentViewport', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowFluentViewport', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowFluentViewport', executionMode: {isScrollStitching: true}},
+  {name: 'checkWindowFluentFully', executionMode: {isVisualGrid: true}},
+  {name: 'checkWindowFluentFully', executionMode: {isCssStitching: true}},
+  {name: 'checkWindowFluentFully', executionMode: {isScrollStitching: true}},
 ]
 
 makeRunTests(sdkName, initialize).runTests(supportedTests)
