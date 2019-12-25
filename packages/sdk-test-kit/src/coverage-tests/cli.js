5a0af1b3fdde5fd0a658afd0b3de773e3a7161e6
David Haeffner <dhaeffner@gmail.com>
2019-12-25 14:32:36 +0200

    sdk-test-kit: updated the cli to send a test report, and cleaned up the cli script some

--- packages/sdk-test-kit/src/coverage-tests/cli.js
+++ packages/sdk-test-kit/src/coverage-tests/cli.js
@@ -2,6 +2,7 @@
 const yargs = require('yargs')
 const path = require('path')
 const {makeRunTests} = require('./index')
+const {sendReport} = require('./send-report')
 const {exec} = require('child_process')
 const {version} = require('../../package.json')
 
@@ -9,6 +10,7 @@ yargs
   .option('run', {
     alias: 'r',
     describe: 'run coverage tests for a given SDK',
+    demandOption: true,
   })
   .option('remote', {
     alias: 'r',
@@ -17,6 +19,7 @@ yargs
   .option('path', {
     alias: 'p',
     describe: 'path to implementation file',
+    demandOption: true,
   })
   .option('concurrency', {
     alias: 'c',
@@ -26,29 +29,49 @@ yargs
     alias: 'n',
     describe: 'kill all ghost browser processes (POSIX only)',
   })
-  .help().argv
+  .option('sendReport', {
+    alias: 's',
+    describe: 'send a result report to the sandbox QA dashboard',
+  })
 
-if (yargs.argv.run && yargs.argv.path) {
-  const sdkImplementation = require(path.join(path.resolve('.'), yargs.argv.path))
-  console.log(`Coverage Tests DSL (v${version})`)
-  console.log('a.k.a. Da Schwartz Lang - except no substitutes\n')
-  console.log(`Running coverage tests for ${sdkImplementation.name}...\n`)
-  makeRunTests(sdkImplementation.initialize)
-    .runTests(sdkImplementation.supportedTests, {
-      host: yargs.argv.remote,
-      concurrency: yargs.argv.concurrency,
+async function run(args) {
+  console.log(args)
+  if (!args) return
+  let exitCode = 0
+  if (args.run && args.path) {
+    const sdkImplementation = require(path.join(path.resolve('.'), args.path))
+
+    console.log(`Coverage Tests DSL (v${version})`)
+    console.log('a.k.a. Da Schwartz Lang - except no substitutes\n')
+    console.log(`Running coverage tests for ${sdkImplementation.name}...\n`)
+
+    const {report} = await makeRunTests(
+      sdkImplementation.name,
+      sdkImplementation.initialize,
+    ).runTests(sdkImplementation.supportedTests, {
+      host: args.remote,
+      concurrency: args.concurrency,
     })
-    .then(({report}) => {
-      console.log(`-------------------- SUMMARY --------------------`)
-      report.summary.forEach(entry => console.log(entry))
-      const exitCode = Object.keys(report.errors).length ? 1 : 0
-      console.log(`Exiting with code ${exitCode}`)
-      if (Object.keys(report.errors).length) {
-        console.log(`-------------------- ERRORS --------------------`)
-        console.log(report.errors)
-      }
-      process.exit(exitCode)
-    })
-} else if (yargs.argv.nuke) {
-  exec(`ps ax | grep Chrome | grep headless | awk '{print $1}' | xargs kill -9`)
+
+    console.log(`-------------------- SUMMARY --------------------`)
+    report.summary.forEach(entry => console.log(entry))
+    exitCode = Object.keys(report.errors).length ? 1 : 0
+
+    if (Object.keys(report.errors).length) {
+      console.log(`-------------------- ERRORS --------------------`)
+      console.log(report.errors)
+    }
+
+    if (args.sendReport) {
+      console.log('Sending report to QA dashboard...')
+      await sendReport(report.toSendReportSchema())
+      console.log('Report sent!')
+    }
+  } else if (args.nuke) {
+    exec(`ps ax | grep Chrome | grep headless | awk '{print $1}' | xargs kill -9`)
+  }
+  console.log(`Exited with code ${exitCode}`)
+  process.exit(exitCode)
 }
+
+run(yargs.argv)
