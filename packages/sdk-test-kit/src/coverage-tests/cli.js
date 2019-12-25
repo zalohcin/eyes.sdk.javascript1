c5d7a2196d081a92acac46f463c32025012a7ef9
David Haeffner <dhaeffner@gmail.com>
2019-12-25 20:02:25 +0200

    sdk-test-kit: broke each major step of the cli script into separate functions

--- packages/sdk-test-kit/src/coverage-tests/cli.js
+++ packages/sdk-test-kit/src/coverage-tests/cli.js
@@ -34,51 +34,66 @@ yargs
   })
 
 async function run(args) {
-  let exitCode = 0
-  if (args.path) {
+  if (args.nuke) {
+    exec(`ps ax | grep Chrome | grep headless | awk '{print $1}' | xargs kill -9`)
+    console.log('KABOOM!')
+  } else if (args.path) {
     const sdkImplementation = require(path.join(path.resolve('.'), args.path))
 
     console.log(`Running coverage tests for ${sdkImplementation.name}...\n`)
 
-    if (needsChromeDriver(args, sdkImplementation))
-      await startChromeDriver(sdkImplementation.options.chromeDriverOptions)
-
-    const {report} = await makeRunTests(
-      sdkImplementation.name,
-      sdkImplementation.initialize,
-    ).runTests(sdkImplementation.supportedTests, {
-      host: args.remote,
-      concurrency: args.concurrency,
-    })
-
-    if (needsChromeDriver(args, sdkImplementation)) stopChromeDriver()
-
-    if (args.sendReport) {
-      console.log('Sending report to QA dashboard...')
-      const result = await sendReport(report.toSendReportSchema())
-      if (result.isSuccessful) console.log('Report sent!')
-      else console.log(`Report not sent because of: ${result.message}`)
-    }
-
-    console.log(`-------------------- SUMMARY --------------------`)
-    report.summary.forEach(entry => console.log(entry))
-
-    if (Object.keys(report.errors).length) {
-      exitCode = 1
-      console.log(`-------------------- ERRORS --------------------`)
-      console.log(report.errors)
-    }
-  } else if (args.nuke) {
-    exec(`ps ax | grep Chrome | grep headless | awk '{print $1}' | xargs kill -9`)
+    const report = await doRunTests(args, sdkImplementation)
+    await doSendReport(args, report)
+    await doDisplayResults(report)
+    doExitCode(report.errors)
   }
-  console.log(`Exited with code ${exitCode}`)
-  process.exit(exitCode)
 }
 
 run(yargs.argv)
 
 function needsChromeDriver(args, sdkImplementation) {
   return !args.remote && sdkImplementation.options && sdkImplementation.options.needsChromeDriver
+}
+
+function doExitCode(errors) {
+  const exitCode = Object.keys(errors).length ? 1 : 0
+  console.log(`Exited with code ${exitCode}`)
+  process.exit(exitCode)
+}
+
+async function doRunTests(args, sdkImplementation) {
+  if (needsChromeDriver(args, sdkImplementation))
+    await startChromeDriver(sdkImplementation.options.chromeDriverOptions)
+
+  const {report} = await makeRunTests(
+    sdkImplementation.name,
+    sdkImplementation.initialize,
+  ).runTests(sdkImplementation.supportedTests, {
+    host: args.remote,
+    concurrency: args.concurrency,
+  })
+
+  if (needsChromeDriver(args, sdkImplementation)) stopChromeDriver()
+
+  return report
+}
+
+async function doSendReport(args, report) {
+  if (args.sendReport) {
+    console.log('Sending report to QA dashboard...')
+    const result = await sendReport(report.toSendReportSchema())
+    if (result.isSuccessful) console.log('Report sent!')
+    else console.log(`Report not sent because of: ${result.message}`)
+  }
+}
+
+function doDisplayResults(report) {
+  if (Object.keys(report.errors).length) {
+    console.log(`-------------------- ERRORS --------------------`)
+    console.log(report.errors)
+  }
+  console.log(`-------------------- SUMMARY --------------------`)
+  report.summary.forEach(entry => console.log(entry))
 }
 
 async function startChromeDriver(options = ['--port=4444', '--url-base=wd/hub', '--silent']) {
