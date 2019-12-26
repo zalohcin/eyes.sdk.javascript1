7e136da2d31659ccaae489578b780f4b7ba3bbcc
Sarah Stahl <sarahstahl@normandy.local>
2019-12-26 15:09:54 +0200

    added IE script

--- packages/eyes-testcafe/build/rollup.config.js
+++ packages/eyes-testcafe/build/rollup.config.js
@@ -5,39 +5,79 @@ const resolve = require('rollup-plugin-node-resolve')
 const commonjs = require('rollup-plugin-commonjs')
 const builtins = require('@joseph184/rollup-plugin-node-builtins')
 const globals = require('rollup-plugin-node-globals')
+const polyfill = require('rollup-plugin-polyfill');
 const wrapeAndExportPlugin = require('./wrapeAndExportPlugin')
 const prepareForClientFunctionPlugin = require('./prepareForClientFunctionPlugin')
 
 const inputFileName = '@applitools/dom-capture/src/browser/captureFrameAndPoll.js'
+const fileName = 'captureFrameAndPoll'
+const fileNameIE = 'captureFrameAndPollForIE'
 
-exports.input = inputFileName
-exports.output = {
-  file: 'dist/captureFrameAndPoll.js',
-  format: 'iife',
-  name: 'captureFrameAndPoll',
-}
-exports.plugins = [
-  builtins(),
-  globals(),
-  resolve({}),
-  commonjs({include: '../../node_modules/**', ignoreGlobal: true}),
-  babel({
-    plugins: [
-      '@babel/plugin-transform-spread',
-      '@babel/plugin-transform-regenerator',
-      [
-        '@babel/plugin-transform-runtime',
-        {
-          absoluteRuntime: false,
-          corejs: false,
-          helpers: false,
-          regenerator: true,
-          useESModules: false,
-        },
+const domCaptureConfig = {
+  input: inputFileName,
+  output: {
+    file: `dist/${fileName}.js`,
+    format: 'iife',
+    name: fileName
+  },
+  plugins: [
+    builtins(),
+    globals(),
+    resolve({}),
+    commonjs({include: '../../node_modules/**', ignoreGlobal: true}),
+    babel({
+      plugins: [
+        '@babel/plugin-transform-spread',
+        '@babel/plugin-transform-regenerator',
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            absoluteRuntime: false,
+            corejs: false,
+            helpers: false,
+            regenerator: true,
+            useESModules: false,
+          },
+        ],
       ],
-    ],
-    runtimeHelpers: true,
-  }),
-  prepareForClientFunctionPlugin,
-  wrapeAndExportPlugin,
-]
+      runtimeHelpers: true,
+    }),
+    prepareForClientFunctionPlugin(fileName),
+    wrapeAndExportPlugin(fileName),
+  ]
+}
+
+const domCaptureForIEConfig = {
+  input: inputFileName,
+  output: {
+    file: `dist/${fileNameIE}.js`,
+    format: 'iife',
+    name: fileNameIE
+  },
+  plugins:[
+    polyfill(
+      ['core-js/stable', 'regenerator-runtime/runtime', 'url-polyfill', 'whatwg-fetch'],
+      { method: 'require' },
+    ),
+    resolve({}),
+    commonjs({include: '**', ignoreGlobal: true}),
+    builtins(),
+    globals(),
+    babel({
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          '@babel/env',
+          {
+            modules: false,
+            targets: {ie: 10}
+          }
+        ]
+      ]
+    }),
+    prepareForClientFunctionPlugin(fileNameIE),
+    wrapeAndExportPlugin(fileNameIE),
+  ]
+}
+
+module.exports = [domCaptureConfig, domCaptureForIEConfig]
--- packages/eyes-testcafe/build/wrapeAndExportPlugin.js
+++ packages/eyes-testcafe/build/wrapeAndExportPlugin.js
@@ -2,12 +2,13 @@
 
 // This rollup plugin is meant to take the functions defined in processPageAndSerialize
 // wrape them in a single function and export it.
-exports.generateBundle = (_outputOptions, bundle, _isWrite) => {
-  const bundleFile = bundle['captureFrameAndPoll.js']
-
-  bundleFile.code = `
+module.exports = (name) => ({
+  generateBundle: (_outputOptions, bundle, _isWrite) => {
+    const bundleFile = bundle[`${name}.js`]
+    bundleFile.code = `
 module.exports = () => {
   ${bundleFile.code}
   return captureFrameAndPoll()
 }`
-}
+  }
+})
