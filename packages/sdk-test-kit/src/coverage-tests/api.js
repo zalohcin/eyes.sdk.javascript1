8f79cecb3f22c21e08db398ee78d5d15210dc7dd
David Haeffner <dhaeffner@gmail.com>
2019-12-29 14:58:03 +0200

    sdk-test-kit: added stub api.js file to document the coverage-tests API

---
+++ packages/sdk-test-kit/src/coverage-tests/api.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+function checkFrame(locator, options) {}
+function checkRegion(target, options) {}
+function checkWindow(options) {}
+function close(options) {}
+function open(options) {}
+function scrollDown(pixels) {}
+function type(locator, inputText) {}
+function visit(url) {}
+/* eslint-enable no-unused-vars */
+
+module.exports = {
+  checkFrame,
+  checkRegion,
+  checkWindow,
+  close,
+  open,
+  scrollDown,
+  type,
+  visit,
+}
