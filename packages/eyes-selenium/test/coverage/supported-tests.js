207abc59948af4f8bb40583030a311622c65784d
David Haeffner <dhaeffner@gmail.com>
2019-12-30 12:42:03 +0200

    eyes-selenium: opted into more coverage tests and re-ordered the list to be alphabetical

--- packages/eyes-selenium/test/coverage/supported-tests.js
+++ packages/eyes-selenium/test/coverage/supported-tests.js
@@ -1,45 +1,60 @@
 const executionModes = [{isVisualGrid: true}, {isCssStitching: true}, {isScrollStitching: true}]
 const tests = [
+  'TestAcmeLogin',
+  'TestCheckElementFully_Fluent',
+  'TestCheckElementWithIgnoreRegionByElementOutsideTheViewport_Fluent',
+  'TestCheckElementWithIgnoreRegionBySameElement_Fluent',
   'TestCheckFrame',
+  'TestCheckFrame_Fluent',
+  'TestCheckFrameFully_Fluent',
+  'TestCheckFrameInFrame_Fully_Fluent',
+  'TestCheckFrameInFrame_Fully_Fluent2',
+  'TestCheckFullWindowWithMultipleIgnoreRegionsBySelector_Fluent',
+  'TestCheckOverflowingRegionByCoordinates_Fluent',
   'TestCheckPageWithHeader_Window',
   'TestCheckPageWithHeaderFully_Window',
   'TestCheckPageWithHeader_Region',
   'TestCheckPageWithHeaderFully_Region',
   'TestCheckRegion',
   'TestCheckRegion2',
-  'TestCheckRegionInFrame',
-  'TestCheckWindow',
-  'TestCheckWindowAfterScroll',
-  'TestCheckWindowFully',
-  'TestCheckWindowViewport',
-  'TestDoubleCheckWindow',
-  'TestCheckElementFully_Fluent',
-  'TestCheckElementWithIgnoreRegionByElementOutsideTheViewport_Fluent',
-  'TestCheckElementWithIgnoreRegionBySameElement_Fluent',
-  'TestCheckFrame_Fluent',
-  'TestCheckFrameFully_Fluent',
-  'TestCheckFullWindowWithMultipleIgnoreRegionsBySelector_Fluent',
   'TestCheckRegionByCoordinates_Fluent',
   'TestCheckRegionByCoordinatesInFrame_Fluent',
   'TestCheckRegionByCoordinatesInFrameFully_Fluent',
-  'TestCheckOverflowingRegionByCoordinates_Fluent',
   'TestCheckRegionBySelectorAfterManualScroll_Fluent',
+  'TestCheckRegionInFrame',
+  'TestCheckRegionInFrame_Fluent',
+  'TestCheckRegionInFrame3_Fluent',
+  'TestCheckRegionInAVeryBigFrame',
+  'TestCheckRegionInAVeryBigFrameAfterManualSwitchToFrame',
   'TestCheckRegionWithIgnoreRegion_Fluent',
+  'TestCheckWindow',
+  'TestCheckWindowAfterScroll',
+  'TestCheckWindow_Body',
+  'TestCheckWindow_Html',
   'TestCheckWindow_Fluent',
+  'TestCheckWindowFully',
+  'TestCheckWindowViewport',
+  'TestCheckWindow_Simple_Html',
+  'TestCheckWindowWithFloatingByRegion_Fluent',
+  'TestCheckWindowWithFloatingBySelector_Fluent',
   'TestCheckWindowWithIgnoreRegion_Fluent',
   'TestCheckWindowWithIgnoreBySelector_Fluent',
   'TestCheckWindowWithIgnoreBySelector_Centered_Fluent',
   'TestCheckWindowWithIgnoreBySelector_Stretched_Fluent',
-  'TestCheckWindowWithFloatingByRegion_Fluent',
-  'TestCheckWindowWithFloatingBySelector_Fluent',
-  'TestScrollbarsHiddenAndReturned_Fluent',
+  'TestDoubleCheckWindow',
   'TestSimpleRegion',
+  'TestScrollbarsHiddenAndReturned_Fluent',
 ]
 let supportedTests = []
+function addTest(name, executionMode) {
+  supportedTests.push({name, executionMode})
+}
 tests.forEach(name => {
   executionModes.forEach(executionMode => {
-    supportedTests.push({name, executionMode})
+    addTest(name, executionMode)
   })
 })
+addTest('Test Abort', {isVisualGrid: true})
+addTest('Test Abort', {isCssStitching: true})
 
 module.exports = supportedTests
