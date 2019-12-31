5a350642b3d85fdf30270d20771f32953b2cea5c
David Haeffner <dhaeffner@gmail.com>
2019-12-31 11:47:20 +0200

    sdk-test-kit: added namespacing to the coverage-test api for easier exporting (e.g., `import {Commands} from 'api'`)

--- packages/sdk-test-kit/src/coverage-tests/api.ts
+++ packages/sdk-test-kit/src/coverage-tests/api.ts
@@ -17,56 +17,58 @@ interface FloatingRegion {
     maxRight: number,
 }
 
-interface Abort {
-    (): Promise<any> 
-}
+namespace Commands {
+  interface Abort {
+      (): Promise<any> 
+  }
 
-interface CheckFrame {
-    (target: Selector | Array<Selector>, options: { isClassicApi?: boolean, isFully?: boolean }): Promise<any>
-}
+  interface CheckFrame {
+      (target: Selector | Array<Selector>, options: { isClassicApi?: boolean, isFully?: boolean }): Promise<any>
+  }
 
-interface CheckRegion {
-    (
-        target: Selector | Array<Selector> | RegionCoordinates,
-        options: {
-            isClassicApi?: boolean,
-            isFully?: boolean,
-            inFrame: Selector,
-            ignoreRegion: Selector | RegionCoordinates
-        }
-    ): Promise<any>
-}
+  interface CheckRegion {
+      (
+          target: Selector | Array<Selector> | RegionCoordinates,
+          options: {
+              isClassicApi?: boolean,
+              isFully?: boolean,
+              inFrame: Selector,
+              ignoreRegion: Selector | RegionCoordinates
+          }
+      ): Promise<any>
+  }
 
-interface CheckWindow {
-    (options: {
-        isClassicApi?: boolean,
-        isFully?: boolean,
-        ignoreRegion?: Selector | RegionCoordinates
-        floatingRegion?: FloatingRegion
-        scrollRootElement?: Selector
-    }): Promise<any>
-}
+  interface CheckWindow {
+      (options: {
+          isClassicApi?: boolean,
+          isFully?: boolean,
+          ignoreRegion?: Selector | RegionCoordinates
+          floatingRegion?: FloatingRegion
+          scrollRootElement?: Selector
+      }): Promise<any>
+  }
 
-interface Close {
-    (options: { throwsException?: boolean }) : Promise<any>
-}
+  interface Close {
+      (options: { throwsException?: boolean }) : Promise<any>
+  }
 
-interface Open {
-    (options: {appName: string, viewportSize: string}): Promise<any>
-}
+  interface Open {
+      (options: {appName: string, viewportSize: string}): Promise<any>
+  }
 
-interface ScrollDown {
-    (pixes: number): Promise<any>
-}
+  interface ScrollDown {
+      (pixes: number): Promise<any>
+  }
 
-interface SwitchToFrame {
-    (selector: Selector): Promise<any>
-}
+  interface SwitchToFrame {
+      (selector: Selector): Promise<any>
+  }
 
-interface Type {
-    (selector: Selector, inputText: string): Promise<any>
-}
+  interface Type {
+      (selector: Selector, inputText: string): Promise<any>
+  }
 
-interface Visit {
-    (url: string): Promise<any>
+  interface Visit {
+      (url: string): Promise<any>
+  }
 }
