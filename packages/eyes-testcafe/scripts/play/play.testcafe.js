3aac34fb90530ab787b718c4abea4820568584c0
Daniel Schwartz <danielsch24@gmail.com>
2020-01-02 11:29:31 +0200

    better play files

--- packages/eyes-testcafe/scripts/play/play.testcafe.js
+++ packages/eyes-testcafe/scripts/play/play.testcafe.js
@@ -12,7 +12,7 @@ const {Eyes, Target, ConsoleLogHandler} = require('../..')
 const eyes = new Eyes()
 const configuration = new Configuration({
   stitchMode: StitchMode.SCROLL,
-  viewportSize: {width: 1024, height: 768},
+  viewportSize: {width: 600, height: 500},
 })
 eyes.setConfiguration(configuration)
 
@@ -20,30 +20,23 @@ if (process.env.APPLITOOLS_SHOW_LOGS || process.env.LIVE) {
   eyes.setLogHandler(new ConsoleLogHandler(true))
 }
 
-fixture`Play`.page`https://google.com`
+fixture`Play`.page`https://applitools.com/helloworld`
 
 test('Play', async t => {
-  await new Promise(r => setTimeout(r, 1000))
   await eyes.open(t, 'Play Testcafe', 'play testcafe')
   await eyes.check('page play', Target.window().fully())
   const result = await eyes.close()
-  console.log('XXXXXXXXX: r', result)
+  console.log('Play result', result)
 })
 
 // test('Play', async t => {
 //   // await t.resizeWindow(600, 500)
 //   // await new Promise(r => setTimeout(r, 1000))
-
-//   const captureFrameAndPollForIE = require('../../dist/captureFrameAndPollForIE')
-//   const {TestCafeJavaScriptExecutor} = require('../../lib/TestCafeJavaScriptExecutor')
-//   const ex = new TestCafeJavaScriptExecutor(t)
-
-//   // console.log('XXXXXXXXX: captureFrameAndPollForIE', captureFrameAndPollForIE.toString())
-//   const r = await ex.executeScript(captureFrameAndPollForIE)
-//   const r2 = await ex.executeScript(captureFrameAndPollForIE)
-//   console.log('XXXXXXXXX: r', r2)
-
-//   // await t.eval(() => (document.documentElement.style.transform = 'translate(-0px, -0px)'))
-//   // const name = new Date().toISOString().replace(/:/g, '_')
-//   // const image = await t.takeScreenshot(`./render-${name}.png`)
+//   // await t.eval(() => {
+//   //   document.documentElement.scrollLeft = 0
+//   //   document.documentElement.scrollTop = 656
+//   // })
+//   const name = new Date().toISOString().replace(/:/g, '_')
+//   const image = await t.takeScreenshot(`./render-${name}.png`)
+//   console.log('XXXXXXXXX: image', image)
 // })
