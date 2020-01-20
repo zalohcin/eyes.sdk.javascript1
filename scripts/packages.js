c6ef8f905e9b4ba0299668ca65782cd73000b1f1
David Haeffner <dhaeffner@gmail.com>
2020-01-20 09:32:24 +0200

    ordered list of packages to give priority to shared libraries

--- scripts/packages.js
+++ scripts/packages.js
@@ -1,10 +1,24 @@
 const fs = require('fs')
 const path = require('path')
 
+function _moveToFront(packages, packageName) {
+  packages.unshift(
+    packages
+      .splice(
+        packages.findIndex(pkgName => pkgName === packageName),
+        1,
+      )
+      .toString(),
+  )
+  return packages
+}
+
 function makePackagesList() {
   const dir = path.join(__dirname, '..', 'packages')
   const packages = fs.readdirSync(dir).filter(f => fs.statSync(path.join(dir, f)).isDirectory())
-
+  _moveToFront(packages, 'eyes-sdk-core')
+  _moveToFront(packages, 'eyes-common')
+  _moveToFront(packages, 'sdk-fake-eyes-server')
   return packages.map(pkgName => {
     const pkgDir = path.join(dir, pkgName)
     const packageJson = require(path.join(pkgDir, 'package.json'))
