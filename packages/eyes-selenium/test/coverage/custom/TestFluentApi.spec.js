1549ce9dbffdc9fbba52c50cdfc115758fec955f
artem <artem.borodavka@applitools.com>
2020-01-22 13:51:12 +0200

    Makes test more flatten, move test with different execution modes to the different describe sections

--- packages/eyes-selenium/test/coverage/custom/TestFluentApi.spec.js
+++ packages/eyes-selenium/test/coverage/custom/TestFluentApi.spec.js
@@ -1,82 +1,165 @@
 'use strict'
 const {By} = require('selenium-webdriver')
-const {getDriver, getEyes, getSetups} = require('./util/TestSetup')
-const {Target, Region, BatchInfo} = require('../../../index')
+const {getDriver, getEyes} = require('./util/TestSetup')
+const {Target, Region, BatchInfo, StitchMode} = require('../../../index')
 const appName = 'Eyes Selenium SDK - Fluent API'
 describe(appName, () => {
-  let setups = getSetups()
   let batch = new BatchInfo('JS test')
-  setups.forEach(setup => {
-    describe(`Test run ${setup.title}`, () => {
-      let webDriver, eyes
-      beforeEach(async () => {
-        webDriver = await getDriver('CHROME')
-        await webDriver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
-        ;({eyes} = await getEyes(setup.runnerType, setup.stitchMode))
-        eyes.setBatch(batch)
-      })
+  let webDriver, eyes
 
-      afterEach(async () => {
-        await eyes.abortIfNotClosed()
-        await webDriver.quit()
-      })
+  afterEach(async () => {
+    await eyes.abortIfNotClosed()
+    await webDriver.quit()
+  })
 
-      it('TestCheckScrollableModal', async () => {
-        let driver = await eyes.open(webDriver, appName, `TestCheckScrollableModal${setup.title}`, {
+  let longIframes = [
+    {
+      clickableId: 'hidden_click',
+      IframeSelector: '#modal3 iframe',
+      title: 'TestCheckLongOutOfBoundsIFrameModal',
+    },
+    {
+      clickableId: 'stretched',
+      IframeSelector: '#modal2 iframe',
+      title: 'TestCheckLongIFrameModal',
+    },
+  ]
+
+  describe(`Test`, () => {
+    beforeEach(async () => {
+      webDriver = await getDriver('CHROME')
+      await webDriver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+      ;({eyes} = await getEyes('classic', StitchMode.CSS))
+      eyes.setBatch(batch)
+    })
+
+    it('TestCheckScrollableModal', async () => {
+      let driver = await eyes.open(webDriver, appName, `TestCheckScrollableModal${setup.title}`, {
+        width: 700,
+        height: 460,
+      })
+      driver.findElement(By.id('centered')).click()
+      let scrollRootLocator = setup.stitchMode === 'CSS' ? 'modal-content' : 'modal1'
+      let scrollRootSelector = By.id(scrollRootLocator)
+      await eyes.check(
+        'TestCheckScrollableModal',
+        Target.region(By.id('modal-content'))
+          .fully()
+          .scrollRootElement(scrollRootSelector),
+      )
+      await eyes.close()
+    })
+
+    longIframes.forEach(settings => {
+      it(`${settings.title}`, async () => {
+        let driver = await eyes.open(webDriver, appName, `${settings.title}`, {
           width: 700,
           height: 460,
         })
-        driver.findElement(By.id('centered')).click()
-        let scrollRootLocator = setup.stitchMode === 'CSS' ? 'modal-content' : 'modal1'
-        let scrollRootSelector = By.id(scrollRootLocator)
-        await eyes.check(
-          'TestCheckScrollableModal',
-          Target.region(By.id('modal-content'))
-            .fully()
-            .scrollRootElement(scrollRootSelector),
-        )
+        await driver.findElement(By.id(settings.clickableId)).click()
+        let frame = await driver.findElement(By.css(settings.IframeSelector))
+        await driver.switchTo().frame(frame)
+        let element = await driver.findElement(By.css('html'))
+        let rect = await element.getRect()
+        await performChecksOnLongRegion(rect, eyes)
         await eyes.close()
       })
+    })
+  })
 
-      let longIframes = [
-        {
-          clickableId: 'hidden_click',
-          IframeSelector: '#modal3 iframe',
-          title: 'TestCheckLongOutOfBoundsIFrameModal',
-        },
-        {
-          clickableId: 'stretched',
-          IframeSelector: '#modal2 iframe',
-          title: 'TestCheckLongIFrameModal',
-        },
-      ]
-      longIframes.forEach(settings => {
-        it(`${settings.title}`, async () => {
-          let driver = await eyes.open(webDriver, appName, `${settings.title}${setup.title}`, {
-            width: 700,
-            height: 460,
-          })
-          await driver.findElement(By.id(settings.clickableId)).click()
-          let frame = await driver.findElement(By.css(settings.IframeSelector))
-          await driver.switchTo().frame(frame)
-          let element = await driver.findElement(By.css('html'))
-          let rect = await element.getRect()
-          await performChecksOnLongRegion(rect)
-          await eyes.close()
-        })
+  describe(`Test_SCROLL`, () => {
+    let webDriver, eyes
+    beforeEach(async () => {
+      webDriver = await getDriver('CHROME')
+      await webDriver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+      ;({eyes} = await getEyes('classic', StitchMode.SCROLL))
+      eyes.setBatch(batch)
+    })
+
+    it('TestCheckScrollableModal', async () => {
+      let driver = await eyes.open(webDriver, appName, `TestCheckScrollableModal_SCROLL`, {
+        width: 700,
+        height: 460,
       })
+      driver.findElement(By.id('centered')).click()
+      let scrollRootSelector = By.id('modal1')
+      await eyes.check(
+        'TestCheckScrollableModal',
+        Target.region(By.id('modal-content'))
+          .fully()
+          .scrollRootElement(scrollRootSelector),
+      )
+      await eyes.close()
+    })
 
-      async function performChecksOnLongRegion(rect) {
-        for (let currentY = rect.y, c = 1; currentY < rect.y + rect.height; currentY += 5000, c++) {
-          let region
-          if (rect.height > currentY + 5000) {
-            region = new Region(rect.x, currentY, rect.width, 5000)
-          } else {
-            region = new Region(rect.x, currentY, rect.width, rect.height - currentY)
-          }
-          await eyes.check('Check Long Out of bounds Iframe Modal', Target.region(region))
-        }
-      }
+    longIframes.forEach(settings => {
+      it(`${settings.title}`, async () => {
+        let driver = await eyes.open(webDriver, appName, `${settings.title}_SCROLL`, {
+          width: 700,
+          height: 460,
+        })
+        await driver.findElement(By.id(settings.clickableId)).click()
+        let frame = await driver.findElement(By.css(settings.IframeSelector))
+        await driver.switchTo().frame(frame)
+        let element = await driver.findElement(By.css('html'))
+        let rect = await element.getRect()
+        await performChecksOnLongRegion(rect, eyes)
+        await eyes.close()
+      })
+    })
+  })
+
+  describe(`Test_VG`, () => {
+    let webDriver, eyes
+    beforeEach(async () => {
+      webDriver = await getDriver('CHROME')
+      await webDriver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+      ;({eyes} = await getEyes(setup.runnerType, setup.stitchMode))
+      eyes.setBatch(batch)
+    })
+
+    it('TestCheckScrollableModal', async () => {
+      let driver = await eyes.open(webDriver, appName, `TestCheckScrollableModal_VG`, {
+        width: 700,
+        height: 460,
+      })
+      driver.findElement(By.id('centered')).click()
+      let scrollRootSelector = By.id('modal-content')
+      await eyes.check(
+        'TestCheckScrollableModal',
+        Target.region(By.id('modal-content'))
+          .fully()
+          .scrollRootElement(scrollRootSelector),
+      )
+      await eyes.close()
+    })
+
+    longIframes.forEach(settings => {
+      it(`${settings.title}`, async () => {
+        let driver = await eyes.open(webDriver, appName, `${settings.title}_VG`, {
+          width: 700,
+          height: 460,
+        })
+        await driver.findElement(By.id(settings.clickableId)).click()
+        let frame = await driver.findElement(By.css(settings.IframeSelector))
+        await driver.switchTo().frame(frame)
+        let element = await driver.findElement(By.css('html'))
+        let rect = await element.getRect()
+        await performChecksOnLongRegion(rect, eyes)
+        await eyes.close()
+      })
     })
   })
 })
+
+async function performChecksOnLongRegion(rect, eyes) {
+  for (let currentY = rect.y, c = 1; currentY < rect.y + rect.height; currentY += 5000, c++) {
+    let region
+    if (rect.height > currentY + 5000) {
+      region = new Region(rect.x, currentY, rect.width, 5000)
+    } else {
+      region = new Region(rect.x, currentY, rect.width, rect.height - currentY)
+    }
+    await eyes.check('Check Long Out of bounds Iframe Modal', Target.region(region))
+  }
+}
--- packages/eyes-selenium/test/coverage/custom/TestSendDom.spec.js
+++ packages/eyes-selenium/test/coverage/custom/TestSendDom.spec.js
@@ -1,6 +1,6 @@
 'use strict'
 const {assertImage} = require('./util/ApiAssertions')
-const {getDriver, getEyes, getSetups} = require('./util/TestSetup')
+const {getDriver, getEyes} = require('./util/TestSetup')
 const {expect} = require('chai')
 const {By} = require('selenium-webdriver')
 const {EyesSelenium, BatchInfo, Target} = require('../../../index')
@@ -14,99 +14,96 @@ class DomInterceptingEyes extends EyesSelenium {
 }
 
 describe(appName, () => {
-  let setups = getSetups('VG')
   let batch = new BatchInfo('JS test')
-  setups.forEach(setup => {
-    describe(`TestSendDom${setup.title}`, () => {
-      let driver
-      beforeEach(async () => {
-        driver = await getDriver('CHROME')
-      })
+  describe(`TestSendDom`, () => {
+    let driver
+    beforeEach(async () => {
+      driver = await getDriver('CHROME')
+    })
 
-      afterEach(async () => {
-        await driver.quit()
+    afterEach(async () => {
+      await driver.quit()
+    })
+    // There differences between fixture and domJson which cause enormous console output
+    it.skip('TestSendDOM_FullWindow', async () => {
+      driver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+      let eyes = new DomInterceptingEyes()
+      eyes.setBatch(batch)
+      eyes.setBranchName('master')
+      await eyes.open(driver, 'Test Send DOM', 'Full Window', {
+        width: 1024,
+        height: 768,
       })
-      // There differences between fixture and domJson which cause enormous console output
-      it.skip('TestSendDOM_FullWindow', async () => {
-        driver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
-        let eyes = new DomInterceptingEyes()
-        eyes.setBatch(batch)
-        eyes.setBranchName('master')
-        await eyes.open(driver, 'Test Send DOM', 'Full Window', {
-          width: 1024,
-          height: 768,
-        })
-        try {
-          await eyes.check('Window', Target.window().fully())
-          let actual = eyes.domJSON
-          let results = await eyes.close(false)
-          await assertImage(results, {hasDom: true})
-          let expected = require('../../fixtures/expected_dom1.json')
-          let json = JSON.stringify(expected)
-          expect(actual).to.be.eql(json, 'There are differences in the dom json')
-        } finally {
-          await eyes.abortIfNotClosed()
-        }
+      try {
+        await eyes.check('Window', Target.window().fully())
+        let actual = eyes.domJSON
+        let results = await eyes.close(false)
+        await assertImage(results, {hasDom: true})
+        let expected = require('../../fixtures/expected_dom1.json')
+        let json = JSON.stringify(expected)
+        expect(actual).to.be.eql(json, 'There are differences in the dom json')
+      } finally {
+        await eyes.abortIfNotClosed()
+      }
+    })
+  })
+
+  describe(`Test run ${setup.title}`, () => {
+    let webDriver, eyes
+    beforeEach(async () => {
+      webDriver = await getDriver('CHROME')
+      ;({eyes} = await getEyes('VG'))
+      eyes.setBatch(batch)
+    })
+
+    afterEach(async () => {
+      await eyes.abortIfNotClosed()
+      await webDriver.quit()
+    })
+
+    it(`TestSendDOM_Selector`, async () => {
+      await webDriver.get('https://applitools.github.io/demo/TestPages/DomTest/dom_capture.html')
+      await eyes.open(webDriver, 'Test Send Dom', `Test SendDom`, {
+        width: 1000,
+        height: 700,
+      })
+      await eyes.check('region', Target.region(By.css('#scroll1')))
+      let results = await eyes.close(false)
+      await assertImage(results, {
+        hasDom: true,
+      })
+    })
+    it(`TestNotSendDOM`, async () => {
+      await webDriver.get('https://applitools.com/helloworld')
+      await eyes.open(webDriver, 'Test NOT SendDom', `Test NOT SendDom`, {
+        width: 1000,
+        height: 700,
+      })
+      await eyes.check('region', Target.window().sendDom(false))
+      let results = await eyes.close(false)
+      await assertImage(results, {
+        hasDom: false,
       })
     })
 
-    describe(`Test run ${setup.title}`, () => {
-      let webDriver, eyes
-      beforeEach(async () => {
-        webDriver = await getDriver('CHROME')
-        ;({eyes} = await getEyes(setup.runnerType, setup.stitchMode))
-        eyes.setBatch(batch)
-      })
+    let domCases = [
+      {
+        url: `https://applitools.github.io/demo/TestPages/DomTest/dom_capture.html`,
+        title: `TestSendDOM_1`,
+      },
+      {
+        url: `https://applitools.github.io/demo/TestPages/DomTest/dom_capture_2.html`,
+        title: `TestSendDOM_2`,
+      },
+    ]
 
-      afterEach(async () => {
-        await eyes.abortIfNotClosed()
-        await webDriver.quit()
-      })
-
-      it(`TestSendDOM_Selector`, async () => {
-        await webDriver.get('https://applitools.github.io/demo/TestPages/DomTest/dom_capture.html')
-        await eyes.open(webDriver, 'Test Send Dom', `Test Send Dom${setup.title}`, {
-          width: 1000,
-          height: 700,
-        })
-        await eyes.check('region', Target.region(By.css('#scroll1')))
+    domCases.forEach(domCase => {
+      it(`${domCase.title}`, async () => {
+        await webDriver.get(domCase.url)
+        await eyes.open(webDriver, 'Test Send DOM', `${domCase.title}`)
+        await eyes.checkWindow()
         let results = await eyes.close(false)
-        await assertImage(results, {
-          hasDom: true,
-        })
-      })
-      it(`TestNotSendDOM`, async () => {
-        await webDriver.get('https://applitools.com/helloworld')
-        await eyes.open(webDriver, 'Test NOT SendDom', `Test NOT SendDom${setup.title}`, {
-          width: 1000,
-          height: 700,
-        })
-        await eyes.check('region', Target.window().sendDom(false))
-        let results = await eyes.close(false)
-        await assertImage(results, {
-          hasDom: false,
-        })
-      })
-
-      let domCases = [
-        {
-          url: `https://applitools.github.io/demo/TestPages/DomTest/dom_capture.html`,
-          title: `TestSendDOM_1`,
-        },
-        {
-          url: `https://applitools.github.io/demo/TestPages/DomTest/dom_capture_2.html`,
-          title: `TestSendDOM_2`,
-        },
-      ]
-
-      domCases.forEach(domCase => {
-        it(`${domCase.title}`, async () => {
-          await webDriver.get(domCase.url)
-          await eyes.open(webDriver, 'Test Send DOM', `${domCase.title}${setup.title}`)
-          await eyes.checkWindow()
-          let results = await eyes.close(false)
-          await assertImage(results, {hasDom: true})
-        })
+        await assertImage(results, {hasDom: true})
       })
     })
   })
