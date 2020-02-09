3a8e68b3c3c9d49fb7671f6efd0ef0b60059f0e0
David Haeffner <dhaeffner@gmail.com>
2020-02-09 14:27:20 +0200

    wdio5-svc: added by and target exports to the index to remove the need for eyes-webdriverio as a peer dependency

--- packages/eyes-webdriverio-5-service/index.js
+++ packages/eyes-webdriverio-5-service/index.js
@@ -2,3 +2,5 @@
 
 exports.default = require('./src/EyesService').EyesService
 exports.EyesService = require('./src/EyesService').EyesService
+exports.By = require('@applitools/eyes-webdriverio').By
+exports.Target = require('@applitools/eyes-webdriverio').Target
