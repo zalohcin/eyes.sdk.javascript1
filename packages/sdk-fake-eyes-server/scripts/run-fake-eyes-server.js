0f75d92992b1f7d7a463d171133298bb2674d26e
Amit Zur <amit.zur@applitools.com>
2020-02-24 17:16:44 +0200

    fix fake eyes server

--- packages/sdk-fake-eyes-server/scripts/run-fake-eyes-server.js
+++ packages/sdk-fake-eyes-server/scripts/run-fake-eyes-server.js
@@ -3,7 +3,10 @@
 const fakeEyesServer = require('../lib/sdk-fake-eyes-server')
 
 ;(async () => {
-  const {port} = await fakeEyesServer({port: process.env.PORT || 0, hangUp: true})
+  const {port} = await fakeEyesServer({
+    port: process.env.PORT || 0,
+    hangUp: process.env.FAKE_SERVER_HANGUP,
+  })
   console.log(`http://localhost:${port}`)
 })().catch(err => {
   console.log('err', err)
