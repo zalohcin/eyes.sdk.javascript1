8bd61e0daadb9064ebb9e6f89951029011a83279
Amit Zur <amit.zur@applitools.com>
2020-03-05 09:10:46 +0200

    avoid unnecessary makeRun

--- packages/eyes-testcafe/test/coverage/index.js
+++ packages/eyes-testcafe/test/coverage/index.js
@@ -67,38 +67,37 @@ function makeCommands() {
   }
 }
 
-function makeRun() {
-  function createTestFiles(testFileDir, supportedTests) {
-    supportedTests.forEach(async supportedTest => {
-      const commands = makeCommands()
-      const tests = makeCoverageTests(commands)
-      const baselineTestName = `${supportedTest.name}${convertExecutionModeToSuffix(
-        supportedTest.executionMode,
-      )}`
-      if (commands._setup) {
-        commands._setup({
-          baselineTestName,
-          branchName: 'master',
-          executionMode: supportedTest.executionMode,
-        })
-      }
-      // run test to generate cache
-      await tests[supportedTest.name]()
-      // emit test contents from cache
-      const body = emitTest(supportedTest.name, commands._out())
-      if (!fs.existsSync(testFileDir)) {
-        fs.mkdirSync(testFileDir)
-      }
-      fs.writeFileSync(`${testFileDir}/${baselineTestName}.js`, body)
-    })
-  }
+function createTestFiles(testFileDir, supportedTests) {
+  supportedTests.forEach(async supportedTest => {
+    const commands = makeCommands()
+    const tests = makeCoverageTests(commands)
+    const baselineTestName = `${supportedTest.name}${convertExecutionModeToSuffix(
+      supportedTest.executionMode,
+    )}`
+    if (commands._setup) {
+      commands._setup({
+        baselineTestName,
+        branchName: 'master',
+        executionMode: supportedTest.executionMode,
+      })
+    }
+    // run test to generate cache
+    await tests[supportedTest.name]()
+    // emit test contents from cache
+    const body = emitTest(supportedTest.name, commands._out())
+    if (!fs.existsSync(testFileDir)) {
+      fs.mkdirSync(testFileDir)
+    }
+    fs.writeFileSync(`${testFileDir}/${baselineTestName}.js`, body)
+  })
+}
 
-  function emitTest(testName, output) {
-    let testCommands = {...output}
-    delete testCommands.visit
-    delete testCommands.getAllTestResults
-    delete testCommands.setup
-    return `const {Eyes, StitchMode, Target} = require('../../../index')
+function emitTest(testName, output) {
+  let testCommands = {...output}
+  delete testCommands.visit
+  delete testCommands.getAllTestResults
+  delete testCommands.setup
+  return `const {Eyes, StitchMode, Target} = require('../../../index')
 const eyes = new Eyes()
 
 fixture\`${testName}\`
@@ -111,39 +110,36 @@ fixture\`${testName}\`
 test('${testName}', async driver => {
   ${Object.values(testCommands).join('\n  ')}
 })`
-  }
+}
 
-  class MyStream extends stream.Writable {
-    _write(chunk, _encoding, next) {
-      this.report = JSON.parse(chunk.toString('utf8'))
-      next()
-    }
+class MyStream extends stream.Writable {
+  _write(chunk, _encoding, next) {
+    this.report = JSON.parse(chunk.toString('utf8'))
+    next()
   }
+}
 
-  async function run(supportedTests) {
-    process.stdout.write('Preparing test files...')
-    const testFileDir = `${__dirname}/tmp`
-    createTestFiles(testFileDir, supportedTests)
-    process.stdout.write(' Done!\n\n')
-    console.log(`(you can see them in ${testFileDir})`)
-    console.log('Running TestCafe tests...')
-    const testCafe = await createTestCafe('localhost', 1337, 1338)
-    const runner = testCafe.createRunner()
-    const stream = new MyStream()
-    await runner
-      .src(testFileDir)
-      .browsers('chrome:headless')
-      .concurrency(5)
-      .reporter('json', stream)
-      .run()
-      .catch(console.error)
-    stream.report.fixtures.forEach(fixture => {
-      console.log(fixture.tests)
-    })
-    testCafe.close()
-  }
-
-  return {run}
+async function run(supportedTests) {
+  process.stdout.write('Preparing test files...')
+  const testFileDir = `${__dirname}/tmp`
+  createTestFiles(testFileDir, supportedTests)
+  process.stdout.write(' Done!\n\n')
+  console.log(`(you can see them in ${testFileDir})`)
+  console.log('Running TestCafe tests...')
+  const testCafe = await createTestCafe('localhost', 1337, 1338)
+  const runner = testCafe.createRunner()
+  const stream = new MyStream()
+  await runner
+    .src(testFileDir)
+    .browsers('chrome:headless')
+    .concurrency(5)
+    .reporter('json', stream)
+    .run()
+    .catch(console.error)
+  stream.report.fixtures.forEach(fixture => {
+    console.log(fixture.tests)
+  })
+  testCafe.close()
 }
 
 const supportedTests = [
@@ -155,4 +151,4 @@ const supportedTests = [
   {name: 'TestCheckPageWithHeader_Window_Fully', executionMode: {isScrollStitching: true}},
 ]
 
-makeRun().run(supportedTests)
+run(supportedTests)
