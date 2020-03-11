2e1a3c9ac551230dbbaf18a2a884296bdc493523
kyrylo <kyrylo.onufriiev@applitools.com>
2020-03-11 15:50:27 +0200

    refactor initPage

---
+++ packages/eyes-storybook/src/initPage.js
@@ -0,0 +1,48 @@
+const {presult} = require('@applitools/functional-commons');
+const browserLog = require('./browserLog');
+
+function makeInitPage({iframeUrl, config, browser, logger}) {
+  return async function initPage({pageId, pagePool}) {
+    logger.log('initializing puppeteer page number ', pageId);
+    const page = await browser.newPage();
+    if (config.viewportSize) {
+      await page.setViewport(config.viewportSize);
+    }
+    if (config.showLogs) {
+      browserLog({
+        page,
+        onLog: text => {
+          if (text.match(/\[dom-snapshot\]/)) {
+            logger.log(`tab ${pageId}: ${text}`);
+          }
+        },
+      });
+    }
+    page.on('error', async err => {
+      logger.log(`Puppeteer error for page ${pageId}:`, err);
+      pagePool.removePage(pageId);
+      const {pageId} = await pagePool.createPage();
+      pagePool.addToPool(pageId);
+    });
+    page.on('close', async () => {
+      if (pagePool.isInPool(pageId)) {
+        logger.log(
+          `Puppeteer page closed [page ${pageId}] while still in page pool, creating a new one instead`,
+        );
+        pagePool.removePage(pageId);
+        const {pageId} = await pagePool.createPage();
+        pagePool.addToPool(pageId);
+      }
+    });
+    const [err] = await presult(page.goto(iframeUrl, {timeout: config.readStoriesTimeout}));
+    if (err) {
+      logger.log(`error navigating to iframe.html`, err);
+      if (pagePool.isInPool(pageId)) {
+        throw err;
+      }
+    }
+    return page;
+  };
+}
+
+module.exports = makeInitPage;
