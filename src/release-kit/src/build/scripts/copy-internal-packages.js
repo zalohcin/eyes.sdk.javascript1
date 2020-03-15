66bc1187c3b5859a378071b86136cf46c6a8d5f3
David Haeffner <dhaeffner@gmail.com>
2020-03-15 15:20:58 +0200

    added some todo comments

--- src/release-kit/src/build/scripts/copy-internal-packages.js
+++ src/release-kit/src/build/scripts/copy-internal-packages.js
@@ -4,7 +4,19 @@ const packageDir = path.join(process.cwd())
 const packageJson = require(path.join(packageDir, 'package.json'))
 const buildFolder = path.join(packageDir, 'dist')
 
-module.exports = copyInternalPackagesToBuildFolder.bind(undefined, {
-  dependencies: packageJson.dependencies,
-  buildFolder,
-})
+module.exports = () => {
+  // TODO
+  // clean package/dist
+  // mirror package into package/dist (e.g., cp -R package into package/dist)
+  // cd into package/dist and work from here
+  copyInternalPackagesToBuildFolder({
+    dependencies: packageJson.dependencies,
+    buildFolder,
+  })
+  // update package/dist/package.json to use package/dist/dist (!) folder contents
+  // pack
+  // mkdir tmpdir
+  // install pack into tmpdir
+  // check installed package for sanity
+  // rm -rf tmpdir
+}
