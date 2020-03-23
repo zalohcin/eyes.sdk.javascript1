b4e9b7332a67239c37ebc5ad6a441c34088e1b4f
Amit Zur <amit.zur@applitools.com>
2020-03-23 22:53:26 +0200

    fix verify-commits so that it works on dependencies

--- packages/sdk-release-kit/src/versions/scripts/verify-commits.js
+++ packages/sdk-release-kit/src/versions/scripts/verify-commits.js
@@ -5,21 +5,43 @@ const {exec} = require('child_process')
 const {promisify} = require('util')
 const pexec = promisify(exec)
 const chalk = require('chalk')
+const {makePackagesList} = require('..')
+const pkgs = makePackagesList()
 
-module.exports = async pkgPath => {
+async function verifyDependencies(pkgPath) {
+  const packageJson = require(path.resolve(pkgPath, 'package.json'))
+  const {dependencies} = packageJson
+  const workspaceDeps = pkgs.filter(pkg => pkg.name in dependencies)
+  const results = (
+    await Promise.all(
+      workspaceDeps.map(async dep => {
+        const output = await verifyCommits(dep.path)
+        return {name: dep.name, output}
+      }),
+    )
+  ).filter(x => x.output)
+
+  if (results.length) {
+    throw new Error(
+      'There are unreleased commits in dependencies of this package:\n' +
+        results.map(({name, output}) => `${chalk.yellow(name)}\n${chalk.cyan(output)}`).join('\n'),
+    )
+  }
+}
+
+async function verifyCommits(pkgPath) {
   const packageJson = require(path.resolve(pkgPath, 'package.json'))
   const {name, version} = packageJson
   const tagName = `${name}@${version}`
-  let output
   try {
-    output = (await pexec(`git log --oneline ${tagName}..HEAD -- ${pkgPath}`)).stdout
+    return (await pexec(`git log --oneline ${tagName}..HEAD -- ${pkgPath}`)).stdout
   } catch (ex) {
     if (/bad revision/.test(ex.message)) {
       const tagNameCyan = chalk.cyan(tagName)
       const versionCyan = chalk.cyan(version)
       console.log(
         chalk.yellow(
-          `Warning: unable to detect unreleased commits because tag ${tagNameCyan} is missing from git.
+          `Warning [${name}]: unable to detect unreleased commits because tag ${tagNameCyan} is missing from git.
 Please make sure there are no additional commits to this package since the release of version ${versionCyan}!`,
         ),
       )
@@ -27,7 +49,6 @@ Please make sure there are no additional commits to this package since the relea
       throw ex
     }
   }
-  if (output) {
-    throw new Error('There are unreleased commits in this package:\n' + output)
-  }
 }
+
+module.exports = verifyDependencies
