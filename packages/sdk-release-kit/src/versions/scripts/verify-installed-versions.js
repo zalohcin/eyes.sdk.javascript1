500e48a549d727b04ac2a1f87b13234c75c1f1dc
David Haeffner <dhaeffner@gmail.com>
2020-03-25 15:49:46 +0200

    release-kit: cleaned up verify-installed-versions and added a test for it

--- packages/sdk-release-kit/src/versions/scripts/verify-installed-versions.js
+++ packages/sdk-release-kit/src/versions/scripts/verify-installed-versions.js
@@ -1,38 +1,7 @@
 const {exec} = require('child_process')
 const {promisify} = require('util')
 const pexec = promisify(exec)
-
-let errors = []
-
-function checkPackagesForUniqueVersions(input, packageNames) {
-  packageNames.forEach(packageName => {
-    hasUniqueVersionForPackage(input, packageName)
-  })
-  if (errors.length) {
-    const affectedPackages = errors.map(error => error.name).join(', ')
-    throw new Error(`Non-unique package versions found of ${affectedPackages}`)
-  }
-}
-
-function findVersionNumbersForPackage(input, pkgName) {
-  let versionNumbers = []
-  const packageEntries = grep(input, pkgName)
-  packageEntries.forEach(entry => {
-    const versionNumber = entry.match(/(\d+\.)?(\d+\.)?(\*|\d+)/)[0]
-    versionNumbers.push(versionNumber)
-  })
-  return new Set(versionNumbers)
-}
-
-function grep(input, target) {
-  if (typeof input === 'string') input = input.split('\n')
-  return input.filter(entry => entry.includes(target))
-}
-
-function hasUniqueVersionForPackage(input, pkgName) {
-  const versions = findVersionNumbersForPackage(input, pkgName)
-  if (versions.size > 1) errors.push({name: pkgName, versions})
-}
+const {checkPackagesForUniqueVersions} = require('..')
 
 async function npmLs() {
   try {
@@ -43,7 +12,7 @@ async function npmLs() {
   }
 }
 
-async function main(packageNames = ['@applitools/eyes-sdk-core', '@applitools/eyes-common']) {
+async function main(packageNames) {
   checkPackagesForUniqueVersions(await npmLs(), packageNames)
 }
 
