97b2957d584d4421c6d789fac6671bf993f223c1
Artem-applitools <artem.borodavka@applitools.com>
2020-03-27 23:06:34 +0200

    added new tests to WDIO 5 and JS 4

--- packages/eyes-webdriverio-5/test/coverage/custom/TestApiMethods.spec.js
+++ packages/eyes-webdriverio-5/test/coverage/custom/TestApiMethods.spec.js
@@ -1,17 +1,16 @@
 'use strict'
-const {By} = require('selenium-webdriver')
 const {getDriver, getEyes, getBatch} = require('./util/TestSetup')
 const {Target, StitchMode} = require('../../../index')
 const batch = getBatch()
 describe('api methods', () => {
-  let webDriver, eyes, runner
+  let browser, eyes, runner
   afterEach(async function() {
-    await webDriver.quit()
+    await browser.deleteSession()
     await eyes.abortIfNotClosed()
   })
   describe('classic', function() {
     beforeEach(async function() {
-      webDriver = await getDriver('CHROME')
+      browser = await getDriver('CHROME')
       ;({eyes, runner} = await getEyes('classic', StitchMode.CSS))
       eyes.setBatch(batch)
     })
@@ -19,7 +18,7 @@ describe('api methods', () => {
   })
   describe('visualGrid', function() {
     beforeEach(async function() {
-      webDriver = await getDriver('CHROME')
+      browser = await getDriver('CHROME')
       ;({eyes, runner} = await getEyes('VG'))
       eyes.setBatch(batch)
     })
@@ -27,16 +26,17 @@ describe('api methods', () => {
   })
 
   async function testCloseAsync() {
-    await webDriver.get('https://applitools.com/helloworld')
-    await eyes.open(webDriver, 'TestApiMethods', `TestCloseAsync_1`, {
+    await browser.url('https://applitools.com/helloworld')
+    await eyes.open(browser, 'TestApiMethods', `TestCloseAsync_1`, {
       width: 800,
       height: 600,
     })
     await eyes.check('step 1', Target.window())
     await eyes.closeAsync()
 
-    await webDriver.findElement(By.css('button')).click()
-    await eyes.open(webDriver, 'TestApiMethods', `TestCloseAsync_2`, {
+    let button = await browser.$('button')
+    await button.click()
+    await eyes.open(browser, 'TestApiMethods', `TestCloseAsync_2`, {
       width: 800,
       height: 600,
     })
