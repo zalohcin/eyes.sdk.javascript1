900067773c3231d208619aff1912d551c6bdfefc
Daniel Schwartz <danielsch24@gmail.com>
2020-04-01 12:22:27 +0300

    added override key warning

--- packages/eyes-testcafe/test/util/assert-js-key.js
+++ packages/eyes-testcafe/test/util/assert-js-key.js
@@ -5,3 +5,8 @@ if (!process.env.APPLITOOLS_API_KEY_JSSDK) {
   console.log(chalk.red('Missing APPLITOOLS_API_KEY_JSSDK!'))
   process.exit(1)
 }
+console.log(
+  chalk.yellow(
+    `Overriding APPLITOOLS_API_KEY (${process.env.APPLITOOLS_API_KEY}) with APPLITOOLS_API_KEY_JSSDK.`,
+  ),
+)
