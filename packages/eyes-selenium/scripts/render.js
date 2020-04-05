09423672fe69b106d225c831665069e8037679f5
Amit Zur <amit.zur@applitools.com>
2020-04-05 07:52:25 +0300

    eyes-selenium: improve render script

--- packages/eyes-selenium/scripts/render.js
+++ packages/eyes-selenium/scripts/render.js
@@ -1,18 +1,18 @@
-/* eslint-disable no-unused-vars */
 'use strict'
 
-require('chromedriver')
-const {Builder, Capabilities} = require('selenium-webdriver')
-const {Options: ChromeOptions} = require('selenium-webdriver/chrome')
+const {URL} = require('url')
+const {Builder} = require('selenium-webdriver')
 const {
   Eyes,
   ClassicRunner,
   Target,
-  ConsoleLogHandler,
+  FileLogHandler,
   Configuration,
-  BatchInfo,
-  FileDebugScreenshotsProvider,
+  StitchMode,
+  // BatchInfo,
+  // FileDebugScreenshotsProvider,
 } = require('../index')
+const path = require('path')
 
 const url = process.argv[2]
 if (!url) {
@@ -21,27 +21,95 @@ if (!url) {
 
 ;(async function() {
   console.log('Running Selenium render for', url, '\n')
-  const driver = await new Builder()
-    .withCapabilities(Capabilities.chrome())
-    .setChromeOptions(new ChromeOptions().headless())
-    .build()
+  const driver = await buildDriver({headless: true})
 
   const runner = new ClassicRunner()
   const eyes = new Eyes(runner)
   const configuration = new Configuration({
     viewportSize: {width: 1024, height: 768},
+    stitchMode: StitchMode.CSS,
   })
   eyes.setConfiguration(configuration)
-
-  if (process.env.APPLITOOLS_SHOW_LOGS) {
-    eyes.setLogHandler(new ConsoleLogHandler(true))
-  }
+  const {logger, logFilePath} = initLog(eyes, new URL(url).hostname.replace(/\./g, '-'))
+  logger.log('Running Selenium render for', url)
+  console.log('log file at:', logFilePath)
 
   await driver.get(url)
-  await eyes.open(driver, 'selenium render', 'selenium render')
-  await eyes.check('selenium render', Target.window())
+  await eyes.open(driver, 'selenium render', url)
+  await eyes.check('selenium render', Target.window().fully())
   await eyes.close(false)
 
-  const results = await runner.getAllTestResults(false)
-  console.log('\nRender results\n\n', results.getAllResults().toString())
+  const testResultsSummary = await runner.getAllTestResults(false)
+  const resultsStr = testResultsSummary
+    .getAllResults()
+    .map(testResultContainer => {
+      const testResults = testResultContainer.getTestResults()
+      return testResults ? formatResults(testResults) : testResultContainer.getException()
+    })
+    .join('\n')
+
+  console.log('\nRender results\n\n', resultsStr)
 })()
+
+function buildDriver({headless} = {}) {
+  return (
+    new Builder()
+      .withCapabilities({
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+          args: headless ? ['--headless'] : [],
+        },
+      })
+      // .usingServer('http://localhost.charlesproxy.com:9515')
+      // .usingWebDriverProxy('http://localhost:8888')
+      .build()
+  )
+}
+
+function initLog(eyes, filename) {
+  const logFilePath = path.resolve(__dirname, `../logs/${filename}-${formatDate(new Date())}.log`)
+  const logHandler = new FileLogHandler(true, logFilePath)
+  logHandler.open()
+  eyes.setLogHandler(logHandler)
+  return {logger: eyes.getLogger(), logFilePath}
+}
+
+function formatResults(testResults) {
+  return `
+Test name                 : ${testResults.getName()}
+Test status               : ${testResults.getStatus()}
+URL to results            : ${testResults.getUrl()}
+Total number of steps     : ${testResults.getSteps()}
+Number of matching steps  : ${testResults.getMatches()}
+Number of visual diffs    : ${testResults.getMismatches()}
+Number of missing steps   : ${testResults.getMissing()}
+Display size              : ${testResults.getHostDisplaySize().toString()}
+Steps                     :
+${testResults
+  .getStepsInfo()
+  .map(step => {
+    return `  ${step.getName()} - ${getStepStatus(step)}`
+  })
+  .join('\n')}`
+}
+
+function getStepStatus(step) {
+  if (step.getIsDifferent()) {
+    return 'Diff'
+  } else if (!step.getHasBaselineImage()) {
+    return 'New'
+  } else if (!step.getHasCurrentImage()) {
+    return 'Missing'
+  } else {
+    return 'Passed'
+  }
+}
+
+function formatDate(d) {
+  const month = String(d.getMonth() + 1).padStart(2, '0')
+  const date = String(d.getDate()).padStart(2, '0')
+  const hours = String(d.getHours()).padStart(2, '0')
+  const minutes = String(d.getMinutes()).padStart(2, '0')
+  const seconds = String(d.getSeconds()).padStart(2, '0')
+  return `${d.getFullYear()}-${month}-${date}-${hours}-${minutes}-${seconds}`
+}
