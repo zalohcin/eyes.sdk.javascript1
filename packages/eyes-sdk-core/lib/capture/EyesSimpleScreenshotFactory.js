32c77f742a75f7ea50bf312a4816d5cb0953ff0e
kyrylo <kyrylo.onufriiev@applitools.com>
2020-04-29 23:29:24 +0300

    fix eyes screenshot factory

--- packages/eyes-sdk-core/lib/capture/EyesSimpleScreenshotFactory.js
+++ packages/eyes-sdk-core/lib/capture/EyesSimpleScreenshotFactory.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {EyesScreenshotFactory} = require('./EyesScreenshotFactory')
+const EyesScreenshotFactory = require('./EyesScreenshotFactory')
 const {EyesSimpleScreenshot} = require('./EyesSimpleScreenshot')
 
 /**
