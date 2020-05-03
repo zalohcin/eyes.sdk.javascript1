6dbf2e71d407ab5fbb62dfafd2a53f6ac3b69ea7
David Haeffner <dhaeffner@gmail.com>
2020-05-03 15:25:17 +0300

    wdio4, wdio5, eyes-selenium (ct): added --remote support to coverage-test scripts through env var (CVG_TEST_REMOTE)

--- packages/eyes-webdriverio-4/scripts/start-chromedriver.js
+++ packages/eyes-webdriverio-4/scripts/start-chromedriver.js
@@ -1,7 +1,7 @@
 const chromedriver = require('chromedriver')
 
 module.exports = async () => {
-  if (!process.env.SKIP_CHROMEDRIVER) {
+  if (!process.env.SKIP_CHROMEDRIVER || process.env.CVG_TEST_REMOTE) {
     const returnPromise = true
     await chromedriver.start(['--port=4444', '--url-base=wd/hub', '--silent'], returnPromise)
   }
