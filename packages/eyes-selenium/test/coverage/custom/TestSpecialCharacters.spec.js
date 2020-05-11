2ca6e9fc8b4cba81186fc452e013feea7d61e7a2
Daniel Schwartz <danielsch24@gmail.com>
2020-05-11 19:27:03 +0300

    eyes-selenium: fixed linting

--- packages/eyes-selenium/test/coverage/custom/TestSpecialCharacters.spec.js
+++ packages/eyes-selenium/test/coverage/custom/TestSpecialCharacters.spec.js
@@ -25,6 +25,6 @@ describe(appName, () => {
     await webDriver.get('https://applitools.github.io/demo/TestPages/SpecialCharacters/index.html')
     await eyes.check('Test Special Characters', Target.window().fully())
     await eyes.close()
-    let allResults = await runner.getAllTestResults()
+    await runner.getAllTestResults()
   })
 })
