b10b6e57bc12faaaa439d71fe18a27cffa33045f
Amit Zur <amit.zur@applitools.com>
2020-06-03 08:38:59 +0300

    dc: another way for preversion

--- packages/dom-capture/scripts/pre-version.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-const {exec} = require('child_process');
-const {promisify} = require('util');
-const pexec = promisify(exec);
-
-(async () => {
-  await pexec('yarn build');
-  await pexec('yarn test');
-})();
