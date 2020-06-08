f1004a301abcb866a4dbf3a9f77c4c9b30f9df49
Amit Zur <amit.zur@applitools.com>
2020-06-08 14:25:50 +0300

    Selenium 3 SpecWrappedElement facade

--- packages/eyes-selenium/src/SeleniumWrappedElement.js
+++ packages/eyes-selenium/src/SeleniumWrappedElement.js
@@ -1,8 +1,5 @@
 const {EyesWrappedElement} = require('@applitools/eyes-sdk-core')
-const SpecWrappedElement =
-  process.env.SELENIUM_MAJOR_VERSION === '3'
-    ? require('./selenium3/SpecWrappedElement')
-    : require('./selenium4/SpecWrappedElement')
+const SpecWrappedElement = require('./SpecWrappedElement')
 
 const SeleniumWrappedElement = EyesWrappedElement.specialize(SpecWrappedElement)
 
