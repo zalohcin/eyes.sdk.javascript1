94e6382849bb30489bea5e4ad78a7438a3039f79
Amit Zur <amit.zur@applitools.com>
2020-06-09 06:41:10 +0300

    fix device info for ios devices and new chrome emulation format

--- packages/visual-grid-client/src/sdk/getDeviceInfoFromBrowserConfig.js
+++ packages/visual-grid-client/src/sdk/getDeviceInfoFromBrowserConfig.js
@@ -1,12 +1,15 @@
 'use strict'
 
 function getDeviceInfoFromBrowserConfig(browser) {
-  if (browser.deviceName) {
-    return `${browser.deviceName} (Chrome emulation)`
+  const chromeEmulationDeviceName =
+    browser.deviceName || (browser.chromeEmulationInfo && browser.chromeEmulationInfo.deviceName)
+
+  if (chromeEmulationDeviceName) {
+    return `${chromeEmulationDeviceName} (Chrome emulation)`
   }
 
   if (browser.iosDeviceInfo) {
-    return browser.iosDeviceInfo.name
+    return browser.iosDeviceInfo.deviceName
   }
 
   return 'Desktop'
