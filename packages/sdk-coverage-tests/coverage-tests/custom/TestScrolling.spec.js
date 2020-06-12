8fed1451eba436868b8bea2d10f5b463e4f21b5d
artem <artem.borodavka@applitools.com>
2020-06-12 17:36:08 +0300

    move testScrolling to the sdk-test-coverage

--- packages/sdk-coverage-tests/coverage-tests/custom/TestScrolling.spec.js
+++ packages/sdk-coverage-tests/coverage-tests/custom/TestScrolling.spec.js
@@ -1,16 +1,24 @@
 'use strict'
-const {getEyes} = require('./util/TestSetup')
-const {EyesWebElement, Target, Region, StitchMode} = require('../../../index')
-const {Builder, By} = require('selenium-webdriver')
+const cwd = process.cwd()
+const path = require('path')
+const {getEyes} = require('../util/TestSetup')
+const spec = require(path.resolve(cwd, 'src/SpecWrappedDriver'))
+const {RectangleSize, Target, Region} = require(cwd)
 const appName = 'TestScrolling'
 
 describe(appName, () => {
   describe('ChromeEmulation', () => {
-    let eyes, driver
+    let eyes, driver, args
+
+    beforeEach(async () => {
+      args = []
+      if (!process.env.NO_HEADLESS) args.push('headless')
+    })
 
     it.skip('TestWebAppScrolling', async () => {
-      driver = await new Builder()
-        .withCapabilities({
+      args.push('--window-size=360,740')
+      driver = await spec.build({
+        capabilities: {
           browserName: 'chrome',
           'goog:chromeOptions': {
             mobileEmulation: {
@@ -18,20 +26,27 @@ describe(appName, () => {
               userAgent:
                 'Mozilla/5.0 (Linux; Android 8.0.0; SM-G960F Build/R16NW) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.137 Mobile Safari/537.36',
             },
-            args: ['--window-size=360,740', 'headless'],
+            args: args,
           },
-        })
-        .build()
+        },
+      })
       try {
-        await driver.get('https://applitools.github.io/demo/TestPages/MobileDemo/adaptive.html')
-        eyes = getEyes('classic')
-        let eyesDriver = await eyes.open(driver, appName, `TestWebAppScrolling`, {
+        await spec.visit(
+          driver,
+          'https://applitools.github.io/demo/TestPages/MobileDemo/adaptive.html',
+        )
+        eyes = getEyes({isCssStitching: true})
+        await eyes.open(driver, appName, `TestWebAppScrolling`, {
           width: 360,
           height: 740,
         })
-        let element = await driver.findElement(By.css('.content'))
-        let eyesElement = new EyesWebElement(eyes._logger, eyesDriver, element)
-        let size = await eyesElement.getScrollSize()
+        let element = await spec.findElement(driver, '.content')
+        let scrollSize = await spec.executeScript(
+          driver,
+          element => [element.scrollWidth, element.scrollHeight],
+          element,
+        )
+        let size = new RectangleSize(Math.ceil(scrollSize[0]) || 0, Math.ceil(scrollSize[1]) || 0)
         for (let currentPosition = 0; currentPosition < size.getHeight(); currentPosition += 6000) {
           let height = Math.min(6000, size.getHeight() - currentPosition)
           await eyes.check(
@@ -44,13 +59,14 @@ describe(appName, () => {
         await eyes.close()
       } finally {
         await eyes.abortIfNotClosed()
-        await driver.quit()
+        await spec.cleanup(driver)
       }
     })
 
     it('TestWebAppScrolling2', async () => {
-      driver = await new Builder()
-        .withCapabilities({
+      args.push('--window-size=386,512')
+      driver = await spec.build({
+        capabilities: {
           browserName: 'chrome',
           'goog:chromeOptions': {
             mobileEmulation: {
@@ -58,26 +74,29 @@ describe(appName, () => {
               userAgent:
                 'Mozilla/5.0 (Linux; Android 7.1.1; Nexus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
             },
-            args: ['--window-size=386,512' /*, 'headless'*/],
+            args: args,
           },
-        })
-        .build()
+        },
+      })
       try {
-        await driver.get('https://applitools.github.io/demo/TestPages/MobileDemo/AccessPayments/')
-        eyes = getEyes('classic')
+        await spec.visit(
+          driver,
+          'https://applitools.github.io/demo/TestPages/MobileDemo/AccessPayments/',
+        )
+        eyes = getEyes({isCssStitching: true})
         await eyes.open(driver, appName, 'TestWebAppScrolling2', {width: 386, height: 512})
-        eyes.setStitchMode(StitchMode.CSS)
         await eyes.check('big page on mobile', Target.window().fully())
         await eyes.close()
       } finally {
         await eyes.abortIfNotClosed()
-        await driver.quit()
+        await spec.cleanup(driver)
       }
     })
 
     it('TestWebAppScrolling3', async () => {
-      driver = await new Builder()
-        .withCapabilities({
+      args.push('--window-size=386,512')
+      driver = await spec.build({
+        capabilities: {
           browserName: 'chrome',
           'goog:chromeOptions': {
             mobileEmulation: {
@@ -85,24 +104,26 @@ describe(appName, () => {
               userAgent:
                 'Mozilla/5.0 (Linux; Android 7.1.1; Nexus 9) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
             },
-            args: ['--window-size=386,512', 'headless'],
+            args: args,
           },
-        })
-        .build()
+        },
+      })
       try {
-        await driver.get('https://www.applitools.com/customers')
-        eyes = getEyes('classic')
+        await spec.visit(driver, 'https://www.applitools.com/customers')
+        eyes = getEyes()
+        eyes.setBranchName('default')
         await eyes.open(driver, appName, 'TestWebAppScrolling3', {width: 386, height: 512})
+        let el = await spec.findElement(driver, 'main#site-main')
         await eyes.check(
           'long page on mobile',
-          Target.region(By.css('div.page'))
+          Target.region(el)
             .fully(false)
             .sendDom(false),
         )
         await eyes.close()
       } finally {
         await eyes.abortIfNotClosed()
-        await driver.quit()
+        await spec.cleanup(driver)
       }
     })
   })
