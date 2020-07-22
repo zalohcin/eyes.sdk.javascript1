98f397e867c559ea2ba4ead1bd2b6275ea1e102f
Amit Zur <amit.zur@applitools.com>
2020-07-22 08:12:39 +0300

    cypress: fix lint

--- packages/eyes-cypress/test/fixtures/testApp/cypress/plugins/index-run.js
+++ packages/eyes-cypress/test/fixtures/testApp/cypress/plugins/index-run.js
@@ -1,4 +1,4 @@
 module.exports = require('./start-test-server');
 
-// eslint-disable-next-line node/no-unpublished-require
+// eslint-disable-next-line node/no-unpublished-require,node/no-missing-require
 require('../../../../../..')(module);
