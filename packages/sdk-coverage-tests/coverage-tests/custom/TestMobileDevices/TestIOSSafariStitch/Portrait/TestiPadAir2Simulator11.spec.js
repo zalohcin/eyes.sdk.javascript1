24105e11249785f09aed11ecada7de731005ea2f
artem <artem.borodavka@applitools.com>
2020-07-24 16:55:03 +0300

    small fix and unskip

--- packages/sdk-coverage-tests/coverage-tests/custom/TestMobileDevices/TestIOSSafariStitch/Portrait/TestiPadAir2Simulator11.spec.js
+++ packages/sdk-coverage-tests/coverage-tests/custom/TestMobileDevices/TestIOSSafariStitch/Portrait/TestiPadAir2Simulator11.spec.js
@@ -2,15 +2,15 @@
 const {getMobileEmulation, testMobileDevices, iPadAgent10} = require('../../TestMobileDevices')
 let device = {
   mobileEmulation: getMobileEmulation(iPadAgent10, 768, 954, 2),
-  name: 'iPad Air 2 Simulator 11.3',
+  name: 'iPad Air 2 Simulator 11.0',
   orientation: 'Portrait',
 }
-describe.skip(`${device.name} Portrait`, () => {
+describe(`${device.name} Portrait`, () => {
   describe(`mobile`, () => {
     it('TestIOSSafariStitch', testMobileDevices(device, 'mobile'))
   })
   describe(`desktop`, () => {
-    it('TestIOSSafariStitch', testMobileDevices(device, 'desktop'))
+    it.skip('TestIOSSafariStitch', testMobileDevices(device, 'desktop'))
   })
   describe(`scrolled_mobile`, () => {
     it('TestIOSSafariStitch', testMobileDevices(device, 'scrolled_mobile'))
