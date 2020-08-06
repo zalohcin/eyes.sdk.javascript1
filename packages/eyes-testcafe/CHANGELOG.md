cd1e672af1fde15b9c461c7931164097b562bd5f
tourdedave <dave.haeffner@applitools.com>
2020-08-06 14:52:45 -0400

    testcafe: reboot!

--- packages/eyes-testcafe/CHANGELOG.md
+++ packages/eyes-testcafe/CHANGELOG.md
@@ -1,46 +1,4 @@
 # Change Log
 
-## Unreleased 
+## Unreleased
 
-- removed "source" attribute from VG checkWindow
-- removed eyes-common dependency
-- fix returned value from `close` method ([Trello](https://trello.com/c/m6K2Ftd5/277-wdio5-difficulty-getting-test-results-object))
-- updated to @applitools/dom-utils@4.7.16
-- updated to @applitools/eyes-sdk-core@10.1.2
-- updated to @applitools/visual-grid-client@14.0.1
-
-## 2.0.0-beta.21
-
-- fix missing device info in dashboard  
-
-## 2.0.0-beta.20
-
-- fix wrong browser and OS names when running emulated devices 
-
-## 2.0.0-beta.19
-
-- fix failure to check chrome emulated devices 
-
-## 2.0.0-beta.18
-
-- using fixed version numbers    for all internal dependencies
-- reverted to 2.0.0-beta.12 (not using eyes-sdk-core of Eyes 10.9 release)
-
-## 2.0.0-beta.17
-
-- fix version name, same version as 2.0.0-beta.16
-
-## 2.0.0-beta.16
-
-- fix trying to fetch branch info from server on non github integration runs
-- upload domsnapshot directly to Azure [Trello](https://trello.com/c/ZCLJo8Fy/241-upload-dom-directly-to-azure)
-- support future long running tasks [Trello](https://trello.com/c/60Rm4xXG/240-support-future-long-running-tasks)
-- fix regression in css stitching [Trello](https://trello.com/c/dp5IIoFw/235-css-stitching-regression-in-41533)
-
-## [2.0.0-beta.15]
-
-make function is runners
-
-## [2.0.0-beta.14]
-
-Branching base commit support
---
+++ packages/eyes-testcafe/src/SpecDriver.js
@@ -0,0 +1,53 @@
+async function isDriver() {}
+async function isElement() {}
+async function isSelector() {}
+async function toEyesSelector() {}
+async function isEqualElements() {}
+async function isStaleElementError() {}
+async function executeScript() {}
+async function mainContext() {}
+async function parentContext() {}
+async function childContext() {}
+async function findElement() {}
+async function findElements() {}
+async function getElementRect() {}
+async function getViewportSize() {}
+async function setViewportSize() {}
+
+async function getTitle() {}
+async function getUrl() {}
+async function visit() {}
+async function takeScreenshot() {}
+async function click() {}
+async function type() {}
+async function waitUntilDisplayed() {}
+
+async function build() {}
+async function cleanup() {}
+
+exports.isDriver = isDriver
+exports.isElement = isElement
+exports.isSelector = isSelector
+exports.toEyesSelector = toEyesSelector
+exports.isEqualElements = isEqualElements
+exports.isStaleElementError = isStaleElementError
+exports.executeScript = executeScript
+exports.mainContext = mainContext
+exports.parentContext = parentContext
+exports.childContext = childContext
+exports.findElement = findElement
+exports.findElements = findElements
+exports.getElementRect = getElementRect
+exports.getViewportSize = getViewportSize
+exports.setViewportSize = setViewportSize
+
+exports.getTitle = getTitle
+exports.getUrl = getUrl
+exports.visit = visit
+exports.takeScreenshot = takeScreenshot
+exports.click = click
+exports.type = type
+exports.waitUntilDisplayed = waitUntilDisplayed
+
+exports.build = build
+exports.cleanup = cleanup
