5e0e955fe14c9bcc39c40fdce128cb4f31bf71e2
tourdedave <dave.haeffner@applitools.com>
2020-08-10 11:19:06 -0400

    tc: updated notes and todos

--- packages/eyes-testcafe/src/SpecDriver.js
+++ packages/eyes-testcafe/src/SpecDriver.js
@@ -1,26 +1,38 @@
 const {TypeUtils} = require('@applitools/eyes-sdk-core')
 
 async function isDriver(driver) {
-  return TypeUtils.instanceOf(driver, 'TestCafe') // TODO: confirm this
+  // TODO: confirm this
+  return TypeUtils.instanceOf(driver, 'TestCafe')
 }
 async function isElement(_element) {
-  // TBD
+  // TODO: determine way to identify a resolved Selector
+  // - DOMNodeState? https://devexpress.github.io/testcafe/documentation/reference/test-api/domnodestate.html
 }
 async function isSelector(_selector) {
-  // TBD
+  // TODO:
+  // - if string, true
+  // - else if Selector object, true
+  // - else false
 }
-async function toEyesSelector(_selector) {}
-async function isEqualElements(_driver, _element1, _element2) {}
+async function toEyesSelector(_selector) {
+  // TODO:
+  // - parse out selector string value
+  // - convert to object structure:
+  //  selector -> {type: 'css' | 'xpath', selector: string}
+  // - return {selector}
+}
+async function isEqualElements(_driver, _element1, _element2) {
+  // TODO
+}
 async function isStaleElementError(_error) {
+  // TODO: confirm, but
   // supposedly not a thing in TestCafe
   // https://codecept.io/testcafe/
   // haven't found anything about it in the TestCafe docs/KB
   return false
-  // TODO: confirm
 }
 async function executeScript(_driver, _script, ..._args) {
   // TODO: ClientFunction
-  // also needs the test context, which is the driver
 }
 async function mainContext(driver) {
   await driver.switchToMainWindow()
@@ -35,36 +47,40 @@ async function findElement(_driver, selector) {
   return await selector
 }
 async function findElements(_driver, _selector) {
-  // TODO: research
-  // could do a ClientFunction w/ document.querySelectorAll
-  // need to sort out the return type needed
+  // TODO: ClientFunction w/ document.querySelectorAll, return array of what's resolved
 }
 async function getElementRect(_driver, _element) {}
-//NOTE: omitting this since it can be addressed elsewhere
-//async function getWindowRect(_driver) {
-//}
 async function setWindowRect(driver, rect = {}) {
   const {width, height} = rect
   await driver.resizeWindow(width, height)
 }
 async function getTitle(_driver) {
+  // TODO:
   // accessible through a ClientFunction
 }
 async function getUrl(_driver) {
+  // TODO:
   // accessible through a ClientFunction
   // https://testcafe-discuss.devexpress.com/t/how-do-you-validate-url-in-testcafe/640
-  // TODO: wire it up
 }
 async function visit(driver, url) {
   await driver.navigateTo(url)
 }
 async function takeScreenshot(driver) {
+  // TODO: sort out what this function give us
+  // and what's needed to consume/use it (e.g., read file from disk? etc.)
   // https://devexpress.github.io/testcafe/documentation/guides/advanced-guides/screenshots-and-videos.html#screenshots
   await driver.takeScreenshot()
 }
 async function click(driver, element) {
-  // need a better understanding of what 'element' is in this context
+  // NOTE:
+  // supposedly works with all options, e.g.,:
+  // 1. selector string
+  // 2. Selector object
+  // 3. resolved Selector object (e.g., DOM Node snapshot/state)
+  // 4. return from a ClientFunction that uses document.querySelector*
   // https://devexpress.github.io/testcafe/documentation/reference/test-api/testcontroller/click.html#select-target-elements
+  // My hunch is `element` most closely resembles #3, but we should be able to pass it and ¯\_(ツ)_/¯
   await driver.click(element)
 }
 async function type(driver, element, keys) {
@@ -73,6 +89,7 @@ async function type(driver, element, keys) {
   await driver.typeText(element, keys)
 }
 async function waitUntilDisplayed(_driver, element, timeout) {
+  // NOTE: documented here:
   // https://devexpress.github.io/testcafe/documentation/guides/concepts/built-in-wait-mechanisms.html#wait-mechanism-for-selectors
   await element.with({visibilityCheck: true, timeout})
 }
@@ -96,7 +113,6 @@ exports.childContext = childContext
 exports.findElement = findElement
 exports.findElements = findElements
 exports.getElementRect = getElementRect
-//exports.getWindowRect = getWindowRect
 exports.setWindowRect = setWindowRect
 exports.getTitle = getTitle
 exports.getUrl = getUrl
