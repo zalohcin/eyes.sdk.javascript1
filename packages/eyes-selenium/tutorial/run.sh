024753cf6d21a18941751fe33daf47e35df43715
artem <artem.borodavka@applitools.com>
2020-08-13 17:22:35 +0300

    update dockerfiles and docker-compose.yml for the wdio4, wdio5 and selenium.

--- packages/eyes-selenium/tutorial/run.sh
+++ packages/eyes-selenium/tutorial/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-set -e
-[ -d "./package" ] && rm -r ./package
+
+rm -r ./package
 mkdir package
 cd ..
 yarn pack
 package=$(find applitools*.tgz)
 mv "$package" ./tutorial/package/"$package"
 cd ./tutorial
-docker-compose build $1
+docker-compose build
 docker-compose run js_selenium_basic
 docker-compose run js_selenium3_basic
 docker-compose run js_selenium_ultrafastgrid
--- packages/eyes-webdriverio-4/tutorial/run.sh
+++ packages/eyes-webdriverio-4/tutorial/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 rm -r ./package
 mkdir package
 cd ..
@@ -7,6 +7,6 @@ yarn pack
 package=$(find applitools*.tgz)
 mv "$package" ./tutorial/package/"$package"
 cd ./tutorial
-docker-compose build $1
+docker-compose build
 docker-compose run wdio4_selenium_basic
 docker-compose run wdio4_selenium_ultrafastgrid
