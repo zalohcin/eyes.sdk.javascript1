c5abba2493ea55374b81df8fcca8ff1e6faa9098
tourdedave <dave.haeffner@applitools.com>
2020-08-18 08:11:46 -0400

    tc: streamlined the SpecDriver tests, removing the test helper functions and inlining everything into each test

--- packages/eyes-testcafe/test/it/SpecDriver.spec.js
+++ packages/eyes-testcafe/test/it/SpecDriver.spec.js
@@ -5,53 +5,94 @@ const {Selector} = require('testcafe')
 fixture`SpecDriver`.page`https://applitools.github.io/demo/TestPages/FramesTestPage/`
 
 test('isDriver(driver)', driver => {
-  return isDriver({driver, expected: true})
+  assert.strictEqual(spec.isDriver(driver), true)
 })
-test('isDriver(wrong)', driver => {
-  return isDriver({driver, input: {}, expected: false})
+test('isDriver(wrong)', _driver => {
+  assert.strictEqual(spec.isDriver({}), false)
 })
-test('isSelector(string)', driver => {
-  return isSelector({driver, input: '//div', expected: true})
+test('isElement(Selector)', async _driver => {
+  const element = await Selector('div')()
+  assert.strictEqual(spec.isElement(element), true)
 })
-test('isSelector(Selector)', driver => {
-  return isSelector({driver, input: Selector('//div'), expected: true})
+test('isElement(wrong)', _driver => {
+  assert.strictEqual(spec.isElement({}), false)
 })
-test('isSelector(wrong)', driver => {
-  return isSelector({driver, input: {}, expected: false})
+test('isSelector(string)', _driver => {
+  assert.strictEqual(spec.isSelector('//div'), true)
 })
-test('executeScript(string)', driver => {
-  return executeScript({driver, script: 'return 4', expected: 4})
+test('isSelector(Selector)', _driver => {
+  assert.strictEqual(spec.isSelector(Selector('//div')), true)
 })
-test('executeScript(string, ...args)', driver => {
-  return executeScript({
-    driver,
-    script: 'return arguments[0] + arguments[1]',
-    args: [4, 5],
-    expected: 9,
-  })
+test('isSelector(wrong)', _driver => {
+  assert.strictEqual(spec.isSelector({}), false)
 })
-test('executeScript(function, ...args)', driver => {
+test('findElement(string)', async driver => {
+  const selector = await spec.findElement(driver, '#overflowing-div')
+  assert.strictEqual(spec.isSelector(selector), true)
+})
+test('findElement(Selector)', async driver => {
+  const selector = await spec.findElement(driver, Selector('#overflowing-div'))
+  assert.strictEqual(spec.isSelector(selector), true)
+})
+test('findElement(DOM Node snapshot)', async driver => {
+  const elSnapshot = await Selector('#overflowing-div')
+  const selector = await spec.findElement(driver, elSnapshot)
+  assert.strictEqual(spec.isSelector(selector), true)
+})
+test('findElement(non-existent)', async driver => {
+  const selector = await spec.findElement(driver, 'non-existent')
+  assert.strictEqual(spec.isSelector(selector), false)
+})
+test('findElements(string)', async driver => {
+  const elements = await spec.findElements(driver, 'div')
+  assert.deepStrictEqual(!!elements.length, true)
+})
+test('findElements(Selector)', async driver => {
+  const elements = await spec.findElements(driver, Selector('div'))
+  assert.deepStrictEqual(!!elements.length, true)
+})
+test('findElements(DOM Node snapshot)', async driver => {
+  const elSnapshot = await Selector('div')()
+  const elements = await spec.findElements(driver, elSnapshot)
+  assert.deepStrictEqual(!!elements.length, true)
+})
+test('findElements(non-existent)', async driver => {
+  const elements = await spec.findElements(driver, 'non-existent')
+  assert.deepStrictEqual(!!elements.length, false)
+})
+test('isEqualElements(element, element)', async driver => {
+  const element1 = await Selector('h1')()
+  const element2 = await Selector('h1')()
+  assert.deepStrictEqual(spec.isEqualElements(driver, element1, element2), true)
+})
+test('isEqualElements(element1, element2)', async driver => {
+  const element1 = await Selector('div')()
+  const element2 = await Selector('h1')()
+  assert.deepStrictEqual(spec.isEqualElements(driver, element1, element2), false)
+})
+test('executeScript(string)', async driver => {
+  assert.deepStrictEqual(await spec.executeScript(driver, 'return 4'), 4)
+})
+test('executeScript(string, ...args)', async driver => {
+  assert.deepStrictEqual(
+    await spec.executeScript(driver, 'return arguments[0] + arguments[1]', 4, 5),
+    9,
+  )
+})
+test('executeScript(function, ...args)', async driver => {
   const script = function() {
     return arguments[0] + arguments[1]
   }
-  return executeScript({driver, script, args: [4, 5], expected: 9})
+  assert.deepStrictEqual(await spec.executeScript(driver, script, 4, 5), 9)
 })
-test('executeScript w/ Selector', driver => {
-  return executeScript({
-    driver,
-    script: "return getComputedStyle(arguments[0]).getPropertyValue('overflow')",
-    args: Selector('html'),
-    expected: 'visible',
-  })
+test('executeScript w/ Selector', async driver => {
+  const script = "return getComputedStyle(arguments[0]).getPropertyValue('overflow')"
+  assert.deepStrictEqual(await spec.executeScript(driver, script, Selector('html')), 'visible')
 })
 test('executeScript w/ DOM Node Snapshot', async driver => {
   const elSnapshot = await Selector('html')()
-  return executeScript({
-    driver,
-    script: "return arguments[0].style['overflow-y']",
-    args: elSnapshot,
-    expected: 'visible',
-  })
+  const script = "return arguments[0].style['overflow-y']"
+  assert.deepStrictEqual(await spec.executeScript(driver, script, elSnapshot), 'visible')
 })
 test('executeScript re-use returned element', async driver => {
   const expected = await Selector('h1')()
@@ -62,8 +103,8 @@ test('executeScript re-use returned element', async driver => {
 test('executeScript re-use returned element (when the element changes)', async driver => {
   const expected = 'blah'
   const result = await spec.executeScript(driver, 'return arguments[0]', Selector('h1'))
-  await spec.executeScript(driver, "document.querySelector('h1').textContent = 'blah'")
-  const actual = await spec.executeScript(driver, 'return arguments[0]', Selector(result))
+  await spec.executeScript(driver, `document.querySelector('h1').textContent = '${expected}'`)
+  const actual = await spec.executeScript(driver, 'return arguments[0]', result.selector)
   assert.deepStrictEqual(actual.innerText, expected)
 })
 test('executeScript return mixed data-types (Array)', async driver => {
@@ -82,110 +123,7 @@ test('executeScript return mixed data-types (Object)', async driver => {
   const actual = Object.entries(result).length
   assert.deepStrictEqual(actual, expected)
 })
-test('isElement(Selector)', driver => {
-  return isElement({driver, input: Selector('div'), expected: true})
-})
-test('isElement(wrong)', driver => {
-  return isElement({driver, input: () => ({}), expected: false})
-})
-test('findElement(string)', driver => {
-  return findElement({driver, input: '#overflowing-div'})
-})
-test('findElement(Selector)', driver => {
-  return findElement({driver, input: Selector('#overflowing-div')})
-})
-test('findElement(DOM Node snapshot)', async driver => {
-  const elSnapshot = await Selector('#overflowing-div')
-  findElement({driver, input: elSnapshot})
-})
-test('findElement(non-existent)', driver => {
-  return findElement({driver, input: Selector('non-existent'), expected: false})
-})
-test('findElements(string)', driver => {
-  return findElements({driver, input: 'div'})
-})
-test('findElements(Selector)', driver => {
-  return findElements({driver, input: Selector('div')})
-})
-test('findElements(DOM Node snapshot)', async driver => {
-  const elSnapshot = await Selector('div')()
-  findElements({driver, input: elSnapshot})
-})
-test('findElements(non-existent)', async driver => {
-  findElements({driver, input: Selector('non-existent'), expected: false})
-})
-test('isEqualElements(element, element)', () => {
-  return isEqualElements({
-    input: async () => ({
-      element1: await Selector('h1')(),
-      element2: await Selector('h1')(),
-    }),
-    expected: true,
-  })
-})
-test('isEqualElements(element1, element2)', () => {
-  return isEqualElements({
-    input: async () => ({
-      element1: await Selector('div')(),
-      element2: await Selector('h1')(),
-    }),
-    expected: false,
-  })
-})
-test('mainContext()', driver => {
-  return mainContext({driver})
-})
-test.skip('parentContext()', driver => {
-  return parentContext({driver})
-})
-test.skip('childContext(element)', async driver => {
-  childContext({driver})
-})
-test('getTitle()', driver => {
-  return getTitle({driver})
-})
-test('getUrl()', driver => {
-  return getUrl({driver, expected: 'https://applitools.github.io/demo/TestPages/FramesTestPage/'})
-})
-test('visit()', async driver => {
-  visit({driver})
-})
-test.skip('takeScreenshot()', async _driver => {})
-
-async function isDriver({driver, input, expected}) {
-  const isDriver = await spec.isDriver(input || driver)
-  assert.strictEqual(isDriver, expected)
-}
-async function isElement({_driver, input, expected}) {
-  const element = await input()
-  const isElement = await spec.isElement(element)
-  assert.strictEqual(isElement, expected)
-}
-function isSelector({_driver, input, expected}) {
-  const isSelector = spec.isSelector(input)
-  assert.strictEqual(isSelector, expected)
-}
-async function isEqualElements({driver, input, expected}) {
-  const {element1, element2} = await input()
-  const result = spec.isEqualElements(driver, element1, element2)
-  assert.deepStrictEqual(result, expected)
-}
-async function executeScript({driver, script, args = [], expected}) {
-  const actual =
-    args && args.length
-      ? await spec.executeScript(driver, script, ...args)
-      : await spec.executeScript(driver, script, args)
-  assert.deepStrictEqual(actual, expected)
-}
-async function findElement({driver, input, expected = true} = {}) {
-  const el = await spec.findElement(driver, input)
-  isSelector({input: el, expected})
-}
-async function findElements({driver, input, expected = true} = {}) {
-  const elements = await spec.findElements(driver, input)
-  assert.deepStrictEqual(!!elements.length, expected)
-}
-async function mainContext({driver}) {
+test('mainContext()', async driver => {
   try {
     const mainDocument = await Selector('html')()
     await driver.switchToIframe('[name="frame1"]')
@@ -198,63 +136,34 @@ async function mainContext({driver}) {
   } finally {
     await driver.switchToMainWindow().catch(() => null)
   }
-}
-async function parentContext({_driver}) {
-  //try {
-  //  await driver.switchToIframe('[name="frame1"]')
-  //  const parentDocument = await Selector('html')()
-  //  await driver.expect(parentDocument.visible).ok()
-  //  await driver.switchToIframe('[name="frame1-1"]')
-  //  const frameDocument = await Selector('html')()
-  //  assert.ok(!spec.isEqualElements(driver, parentDocument, frameDocument))
-  //  //await spec.parentContext(driver)
-  //  const elSnapshot = await frameDocument.selector.parent()()
-  //  const r = spec.helpers.extractSelectorString(elSnapshot)
-  //  debugger
-  //  await driver.switchToIframe(elSnapshot)
-  //  const resultDocument = await Selector('html')()
-  //  assert.ok(spec.isEqualElements(driver, resultDocument, parentDocument))
-  //} finally {
-  //  await driver.switchToMainWindow().catch(() => null)
-  //}
-}
-function childContext({_driver}) {
-  return async () => {
-    //  try {
-    //    const element = await driver.findElement(By.css('[name="frame1"]'))
-    //    await driver.switchTo().frame(element)
-    //    const expectedDocument = await driver.findElement(By.css('html'))
-    //    await driver.switchTo().frame(null)
-    //    await spec.childContext(driver, element)
-    //    const resultDocument = await driver.findElement(By.css('html'))
-    //    assert.ok(await spec.isEqualElements(driver, resultDocument, expectedDocument))
-    //  } finally {
-    //    await driver
-    //      .switchTo()
-    //      .frame(null)
-    //      .catch(() => null)
-    //  }
-  }
-}
-async function getTitle({driver}) {
+})
+test.skip('parentContext()', _driver => {
+  //return parentContext({driver})
+})
+test.skip('childContext(element)', _driver => {
+  //return childContext({driver})
+})
+test('getTitle()', async driver => {
   //const expected = 'Hello World'
   //await spec.visit(driver, 'data:text/html,<title>Hello%20World</title><body>blah</body>')
   const expected = 'Cross SDK test'
   const actual = await spec.getTitle(driver)
   assert.deepStrictEqual(actual, expected)
-}
-async function getUrl({driver, expected}) {
+})
+test('getUrl()', async driver => {
+  const expected = 'https://applitools.github.io/demo/TestPages/FramesTestPage/'
   const result = await spec.getUrl(driver)
   assert.deepStrictEqual(result, expected)
-}
-async function visit({driver}) {
+})
+test('visit()', async driver => {
+  let startUrl
   try {
-    const startUrl = await getUrl(driver)
+    startUrl = await spec.getUrl(driver)
     const blank = 'about:blank'
     await spec.visit(driver, blank)
     const actual = await spec.getUrl()
     assert.deepStrictEqual(actual, blank)
   } finally {
-    await spec.visit(startUrl)
+    await spec.visit(driver, startUrl)
   }
-}
+})
