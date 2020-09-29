71b0ccb80b01c33a76673e3917f39b8531fb12f8
kyrylo <kyrylo.onufriiev@applitools.com>
2020-09-29 15:32:31 +0300

    fix render script

--- packages/sdk-shared/scripts/render.js
+++ packages/sdk-shared/scripts/render.js
@@ -408,35 +408,19 @@ function buildDriver({
 } = {}) {
   let env
   if (!envBrowser) {
-    const capabilities = {
-      browserName: 'chrome',
-      'goog:chromeOptions': {
-        // w3c: false,
-        args: headless ? ['--headless'] : [],
-        mobileEmulation: isMobileEmulation ? {deviceName} : undefined,
-        debuggerAddress: attach ? '127.0.01:9222' : undefined,
-      },
-      ...driverCapabilities,
+    if (driverCapabilities) {
+      console.log(
+        'Running with capabilities:\n',
+        Object.entries(driverCapabilities)
+          .map(argToString)
+          .join('\n '),
+        '\n',
+      )
     }
 
-    if (process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY) {
-      capabilities['sauce:options'] = {
-        username: process.env.SAUCE_USERNAME,
-        accesskey: process.env.SAUCE_ACCESS_KEY,
-      }
-    }
-
-    console.log(
-      'Running with capabilities:\n',
-      Object.entries(capabilities)
-        .map(argToString)
-        .join('\n '),
-      '\n',
-    )
-
-    env = {capabilities, serverUrl: driverServer, webdriverProxy}
+    env = {browser: 'chrome', url: driverServer, capabilities: driverCapabilities, attach, headless}
   } else {
-    env = {browser: envBrowser}
+    env = {browser: envBrowser, headless}
   }
 
   return spec.build(env)
