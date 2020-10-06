341c7477f933099e3f7692e9601c54ec79fde9f9
kyrylo <kyrylo.onufriiev@applitools.com>
2020-10-06 08:44:29 +0300

    remove perfecto

--- packages/sdk-shared/src/test-setup.js
+++ packages/sdk-shared/src/test-setup.js
@@ -96,20 +96,6 @@ const DEVICES = {
       ...SAUCE_CREDENTIALS,
     },
   },
-  'perfecto': {
-    url: 'https://testingcloud.perfectomobile.com/nexperience/perfectomobile/wd/hub',
-    capabilities: {
-      'platformName': 'iOS',
-      'platformVersion': '12.2',
-      'location': 'NA-US-BOS',
-      'resolution': '1242x2688',
-      'manufacturer': 'Apple',
-      'model': 'iPhone-XS Max',
-      orientation: 'LANDSCAPE',
-      'waitForAvailableLicense': true,
-      securityToken: 'eyJhbGciOiJIUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICJmZjE2YTM1ZS05ZDg3LTRkMjYtOTEyYS0xZmY0MzgwM2RjNjEifQ.eyJqdGkiOiIxNjI4ZDg5Yi0yMTBlLTRkNTItYWQyMy1mOTZkNzg3ZGI4ZDgiLCJleHAiOjAsIm5iZiI6MCwiaWF0IjoxNjAwODU0NjYzLCJpc3MiOiJodHRwczovL2F1dGgyLnBlcmZlY3RvbW9iaWxlLmNvbS9hdXRoL3JlYWxtcy90ZXN0aW5nY2xvdWQtcGVyZmVjdG9tb2JpbGUtY29tIiwiYXVkIjoiaHR0cHM6Ly9hdXRoMi5wZXJmZWN0b21vYmlsZS5jb20vYXV0aC9yZWFsbXMvdGVzdGluZ2Nsb3VkLXBlcmZlY3RvbW9iaWxlLWNvbSIsInN1YiI6IjQ4OWE1YzhjLTQ0MWQtNGM2NC05ZTU5LTE4YmFlOTY0MDM4NiIsInR5cCI6Ik9mZmxpbmUiLCJhenAiOiJvZmZsaW5lLXRva2VuLWdlbmVyYXRvciIsIm5vbmNlIjoiMzlmZWVlMDktMTQwMi00NDAzLWFmNmEtYWQzN2RmYzk4ODE5IiwiYXV0aF90aW1lIjowLCJzZXNzaW9uX3N0YXRlIjoiZDFjZDkxNDAtYjFhNC00YjI0LWI5ZmYtMTIxZGFkNmJiZDQ3IiwicmVhbG1fYWNjZXNzIjp7InJvbGVzIjpbIm9mZmxpbmVfYWNjZXNzIiwidW1hX2F1dGhvcml6YXRpb24iXX0sInJlc291cmNlX2FjY2VzcyI6eyJhY2NvdW50Ijp7InJvbGVzIjpbIm1hbmFnZS1hY2NvdW50IiwibWFuYWdlLWFjY291bnQtbGlua3MiLCJ2aWV3LXByb2ZpbGUiXX19LCJzY29wZSI6Im9wZW5pZCBvZmZsaW5lX2FjY2VzcyBwcm9maWxlIGVtYWlsIn0.NJvVw77rBObnm9KGRfmM4zy5zOfCPWPpWoPqicLatOg'
-    },
-  },
 }
 
 const BROWSERS = {
