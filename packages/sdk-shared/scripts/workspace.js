20a03093da0a75ce4a1d3f1242052e8d1304647d
Amit Zur <amit.zur@applitools.com>
2020-10-14 23:06:02 +0300

    WIP scripts

--- packages/sdk-shared/scripts/workspace.js
+++ packages/sdk-shared/scripts/workspace.js
@@ -1,47 +1,14 @@
 'use strict'
 
-const path = require('path')
+const forEachPackage = require('../src/for-each-package')
 const {sh} = require('../src/process-commons')
-const {presult} = require('@applitools/functional-commons')
 
 ;(async function main() {
   const commands = process.argv.slice(2)
-  const folders = [
-    'sdk-shared',
-    'sdk-fake-eyes-server',
-    'sdk-release-kit',
-    'sdk-coverage-tests',
-    'snippets',
-    'eyes-sdk-core',
-    'visual-grid-client',
-    'eyes-selenium',
-    'eyes-webdriverio-5',
-    'eyes-webdriverio-4',
-    'eyes-protractor',
-    'eyes-playwright',
-    'eyes-cypress',
-    'eyes-storybook',
-    'eyes-webdriverio-5-service',
-  ]
-  const errors = []
-  const start = Date.now()
-  for (const [index, folder] of folders.entries()) {
-    console.log('[workspace]', folder, `(${index + 1} of ${folders.length})`)
-    process.chdir(path.resolve(__dirname, '../..', folder))
-    const [err] = await presult(
-      commands.reduce((p, cmd) => p.then(() => sh(cmd)), Promise.resolve()),
-    )
-    if (err) errors.push({folder, err})
-  }
-  const total = Date.now() - start
+  await forEachPackage(runCommands)
 
-  if (errors.length) {
-    console.log(`[workspace] done with ${errors.length} errors. Total time: ${total}ms`)
-    for (const [index, {folder, err}] of errors.entries()) {
-      console.log(`[workspace] Error ${index + 1} ${folder}:`, err)
-    }
-  } else {
-    console.log(`[workspace] done without errors! Total time: ${total}ms`)
+  function runCommands() {
+    return commands.reduce((p, cmd) => p.then(() => sh(cmd)), Promise.resolve())
   }
 })().catch(err => {
   console.log(err)
