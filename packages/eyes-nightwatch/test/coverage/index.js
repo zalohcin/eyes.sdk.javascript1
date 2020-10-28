06e2f970cea8ed4d9be6d130f30fccd5f5874c75
tourdedave <dave.haeffner@applitools.com>
2020-10-28 13:34:06 -0400

    nw: skip non-chrome coverage tests until programmatic building is implemented

--- packages/eyes-nightwatch/test/coverage/index.js
+++ packages/eyes-nightwatch/test/coverage/index.js
@@ -7,5 +7,17 @@ module.exports = {
   ext: '.spec.js',
   initializeSdk,
   testFrameworkTemplate,
-  overrideTests,
+  overrideTests: {
+    ...overrideTests,
+    'check region by native selector': {skip: true},
+    'check region by selector on ie': {skip: true},
+    'check window after manual scroll on safari 11': {skip: true},
+    'check window after manual scroll on safari 12': {skip: true},
+    'check window fully on android chrome emulator on mobile page': {skip: true},
+    'check window fully on android chrome emulator on mobile page with horizontal scroll': {
+      skip: true,
+    },
+    'check window fully on page with horizontal scroll with css stitching': {skip: true},
+    'check window fully on page with horizontal scroll with scroll stitching': {skip: true},
+  },
 }
