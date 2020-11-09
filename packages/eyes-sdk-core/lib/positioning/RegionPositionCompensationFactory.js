d97a3b6fb759fda66a67c4014e32c62f8f0a473a
Tal Hayut <tal.hayut@applitools.com>
2020-11-09 09:31:50 +0200

    fix: remove firefox position compensation (#291)

--- packages/eyes-sdk-core/lib/positioning/RegionPositionCompensationFactory.js
+++ packages/eyes-sdk-core/lib/positioning/RegionPositionCompensationFactory.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const BrowserNames = require('../useragent/BrowserNames')
-const FirefoxRegionPositionCompensation = require('./FirefoxRegionPositionCompensation')
 const NullRegionPositionCompensation = require('./NullRegionPositionCompensation')
 const SafariRegionPositionCompensation = require('./SafariRegionPositionCompensation')
 
@@ -14,15 +13,7 @@ class RegionPositionCompensationFactory {
    */
   static getRegionPositionCompensation(userAgent, eyes, logger) {
     if (userAgent) {
-      if (userAgent.getBrowser() === BrowserNames.Firefox) {
-        try {
-          if (parseInt(userAgent.getBrowserMajorVersion(), 10) >= 48) {
-            return new FirefoxRegionPositionCompensation(eyes, logger)
-          }
-        } catch (err) {
-          return new NullRegionPositionCompensation()
-        }
-      } else if (userAgent.getBrowser() === BrowserNames.Safari) {
+      if (userAgent.getBrowser() === BrowserNames.Safari) {
         return new SafariRegionPositionCompensation()
       }
     }
