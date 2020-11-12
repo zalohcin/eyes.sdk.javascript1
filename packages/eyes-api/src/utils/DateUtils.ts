b57c1d5c5cd0bc1fa17ad69f3e9c5053eddd0708
Tal Hayut <tal.hayut@applitools.com>
2020-11-12 16:49:49 +0200

    chore: remove unneeded functions and deps

--- packages/eyes-api/src/utils/DateUtils.ts
+++ packages/eyes-api/src/utils/DateUtils.ts
@@ -1,25 +1,3 @@
-'use strict'
-
-import * as dateFormat from 'dateformat';
-
-enum DateFormats {
-    DATE_FORMAT_ISO8601 = "yyyy-mm-dd'T'HH:MM:ss'Z'",
-    DATE_FORMAT_RFC1123 = "ddd, dd mmm yyyy HH:MM:ss 'GMT'",
-    DATE_FORMAT_LOGFILE = "yyyy_mm_dd__HH_MM_ss_l"
-}
-
-export function toISO8601DateTime(date: Date = new Date()): string {
-    return dateFormat(date, DateFormats.DATE_FORMAT_ISO8601, true);
-}
-
-export function toRfc1123DateTime(date: Date = new Date()): string {
-    return dateFormat(date, DateFormats.DATE_FORMAT_RFC1123, true);
-}
-
-export function toLogFileDateTime(date: Date = new Date(), utc: boolean = false): string {
-    return dateFormat(date, DateFormats.DATE_FORMAT_LOGFILE, utc);
-}
-
 export function fromISO8601DateTime(dateTime: string): Date {
     return new Date(dateTime);
 }
