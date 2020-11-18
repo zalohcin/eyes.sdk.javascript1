d120bf0db05f567decbe4bd09e16a2b9cbf0fd9c
Tal Hayut <tal.hayut@applitools.com>
2020-11-18 20:00:38 +0200

    fix: refactoring cypress output

--- packages/eyes-cypress/src/plugin/errorDigest.js
+++ packages/eyes-cypress/src/plugin/errorDigest.js
@@ -1,7 +1,26 @@
 'use strict';
 const chalk = require('chalk');
 
-function errorDigest({passed, failed, diffs, logger}) {
+const statuses = {
+  Passed: {
+    color: 'green',
+    symbol: '\u2713'
+  },
+  Failed: {
+    color: 'red',
+    symbol: '\u2716'
+  },
+  Empty: {
+    color: 'yellow',
+    symbol: '\u26A0'
+  },
+  Unresolved: {
+    color: 'red',
+    symbol: '\u2716'
+  }
+};
+
+function errorDigest({passed, failed, diffs, empty, logger}) {
   logger.log('errorDigest: diff errors', diffs);
   logger.log('errorDigest: test errors', failed);
 
@@ -9,6 +28,7 @@ function errorDigest({passed, failed, diffs, logger}) {
 
   return `Eyes-Cypress detected diffs or errors during execution of visual tests:
 ${indent(2)}${chalk.green(`Passed - ${passed.length} tests`)}${testResultsToString(passed, true)}
+${indent(2)}${chalk.yellow(`Empty - ${empty.length} tests`)}${testResultsToString(empty)}
 ${indent(2)}${chalk.red(`Diffs detected - ${diffs.length} tests`)}${testResultsToString(diffs)}
 ${indent(2)}${chalk.red(`Errors - ${failed.length} tests`)}${testResultsToString(
     failed,
@@ -16,7 +36,9 @@ ${indent(2)}${chalk.red(`Errors - ${failed.length} tests`)}${testResultsToString
 }
 
 function stringifyTestResults(testResults) {
-  return `${testResults.getName()} [${testResults.getHostDisplaySize()}]${
+  const hostDisplaySize = testResults.getHostDisplaySize();
+  const viewport = hostDisplaySize ? `[${hostDisplaySize}]` : '';
+  return `${testResults.getName()}${viewport}${
     testResults.error ? ` : ${testResults.error}` : ''
   }`;
 }
@@ -26,18 +48,21 @@ function stringifyError(error) {
 }
 
 function testResultsToString(testResultsArr, isGood) {
-  return testResultsArr.length
-    ? `\n${indent(3)}${testResultsArr
-        .map(
-          testResults =>
-            `${isGood ? chalk.green('\u2713') : chalk.red('\u2716')} ${chalk.reset(
-              testResults instanceof Error
-                ? stringifyError(testResults)
-                : stringifyTestResults(testResults),
-            )}`,
-        )
-        .join(`\n${indent(3)}`)}`
-    : '';
+  const resultString = testResultsArr.map(testResults => {
+    const formattedResult = getFormattedResult(testResults)
+    return `${formattedResult} ${chalk.reset(
+      testResults instanceof Error
+        ? stringifyError(testResults)
+        : stringifyTestResults(testResults),
+    )}`
+  })
+  return testResultsArr.length ? `\n${indent(3)}${resultString.join(`\n${indent(3)}`)}` : '';
+}
+
+function getFormattedResult(result) {
+  const status = result.getStatus();
+  const { color, symbol } = statuses[status];
+  return color ? `${chalk[color](symbol)}` : '';
 }
 
 function indent(count) {
