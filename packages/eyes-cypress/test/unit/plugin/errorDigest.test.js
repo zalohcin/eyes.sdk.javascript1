2e9815965ce820790001d813d07e65a90b7b1c1f
Tal Hayut <tal.hayut@applitools.com>
2020-11-30 06:17:21 +0000

    chore: using snaptdout

--- packages/eyes-cypress/test/unit/plugin/errorDigest.test.js
+++ packages/eyes-cypress/test/unit/plugin/errorDigest.test.js
@@ -4,14 +4,10 @@ const {expect} = require('chai');
 const chalk = require('chalk');
 const errorDigest = require('../../../src/plugin/errorDigest');
 const {TestResults} = require('@applitools/visual-grid-client');
-
-// cypress wraps the error digest in red and 3 space indentation, and we throw the error digest in an `Error`
-function _wrap(str) {
-  return chalk.red(`   ${new Error(str)}`);
-}
+const snap = require('snaptdout');
 
 describe('errorDigest', () => {
-  it('works', () => {
+  it('works', async () => {
     const err1 = new TestResults({
       name: 'test0',
       hostDisplaySize: {width: 4, height: 5},
@@ -55,31 +51,10 @@ describe('errorDigest', () => {
       logger: {log: () => {}},
     });
 
-    // NOTE: this is a try to validate the expected output.
-    // It was very hard to construct this expected string, so if this becomes a maintenance nightmare, I suggest not to try and preserve it.
-    // It was mainly written for debugging purposes in order to quickly craft the output without having to run Cypress.
-
-    //  this might help:
-    // https://github.com/avajs/ava/blob/master/docs/04-snapshot-testing.md
-
-    const expectedOutput = `Eyes-Cypress detected diffs or errors during execution of visual tests:
-       ${chalk.green('Passed - 1 tests')}
-         ${chalk.green('\u2713')} ${chalk.reset('test3 [1x2]')}
-       ${chalk.yellow('Diffs detected - 2 tests')}
-         ${chalk.yellow('\u26A0')} ${chalk.reset('test1 [100x200]')}
-         ${chalk.yellow('\u26A0')} ${chalk.reset('test2 [300x400]')}
-       ${chalk.red('Errors - 3 tests')}
-         ${chalk.red('\u2716')} ${chalk.reset('test0 [4x5] : Error: bla')}
-         ${chalk.red('\u2716')} ${chalk.reset('test0 [6x7] : Error: bloo')}
-         ${chalk.red('\u2716')} ${chalk.reset('[Eyes test not started] : Error: kuku')}
-
-       See details at: url1`;
-
-    // console.log(_wrap(output)); // debugging
-    expect(output).to.deep.equal(expectedOutput);
+    await snap(output, 'works')
   });
 
-  it('should only print existing results', () => {
+  it('should only print existing results', async () => {
     const emptyResult = new TestResults();
     emptyResult.isEmpty = true;
     const passed = [
@@ -99,14 +74,10 @@ describe('errorDigest', () => {
       logger: {log: () => {}},
     });
 
-    const expectedOutput = `Eyes-Cypress detected diffs or errors during execution of visual tests:
-       ${chalk.green('Passed - 1 tests')}
-         ${chalk.green('\u2713')} ${chalk.reset('test3 [1x2]')}`;
-
-    expect(output).to.deep.equal(expectedOutput);
+    await snap(output, 'no empty')
   });
 
-  it('should handle error results', () => {
+  it('should handle error results', async () => {
     const failure = new Error('i failed you');
     const passed = [];
     const failed = [failure];
@@ -118,10 +89,6 @@ describe('errorDigest', () => {
       logger: {log: () => {}},
     });
 
-    const expectedOutput = `Eyes-Cypress detected diffs or errors during execution of visual tests:
-       ${chalk.red('Errors - 1 tests')}
-         ${chalk.red('\u2716')} ${chalk.reset('[Eyes test not started] : Error: i failed you')}`;
-
-    expect(output).to.deep.equal(expectedOutput);
+    await snap(output, 'error results')
   });
 });
