d16d5b0c39a52b8db5a744fbb9f9e69d0de4afd5
Tal Hayut <tal.hayut@applitools.com>
2020-12-09 08:23:57 +0200

    chore: use yargs the way god intended

--- packages/sdk-release-kit/src/cli/index.js
+++ packages/sdk-release-kit/src/cli/index.js
@@ -1,28 +1,5 @@
 #!/usr/bin/env node
 
-const args = require('yargs')
-  .command(
-    ['preversion', 'release-pre-check', 'pre-version'],
-    'Run all verification checks pre-release',
-  )
-  .command(['version'], 'Supportive steps to version a package')
-  .command(['postversion, post-version'], 'Supportive steps to after a package has been versioned')
-  .command(['lint', 'l'], 'Static code analysis ftw')
-  .command(['verify-changelog', 'vch'], 'Verify changelog has unreleased entries')
-  .command(
-    ['verify-commits', 'vco'],
-    'Verify no unreleased changes for internal dependencies exist',
-  )
-  .command(['verify-versions', 'vv'], 'Verify consistent versions in relevant packages')
-  .command(['verify-installed-versions', 'viv'], 'Verify correct dependencies are installable')
-  .command(['ls-dry-run', 'ls'], 'Display dependencies from a verify-installed-versions run')
-  .command(['update-changelog', 'uc'], 'Create release entry in the changelog')
-  .command(
-    ['send-release-notification', 'hello-world'],
-    'Send a notification that a has been released',
-  )
-  .demandCommand()
-  .help().argv
 const chalk = require('chalk')
 const cwd = process.cwd()
 const path = require('path')
@@ -36,106 +13,127 @@ const {verifyCommits, verifyInstalledVersions, verifyVersions} = require('../ver
 const {gitAdd, gitCommit, gitPushWithTags, isChanged} = require('../git')
 const {yarnInstall, yarnUpgrade, verifyUnfixedDeps} = require('../yarn')
 
-const command = args._[0]
-
-;(async () => {
-  try {
-    switch (command) {
-      case 'lint':
-      case 'l':
-        return await lint(cwd)
-      case 'ls-dry-run':
-      case 'ls':
-        return lsDryRun()
-      case 'postversion':
-      case 'post-version':
-        await gitPushWithTags()
-        if (!args['skip-release-notification']) {
-          await sendReleaseNotification(cwd, args.recipient)
-        }
-        return
-      case 'preversion':
-      case 'pre-version':
-      case 'release-pre-check':
-        if (!args.skipDeps) {
-          console.log('[bongo preversion] yarn install')
-          await yarnInstall()
-        }
-        console.log('[bongo preversion] lint')
-        await lint(cwd)
-        console.log('[bongo preversion] verify changelog')
-        await verifyChangelog(cwd)
-        console.log('[bongo preversion] verify unfixed dependencies')
-        verifyUnfixedDeps(cwd)
-        if (!args.skipVerifyVersions) {
-          console.log('[bongo preversion] verify commits')
-          await verifyCommits({pkgPath: cwd}).catch(err => console.log(err.message))
-        }
-        try {
-          console.log('[bongo preversion] verify versions')
-          verifyVersions({pkgPath: cwd})
-        } catch (err) {
-          console.log(chalk.yellow(err.message))
-        }
-        if (!args.skipVerifyInstalledVersions) {
-          console.log('[bongo preversion] verify installed versions')
-          createDotFolder(cwd)
-          await packInstall(cwd)
-          await verifyInstalledVersions({
-            pkgPath: cwd,
-            installedDirectory: path.join('.bongo', 'dry-run'),
-          })
-        }
-        await commitFiles()
-        console.log('[bongo preversion] done!')
-        return
-      case 'send-release-notification':
-      case 'hello-world':
-        return await sendReleaseNotification(cwd, args.recipient)
-      case 'update-changelog':
-      case 'uc':
-        return writeReleaseEntryToChangelog(cwd)
-      case 'verify-changelog':
-      case 'vch':
-        return await verifyChangelog(cwd)
-      case 'verify-commits':
-      case 'vco':
-        return await verifyCommits({pkgPath: cwd})
-      case 'verify-installed-versions':
-      case 'viv':
+const args = require('yargs')
+  .command(
+    ['preversion', 'release-pre-check', 'pre-version'],
+    'Run all verification checks pre-release',
+    {},
+    async args => {
+      if (!args.skipDeps) {
+        console.log('[bongo preversion] yarn install')
+        await yarnInstall()
+      }
+      console.log('[bongo preversion] lint')
+      await lint(cwd)
+      console.log('[bongo preversion] verify changelog')
+      verifyChangelog(cwd)
+      console.log('[bongo preversion] verify unfixed dependencies')
+      verifyUnfixedDeps(cwd)
+      if (!args.skipVerifyVersions) {
+        console.log('[bongo preversion] verify commits')
+        await verifyCommits({pkgPath: cwd}).catch(err => console.log(err.message))
+      }
+      try {
+        console.log('[bongo preversion] verify versions')
+        verifyVersions({pkgPath: cwd})
+      } catch (err) {
+        console.log(chalk.yellow(err.message))
+      }
+      if (!args.skipVerifyInstalledVersions) {
+        console.log('[bongo preversion] verify installed versions')
         createDotFolder(cwd)
         await packInstall(cwd)
-        return await verifyInstalledVersions({
+        await verifyInstalledVersions({
           pkgPath: cwd,
           installedDirectory: path.join('.bongo', 'dry-run'),
         })
-      case 'verify-versions':
-      case 'vv':
-        try {
-          verifyVersions({pkgPath: cwd})
-        } catch (err) {
-          console.log(chalk.yellow(err.message))
-        }
-        return
-      case 'version':
-        writeReleaseEntryToChangelog(cwd)
-        return await gitAdd('CHANGELOG.md')
-      case 'deps':
-        await deps()
-        return await commitFiles()
-      default:
-        throw new Error('Invalid option provided')
-    }
-  } catch (error) {
+      }
+      await commitFiles()
+      console.log('[bongo preversion] done!')
+      return
+    },
+  )
+  .command(['version'], 'Supportive steps to version a package', {}, async () => {
+    writeReleaseEntryToChangelog(cwd)
+    await gitAdd('CHANGELOG.md')
+  })
+  .command(
+    ['postversion, post-version'],
+    'Supportive steps to after a package has been versioned',
+    {},
+    async args => {
+      await gitPushWithTags()
+      if (!args['skip-release-notification']) {
+        await sendReleaseNotification(cwd, args.recipient)
+      }
+    },
+  )
+  .command(['lint', 'l'], 'Static code analysis ftw', {}, async () => await lint(cwd))
+  .command(['verify-changelog', 'vch'], 'Verify changelog has unreleased entries', {}, () =>
+    verifyChangelog(cwd),
+  )
+  .command(
+    ['verify-commits', 'vco'],
+    'Verify no unreleased changes for internal dependencies exist',
+    {},
+    async () => await verifyCommits({pkgPath: cwd}),
+  )
+  .command(
+    ['verify-versions', 'vv'],
+    'Verify consistent versions in relevant packages',
+    {},
+    async () => {
+      try {
+        verifyVersions({pkgPath: cwd})
+      } catch (err) {
+        console.log(chalk.yellow(err.message))
+      }
+    },
+  )
+  .command(
+    ['verify-installed-versions', 'viv'],
+    'Verify correct dependencies are installable',
+    {},
+    async () => {
+      createDotFolder(cwd)
+      await packInstall(cwd)
+      await verifyInstalledVersions({
+        pkgPath: cwd,
+        installedDirectory: path.join('.bongo', 'dry-run'),
+      })
+    },
+  )
+  .command(
+    ['ls-dry-run', 'ls'],
+    'Display dependencies from a verify-installed-versions run',
+    {},
+    () => lsDryRun(),
+  )
+  .command(['update-changelog', 'uc'], 'Create release entry in the changelog', {}, () =>
+    writeReleaseEntryToChangelog(cwd),
+  )
+  .command(
+    ['send-release-notification', 'hello-world'],
+    'Send a notification that a has been released',
+    {},
+    async () => await sendReleaseNotification(cwd, args.recipient),
+  )
+  .command(['deps', 'd'], {}, async () => {
+    await deps()
+    await commitFiles()
+  })
+  .demandCommand()
+  .fail((msg, error, args) => {
+    const command = args._[0]
     if (args.verbose) {
-      console.log(error)
+      console.log(msg)
     } else {
       console.log(chalk.red(error.message))
       console.log(`run "npx bongo ${command} --verbose" to see stack trace`)
     }
     process.exit(1)
-  }
-})()
+  })
+  .help().argv
 
 async function deps() {
   verifyUnfixedDeps(cwd)
