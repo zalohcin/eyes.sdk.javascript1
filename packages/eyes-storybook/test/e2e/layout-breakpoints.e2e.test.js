662215a8a1d8c1666f43474b516a5910bbd4c2da
tourdedave <dave.haeffner@applitools.com>
2020-12-15 19:33:54 -0500

    sb: add initial e2e test for layout breakpoints - still wip

---
+++ packages/eyes-storybook/test/e2e/layout-breakpoints.e2e.test.js
@@ -0,0 +1,27 @@
+const {describe, it, _before, _after} = require('mocha');
+const {expect} = require('chai');
+const path = require('path');
+const {delay: _psetTimeout, presult} = require('@applitools/functional-commons');
+const {sh} = require('@applitools/sdk-shared/src/process-commons');
+
+describe('eyes-storybook', () => {
+  it('renders with layout breakpoints in config', async () => {
+    const [err, result] = await presult(
+      sh(
+        `node ${path.resolve(__dirname, '../../bin/eyes-storybook')} -f ${path.resolve(
+          __dirname,
+          'happy-config/layout-breakpoints.config.js',
+        )}`,
+        {
+          spawnOptions: {stdio: 'pipe'},
+        },
+      ),
+    );
+    const stdout = err ? err.stdout : result.stdout;
+    //const stderr = err ? err.stderr : result.stderr;
+
+    expect(stdout.replace(/\[Chrome \d+.\d+\]/g, '[Chrome]')).to.include(
+      'Single story [Chrome] [640x480] - Passed',
+    );
+  });
+});
