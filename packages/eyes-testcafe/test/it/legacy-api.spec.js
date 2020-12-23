0db3453ddb6682a0d5861f24107b2556d2935a48
tourdedave <dave.haeffner@applitools.com>
2020-12-23 10:45:42 -0500

    tc: filled in placeholder tests, skipped a couple which still need finalizing

--- packages/eyes-testcafe/test/it/legacy-api.spec.js
+++ packages/eyes-testcafe/test/it/legacy-api.spec.js
@@ -27,23 +27,126 @@ test('eyes.open with config params', async driver => {
   const config = eyes.getConfiguration()
   assert.deepStrictEqual(config.getBrowsersInfo(), browser)
 })
-test('eyes.checkWindow', async t => {
+test.skip('eyes.checkWindow tag', async t => {
   await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
-  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow'})
-  await eyes.checkWindow()
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow tag'})
+  await eyes.checkWindow('tag')
+  await eyes.checkWindow({tag: 'tag'})
+  await eyes.close(false)
+  // assert tags in jobs
+})
+test('eyes.checkWindow fully', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow fully'})
+  await eyes.checkWindow({target: 'window', fully: true})
   await eyes.close(true)
 })
-test.skip('eyes.checkWindow tag', async _t => {})
-test.skip('eyes.checkWindow target', async _t => {})
-test.skip('eyes.checkWindow fully', async _t => {})
-test.skip('eyes.checkWindow selector', async _t => {})
-test.skip('eyes.checkWindow region', async _t => {})
-test.skip('eyes.checkWindow ignore', async _t => {})
-test.skip('eyes.checkWindow floating', async _t => {})
-test.skip('eyes.checkWindow layout', async _t => {})
-test.skip('eyes.checkWindow strict', async _t => {})
-test.skip('eyes.checkWindow content', async _t => {})
-test.skip('eyes.checkWindow accessibility', async _t => {})
-test.skip('eyes.checkWindow scriptHooks', async _t => {})
-test.skip('eyes.checkWindow sendDom', async _t => {})
+test('eyes.checkWindow selector', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow selector'})
+  await eyes.checkWindow({target: 'region', selector: '#overflowing-div'})
+  await eyes.close(true)
+})
+test('eyes.checkWindow region', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow region'})
+  await eyes.checkWindow({target: 'region', region: {top: 100, left: 0, width: 1000, height: 200}})
+  await eyes.close(true)
+})
+test('eyes.checkWindow ignore', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow ignore'})
+  await eyes.checkWindow({
+    ignore: [{selector: '#overflowing-div'}, {top: 100, left: 0, width: 1000, height: 200}],
+  })
+  await eyes.close(true)
+})
+test('eyes.checkWindow floating', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow floating'})
+  await eyes.checkWindow({
+    floating: [
+      {
+        top: 100,
+        left: 0,
+        width: 1000,
+        height: 100,
+        maxUpOffset: 20,
+        maxDownOffset: 20,
+        maxLeftOffset: 20,
+        maxRightOffset: 20,
+      },
+      {
+        selector: '#overflowing-div',
+        maxUpOffset: 20,
+        maxDownOffset: 20,
+        maxLeftOffset: 20,
+        maxRightOffset: 20,
+      },
+    ],
+  })
+  await eyes.close(true)
+})
+test('eyes.checkWindow layout', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow layout'})
+  await eyes.checkWindow({
+    layout: [{top: 100, left: 0, width: 1000, height: 100}, {selector: '#overflowing-div'}],
+  })
+  await eyes.close(true)
+})
+test('eyes.checkWindow strict', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow strict'})
+  await eyes.checkWindow({
+    strict: [{top: 100, left: 0, width: 1000, height: 100}, {selector: '#overflowing-div'}],
+  })
+  await eyes.close(true)
+})
+test('eyes.checkWindow content', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow content'})
+  await eyes.checkWindow({
+    content: [{top: 100, left: 0, width: 1000, height: 100}, {selector: '#overflowing-div'}],
+  })
+  await eyes.close(true)
+})
+test('eyes.checkWindow accessibility', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({
+    t,
+    appName: 'eyes-testcafe',
+    testName: 'legacy api test: checkWindow accessibility',
+  })
+  await eyes.checkWindow({
+    accessibility: [
+      {accessibilityType: 'RegularText', selector: '#overflowing-div'},
+      {accessibilityType: 'BoldText', top: 100, left: 0, width: 1000, height: 100},
+    ],
+  })
+  await eyes.close(true)
+})
+test('eyes.checkWindow scriptHooks', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({
+    t,
+    appName: 'eyes-testcafe',
+    testName: 'legacy api test: checkWindow scriptHooks',
+  })
+  await eyes.checkWindow({
+    scriptHooks: {
+      beforeCaptureScreenshot: "document.body.style.backgroundColor = 'gold'",
+    },
+  })
+  await eyes.close(true)
+})
+test.skip('eyes.checkWindow sendDom', async t => {
+  await t.navigateTo('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+  await eyes.open({t, appName: 'eyes-testcafe', testName: 'legacy api test: checkWindow sendDom'})
+  await eyes.checkWindow({
+    sendDom: false,
+  })
+  await eyes.close(false)
+  // assert dom not sent
+})
 test.skip('eyes.waitForResults', async _t => {})
