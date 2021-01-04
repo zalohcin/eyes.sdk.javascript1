7de7e8dc12b6d47418964f6bb872f0b4943a8bd2
tourdedave <dave.haeffner@applitools.com>
2021-01-04 07:21:23 -0500

    tc: update test name and add batching for better description (still skipped)

--- packages/eyes-testcafe/test/custom/TestCheckProxyResourceIssue.spec.js
+++ packages/eyes-testcafe/test/custom/TestCheckProxyResourceIssue.spec.js
@@ -1,11 +1,14 @@
 // re: https://trello.com/c/HnnKL5VU/677-image-is-shown-as-blank-and-the-buttons-images-are-shown-as-squares-testcafe-hackathon-nov-2020
 const {Eyes} = require('../../src/TestCafeSDK')
 let eyes
+const {v4: uuidv4} = require('uuid')
+process.env.APPLITOOLS_BATCH_NAME = 'JS Coverage Tests - eyes-testcafe (custom)'
+process.env.APPLITOOLS_BATCH_ID = uuidv4()
 
 fixture`internal proxying of resources`.before(async () => {
   eyes = new Eyes()
 })
-test.skip('works', async t => {
+test.skip('works with hard-coded resource URLs', async t => {
   // NOTE:
   // - resource missing in the render: hero image (e.g., an image loaded as background-image w/ url attribute)
   // -- https://eyes.applitools.com/app/test-results/00000251792962534191/00000251792962534065/steps/1?accountId=UAujt6tHnEKUivQXIz7G6A~~&mode=step-editor
@@ -14,7 +17,7 @@ test.skip('works', async t => {
   // - dom-snapshot finds the resource (when running on the page) & vg-cli gets a correct render
   // -- https://eyes.applitools.com/app/batches/00000251792962348464/00000251792962348324?accountId=UAujt6tHnEKUivQXIz7G6A~~
   await t.navigateTo('https://demo.applitools.com/tlcHackathonProductDetailsMasterV1.html?id=1')
-  await eyes.open(t, 'internal proxying of resources', 'works', {
+  await eyes.open(t, 'internal proxying of resources', 'works with resource hard-coded URLs', {
     width: 1200,
     height: 800,
   })
