5d0cb3b41be66fbb2d690045a976f1ae4691a568
Amit Zur <amit.zur@applitools.com>
2021-01-07 06:53:53 +0200

    Revert "use coverage tests from dom-image-location"
    This reverts commit a030a0e55e08cb2366aebe9d6fe9b12b33c527e5.

--- packages/sdk-shared/coverage-tests/generic/index.js
+++ packages/sdk-shared/coverage-tests/generic/index.js
@@ -3,11 +3,11 @@ const prettier = require('prettier')
 module.exports = {
   output: './test/generic',
   ext: '.spec.js',
-  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/dom-image-location/js/emitter.js',
+  emitter: 'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/emitter.js',
   overrides:
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/dom-image-location/js/overrides.js',
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/overrides.js',
   template:
-    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/dom-image-location/js/template.hbs',
+    'https://raw.githubusercontent.com/applitools/sdk.coverage.tests/master/js/template.hbs',
   formatter: code => {
     return prettier.format(code, {
       parser: 'babel',
