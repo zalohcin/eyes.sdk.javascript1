24f2fa12d1c61335342b578d2c8832c4f8a5649a
tourdedave <dave.haeffner@applitools.com>
2021-01-08 10:24:00 -0500

    tc: update test - still wip

--- packages/eyes-testcafe/test/custom/TestPerformanceIssue.spec.js
+++ packages/eyes-testcafe/test/custom/TestPerformanceIssue.spec.js
@@ -1,29 +1,30 @@
 const cwd = process.cwd()
 const path = require('path')
 const {Eyes} = require('../../index')
-let _eyes
+let eyes
 const pages = [
   {name: 'mopub', path: 'https://www.mopub.com/content/mopub-aem-twitter/en'},
   {name: 'careers', path: 'https://careers.twitter.com/content/careers-twitter/en.html'},
   {name: 'business', path: 'https://business.twitter.com/content/business-twitter/en.html'},
 ]
-fixture`benchmark tc`
-//test.skip('page 1', async t => {
-//  await t.navigateTo(pages[0].path)
-//  await eyes.open({t, appName: 'eyes-testcafe benchmark', testName: 'page 1'})
-//  await eyes.checkWindow('page 1')
-//  await eyes.close(false)
-//})
-test.skip('page 2', async t => {
-  const eyes = new Eyes({configPath: path.join(cwd, 'test', 'custom', 'applitools.config.js')})
-  await t.navigateTo(pages[1].path)
-  await eyes.open({t, appName: 'eyes-testcafe benchmark', testName: 'page 2', showLogs: true})
-  await eyes.checkWindow('page 2')
+fixture`benchmark`.beforeEach(() => {
+  eyes = new Eyes({configPath: path.join(cwd, 'test', 'custom', 'applitools.config.js')})
+})
+test.skip(pages[0].path, async t => {
+  await t.navigateTo(pages[0].path)
+  await eyes.open({t, appName: 'eyes-testcafe benchmark', testName: pages[0].name, showLogs: true})
+  await eyes.checkWindow(pages[0].path)
   await eyes.close(false)
 })
-//test.skip('page 3', async t => {
-//  await t.navigateTo(pages[2].path)
-//  await eyes.open({t, appName: 'eyes-testcafe benchmark', testName: 'page 3'})
-//  await eyes.checkWindow('page 3')
-//  await eyes.close(false)
-//})
+test.skip(pages[1].path, async t => {
+  await t.navigateTo(pages[1].path)
+  await eyes.open({t, appName: 'eyes-testcafe benchmark', testName: pages[1].name})
+  await eyes.checkWindow(pages[1].path)
+  await eyes.close(false)
+})
+test.skip(pages[2].path, async t => {
+  await t.navigateTo(pages[2].path)
+  await eyes.open({t, appName: 'eyes-testcafe benchmark', testName: pages[2].name})
+  await eyes.checkWindow(pages[2].path)
+  await eyes.close(false)
+})
