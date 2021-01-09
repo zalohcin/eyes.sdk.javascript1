0c1930d2296f9fa4bbb018243718e8586f759706
tourdedave <dave.haeffner@applitools.com>
2021-01-08 22:00:47 -0500

    tc: add takeDomSnapshot env var for setting byte chunk length to index-short

--- packages/eyes-testcafe/index-short.js
+++ packages/eyes-testcafe/index-short.js
@@ -1,4 +1,6 @@
 const {EyesFactory} = require('./src/sdk')
 
+process.env.APPLITOOLS_SCRIPT_RESULT_MAX_BYTE_LENGTH = 4718592 // 4.5 MB
+
 module.exports = EyesFactory
 module.exports.default = EyesFactory
