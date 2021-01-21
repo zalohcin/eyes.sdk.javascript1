139996b9ec52295eafd6265a8bf128c2eeb1e620
tourdedave <dave.haeffner@applitools.com>
2021-01-21 07:35:43 -0500

    rb: fix test hooks, implement executeScript, and add skipped tests for rest of the spec to implement

--- rb/eyes-selenium/lib/spec-driver.rb
+++ rb/eyes-selenium/lib/spec-driver.rb
@@ -1,3 +1,7 @@
 def isEqualElements(driver, element1, element2)
   element1.hash == element2.hash
 end
+
+def executeScript(driver, script, *args)
+  driver.execute_script(script, *args)
+end
--- rb/eyes-selenium/spec/spec-driver_spec.rb
+++ rb/eyes-selenium/spec/spec-driver_spec.rb
@@ -3,11 +3,14 @@ require('webdrivers/chromedriver')
 require_relative('../lib/spec-driver')
 
 describe 'spec-driver' do
-  before do
+  before(:all) do
     options = Selenium::WebDriver::Chrome::Options.new
     options.add_argument('--headless')
     @driver = Selenium::WebDriver.for :chrome, options: options
     @driver.get 'https://applitools.github.io/demo/TestPages/FramesTestPage/'
+  end
+  after(:all) do
+    @driver.quit
   end
   it('isEqualElements(element, element)') do
     el1 = @driver.find_element(:css, 'div')
@@ -17,5 +20,44 @@ describe 'spec-driver' do
     el1 = @driver.find_element(:css, 'div')
     el2 = @driver.find_element(:css, 'h1')
     expect(isEqualElements(@driver, el1, el2)).to eq(false)
+  end
+  it('executeScript') do
+    args = [0, 'string', {key: 'value'}, [0, 1, 2, 3]]
+    expected = @driver.execute_script('return arguments', *args)
+    result = executeScript(@driver, 'return arguments', *args)
+    expect(result).to eq(expected)
+  end
+  it('mainContext') do
+    skip
+  end
+  it('parentContext') do
+    skip
+  end
+  it('childContext') do
+    skip
+  end
+  it('findElement') do
+    skip
+  end
+  it('findElements') do
+    skip
+  end
+  it('getViewportSize') do
+    skip
+  end
+  it('setViewportSize') do
+    skip
+  end
+  it('getTitle') do
+    skip
+  end
+  it('getUrl') do
+    skip
+  end
+  it('getDriverInfo') do
+    skip
+  end
+  it('takeScreenshot') do
+    skip
   end
 end
