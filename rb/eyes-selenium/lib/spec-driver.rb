c8cfb8540781f140837c2b2d223b90616d8065a8
tourdedave <dave.haeffner@applitools.com>
2021-01-21 07:53:15 -0500

    rb: implement mainContext, and wrap spec-driver in a module for easier reasoning

--- rb/eyes-selenium/lib/spec-driver.rb
+++ rb/eyes-selenium/lib/spec-driver.rb
@@ -1,7 +1,15 @@
-def isEqualElements(driver, element1, element2)
-  element1.hash == element2.hash
-end
+module SpecDriver
+  extend self
 
-def executeScript(driver, script, *args)
-  driver.execute_script(script, *args)
+  def isEqualElements(driver, element1, element2)
+    element1.hash == element2.hash
+  end
+
+  def executeScript(driver, script, *args)
+    driver.execute_script(script, *args)
+  end
+
+  def mainContext(driver)
+    driver.switch_to.default_content
+  end
 end
--- rb/eyes-selenium/spec/spec-driver_spec.rb
+++ rb/eyes-selenium/spec/spec-driver_spec.rb
@@ -1,6 +1,6 @@
 require('selenium-webdriver')
 require('webdrivers/chromedriver')
-require_relative('../lib/spec-driver')
+require_relative('../lib/spec-driver') # brings in SpecDriver
 
 describe 'spec-driver' do
   before(:all) do
@@ -14,21 +14,32 @@ describe 'spec-driver' do
   end
   it('isEqualElements(element, element)') do
     el1 = @driver.find_element(:css, 'div')
-    expect(isEqualElements(@driver, el1, el1)).to eq(true)
+    expect(SpecDriver.isEqualElements(@driver, el1, el1)).to eq(true)
   end
   it('isEqualElements(element1, element2)') do
     el1 = @driver.find_element(:css, 'div')
     el2 = @driver.find_element(:css, 'h1')
-    expect(isEqualElements(@driver, el1, el2)).to eq(false)
+    expect(SpecDriver.isEqualElements(@driver, el1, el2)).to eq(false)
   end
   it('executeScript') do
     args = [0, 'string', {key: 'value'}, [0, 1, 2, 3]]
     expected = @driver.execute_script('return arguments', *args)
-    result = executeScript(@driver, 'return arguments', *args)
+    result = SpecDriver.executeScript(@driver, 'return arguments', *args)
     expect(result).to eq(expected)
   end
   it('mainContext') do
-    skip
+    begin
+      mainDocument = @driver.find_element(:css, 'html')
+      @driver.switch_to.frame(@driver.find_element(:css, '[name="frame1"]'))
+      @driver.switch_to.frame(@driver.find_element(:css, '[name="frame1-1"]'))
+      frameDocument = @driver.find_element(:css, 'html')
+      expect(SpecDriver.isEqualElements(@driver, mainDocument, frameDocument)).to eq(false)
+      SpecDriver.mainContext(@driver)
+      resultDocument = @driver.find_element(:css, 'html')
+      expect(SpecDriver.isEqualElements(@driver, resultDocument, mainDocument)).to eq(true)
+    ensure
+      @driver.switch_to.default_content
+    end
   end
   it('parentContext') do
     skip
