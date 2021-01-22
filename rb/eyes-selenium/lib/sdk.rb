0ac32f5c13f1282ac2da8bbb9474a6b27dcfc2cc
tourdedave <dave.haeffner@applitools.com>
2021-01-22 14:53:08 -0500

    rb: implement more of the socket wrapper, wire up socket.command in sdk.rb, and added TODO comments

--- rb/eyes-selenium/lib/sdk.rb
+++ rb/eyes-selenium/lib/sdk.rb
@@ -2,8 +2,58 @@ require 'eventmachine'
 require_relative 'socket'
 require_relative 'spec-driver'
 
+# TODO:
+# - implement remaining functions in socket wrapper
+# - add ref/unref to commands
+# - implement openEyes w/ check, close, abort
+# - implement takeScreenshot
+# - e2e test (classic and VG)
+# - spawn server in unref'd child process
+# - look into `unref` equivalent for socket library (if necessary)
+# - add queueing to socket wrapper
+# - add colorized logging to be consistent w/ JS POC
+# - test concurrency
 EM.run do
   socket = Applitools::Socket.new
   socket.connect('ws://localhost:2107/eyes')
   socket.emit('Session.init', {:commands => SpecDriver.commands})
+  socket.command('Driver.isEqualElements', ->(params) {
+    SpecDriver.isEqualElements(params[:context], params[:element1], params[:element2])
+  })
+  socket.command('Driver.executeScript', ->(params) {
+    SpecDriver.executeScript(params[:context], params[:script], *params[:args])
+  })
+  socket.command('Driver.mainContext', ->(params) {
+   SpecDriver.mainContext(params[:context])
+  })
+  socket.command('Driver.parentContext', ->(params) {
+    SpecDriver.parentContext(params[:context])
+  })
+  socket.command('Driver.childContext', ->(params) {
+    SpecDriver.mainContext(params[:context], params[:element])
+  })
+  socket.command('Driver.findElement', ->(params) {
+    SpecDriver.findElement(params[:context], params[:selector])
+  })
+  socket.command('Driver.findElements', ->(params) {
+    SpecDriver.findElements(params[:context], params[:selector])
+  })
+  socket.command('Driver.getViewportSize', ->(params) {
+    SpecDriver.getViewportSize(params[:driver])
+  })
+  socket.command('Driver.setViewportSize', ->(params) {
+    SpecDriver.setViewportSize(params[:driver], params[:size])
+  })
+  socket.command('Driver.getTitle', ->(params) {
+    SpecDriver.getTitle(params[:driver])
+  })
+  socket.command('Driver.getUrl', ->(params) {
+    SpecDriver.getUrl(params[:driver])
+  })
+  socket.command('Driver.getDriverInfo', ->(params) {
+    SpecDriver.getDriverInfo(params[:driver])
+  })
+  #socket.command('Driver.takeScreenshot', ->(params) {
+  #  SpecDriver.takeScreenshot(params[:driver])
+  #})
 end
--- rb/eyes-selenium/lib/socket.rb
+++ rb/eyes-selenium/lib/socket.rb
@@ -3,6 +3,10 @@ require 'json'
 
 module Applitools
   class Socket
+    def initialize
+      @listeners = {}
+    end
+
     def connect(uri)
       @socket = ::Faye::WebSocket::Client.new(uri)
     end
@@ -11,11 +15,40 @@ module Applitools
       @socket.send(serialize(message, payload))
     end
 
+    def command(name, fn)
+      on(name, ->(payload, key) {
+        begin
+          puts "[COMMAND] #{name}, #{key}, #{JSON.generate(payload, {indent: 2})}"
+          result = fn(payload).call
+          emit({name: name, key: key}, result)
+        rescue => error
+          emit({name: name, key: key}, error.message)
+        end
+      })
+    end
+
     private
 
       def serialize(type, payload)
-        message = {:name => type, :payload => payload}
+        message = type.is_a?(String) ? 
+          {:name => type, :payload => payload} : {:name => type[:name], key: type[:key], :payload => payload}
         JSON.generate(message)
       end
+
+      def on(type, fn)
+        name = type.is_a?(String) ? type : "#{type[:name]}/#{type[:key]}"
+        fns = listeners[name]
+        if (!fns)
+          fns = []
+          listeners[:name] = fns
+        end
+        fns.push(fn)
+        # NOTE:
+        # There's no return here like in the JS POC. The closest thing to 
+        # returning a function would be something like this: `->() { off(name, fn)`.
+        # It would need to receive `.call` to run though. It's unclear to me how 
+        # this return function is used, so I've omitted it for now.
+      end
+
   end
 end
