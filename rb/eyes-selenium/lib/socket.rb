6ec47fb4c47e788ecbca40a3083c31072e11421e
tourdedave <dave.haeffner@applitools.com>
2021-01-25 10:12:43 -0500

    rb: add lifecycle listeners on connect and test lookup/execution of them in isolation

--- rb/eyes-selenium/lib/socket.rb
+++ rb/eyes-selenium/lib/socket.rb
@@ -11,6 +11,17 @@ module Applitools
 
     def connect(uri, ws)
       @socket = ws ? ws : ::Faye::WebSocket::Client.new(uri)
+
+      ws.on :message do |event|
+        message = JSON.parse(event.data).values
+        params = [message[:payload], message[:key]]
+        find_and_execute_listeners_by_name(message[:name], params)
+        find_and_execute_listeners_by_name("#{name}/#{key}", params) if (key)
+      end
+
+      ws.on :close do |event|
+        find_and_execute_listeners_by_name('close')
+      end
     end
 
     def emit(message, payload)
@@ -31,6 +42,12 @@ module Applitools
 
     private
 
+      def find_and_execute_listeners_by_name(name, params = [])
+        fns = listeners[name.to_sym]
+        return if (!fns)
+        fns.each {|fn| fn.call(*params)}
+      end
+
       def serialize(type, payload)
         message = type.is_a?(String) ? 
           {:name => type, :payload => payload} : {:name => type[:name], key: type[:key], :payload => payload}
@@ -39,15 +56,17 @@ module Applitools
 
       def on(type, fn)
         name = type.is_a?(String) ? type : "#{type[:name]}/#{type[:key]}"
-        fns = @listeners[name: name]
+        fns = listeners[name.to_sym]
         if (!fns)
           fns = []
-          @listeners[name: name] = fns
+          listeners[name.to_sym] = fns
         end
         fns.push(fn)
         # NOTE:
-        # There's no return here like in the JS POC. The closest thing to 
-        # returning a function would be something like this: `->() { off(name, fn)`.
+        # There's no return here like in the JS POC
+        # e.g., https://github.com/applitools/eyes.sdk.javascript1/blob/poc/universal-ruby-sdk/packages/eyes-universal/src/socket.js#L85
+        # The closest thing to returning a function would be something like this:
+        # `->() { off(name, fn)`.
         # It would need to receive `.call` to run though. It's unclear to me how 
         # this return function is used, so I've omitted it for now.
       end
--- rb/eyes-selenium/spec/socket_spec.rb
+++ rb/eyes-selenium/spec/socket_spec.rb
@@ -3,7 +3,7 @@ require 'json'
 
 describe 'socket' do
   before(:each) do
-    @ws = double()
+    @ws = double().as_null_object
     @socket = ::Applitools::Socket.new
     @socket.connect('http://blah', @ws)
   end
@@ -22,8 +22,8 @@ describe 'socket' do
     }
     @socket.command(name, payload)
     expect(@socket.listeners.length).to eq(1)
-    expect(@socket.listeners.first.first[:name]).to eq(name)
-    expect(@socket.listeners.first[1].first).to_not eq(payload) # fn that wraps execution and emits the result
+    expect(@socket.listeners.first.first[name]).to eq(name)
+    expect(@socket.listeners.first[1].first).to_not eq(payload) # e.g., fn that wraps execution and emits the result
   end
 
   it 'an executed wrapped command emits the correct payload' do
@@ -37,15 +37,27 @@ describe 'socket' do
     listener.call({blah: 'blah'}, 'key')
   end
 
+  it 'registers lifecycle listeners on connection' do
+    ws = double().as_null_object
+    expect(ws).to receive(:on).with(:message).with(:close)
+    socket = ::Applitools::Socket.new
+    socket.connect('http://blah', ws)
+  end
+
+  it 'lifecycle listeners can find and execute listeners by name' do
+    name = 'Driver.someCommand'
+    @socket.command(name, ->(params) {
+      'blah'
+    })
+    expect(@ws).to receive(:send).with(JSON.generate({name: name, key: 'key', payload: 'blah'}))
+    @socket.send(:find_and_execute_listeners_by_name, name, [{payload: 'blah'}, 'key'])
+  end
+
   it 'queues commands when no socket present' do
     skip
   end
 
   it 'processes queue on connection' do
-    skip
-  end
-
-  it 'registers lifecycle listeners on connection' do
     skip
   end
 end
