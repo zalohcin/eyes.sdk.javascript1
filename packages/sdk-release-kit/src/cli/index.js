9ea98622e1dafcc0f4d7200ec5b5e59fc4a17c45
Amit Zur <amit.zur@applitools.com>
2021-01-27 07:41:06 +0200

    add verbose logging to bongo deps

--- packages/sdk-release-kit/src/cli/index.js
+++ packages/sdk-release-kit/src/cli/index.js
@@ -10,7 +10,7 @@ const {lint} = require('../lint')
 const sendReleaseNotification = require('../send-report')
 const {createDotFolder} = require('../setup')
 const {verifyCommits, verifyInstalledVersions, verifyVersions} = require('../versions')
-const {gitAdd, gitCommit, gitPushWithTags, isChanged} = require('../git')
+const {gitAdd, gitCommit, gitPushWithTags, isChanged, gitStatus} = require('../git')
 const {yarnInstall, yarnUpgrade, verifyUnfixedDeps} = require('../yarn')
 
 yargs
@@ -135,8 +135,11 @@ yargs
       upgradeAll: {type: 'boolean', default: false},
     },
     async args => {
+      console.log('[bongo deps] running...')
       await deps(args)
+      console.log('[bongo deps] updated deps. now committing...')
       await commitFiles(args)
+      console.log('[bongo deps] done')
     },
   )
   .demandCommand(1, 'exit')
@@ -166,18 +169,22 @@ async function deps({cwd, upgradeAll}) {
 
 async function commitFiles({cwd, skipCommit}) {
   if (!skipCommit) {
+    console.log('[bongo] commit files running...\n', (await gitStatus()).stdout)
     const files = ['package.json', 'CHANGELOG.md', 'yarn.lock']
     for (const file of files) {
       // git add fails when trying to add files that weren't changed
       if (await isChanged(file)) {
+        console.log(`[bongo] git add changed file: ${file}`)
         await gitAdd(file)
       }
     }
 
     // git commit fails when trying to commit files that weren't changed
+    console.log(`[bongo] committing changed files:\n${(await gitStatus()).stdout}`)
     if (await isChanged(...files)) {
       const pkgName = JSON.parse(fs.readFileSync(path.resolve(cwd, 'package.json'))).name
       await gitCommit(`[auto commit] ${pkgName}: upgrade deps`)
+      console.log(`[bongo] actually committed files`)
     }
   }
 }
