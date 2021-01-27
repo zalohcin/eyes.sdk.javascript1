9ef004eb7648e0a9384fd9b408f32f8f6ce677eb
tourdedave <dave.haeffner@applitools.com>
2021-01-27 08:00:02 -0500

    tc: remove fixture that is causing failures in CI

--- packages/eyes-testcafe/test/custom/fixtures/setup-collecting-errors-.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-window.capturedErrors = [];
-
-window.onerror = (message, source, lineno, colno, error) => {
-    window.capturedErrors.push({ message, source, lineno, colno, error, level: error.level, url: error.url });
-}
