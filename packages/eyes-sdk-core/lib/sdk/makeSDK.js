093a14c8e892459d78f3268039297129947bda98
tourdedave <dave.haeffner@applitools.com>
2021-02-02 09:57:52 -0500

    core: pass config to eyes instance in makeSDK

--- packages/eyes-sdk-core/lib/sdk/makeSDK.js
+++ packages/eyes-sdk-core/lib/sdk/makeSDK.js
@@ -2,6 +2,7 @@ const EyesSDK = require('./EyesSDK')
 const ClassicRunner = require('../runner/ClassicRunner')
 const VisualGridRunner = require('../runner/VisualGridRunner')
 const closeBatch = require('../close/closeBatch')
+const Configuration = require('../config/Configuration')
 
 function makeSDK({name, version, spec, VisualGridClient}) {
   const sdk = EyesSDK({name, version, spec, VisualGridClient})
@@ -11,6 +12,7 @@ function makeSDK({name, version, spec, VisualGridClient}) {
   async function openEyes(driver, config) {
     const runner = config.vg ? new VisualGridRunner(/* concurrency */) : new ClassicRunner()
     const eyes = new sdk.EyesFactory(runner)
+    eyes.setConfiguration(new Configuration(config))
     await eyes.open(driver, config.appName, config.testName, config.viewportSize)
 
     return {check, close, abort}
