68d0f8952e3bac105b352ca2307a980df3dc68e4
Tal Hayut <tal.hayut@applitools.com>
2021-02-19 23:25:42 +0200

    fix: revert terminal check

--- packages/eyes-cypress/src/plugin/pluginExport.js
+++ packages/eyes-cypress/src/plugin/pluginExport.js
@@ -11,11 +11,7 @@ function makePluginExport({startServer, eyesConfig, visualGridClient, logger}) {
       closeEyesServer = closeServer;
       const moduleExportsResult = await pluginModuleExports(...args);
       const [on, config] = args;
-      if (
-        config.isTextTerminal &&
-        config.version >= CYPRESS_SUPPORTED_VERSION &&
-        !eyesConfig.eyesLegacyHooks
-      ) {
+      if (config.version >= CYPRESS_SUPPORTED_VERSION && !eyesConfig.eyesLegacyHooks) {
         setGlobalRunHooks(on, {visualGridClient, logger});
       }
       return Object.assign({}, eyesConfig, {eyesPort}, moduleExportsResult);
