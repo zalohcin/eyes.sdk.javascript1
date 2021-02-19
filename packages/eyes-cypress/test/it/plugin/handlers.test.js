c6604df9ccd4cf9ca61b1fbc97a231508f071b2d
Tal Hayut <tal.hayut@applitools.com>
2021-02-19 23:18:18 +0200

    fix: unused before

--- packages/eyes-cypress/test/it/plugin/handlers.test.js
+++ packages/eyes-cypress/test/it/plugin/handlers.test.js
@@ -1,5 +1,5 @@
 'use strict';
-const {describe, it, before} = require('mocha');
+const {describe, it} = require('mocha');
 const {expect} = require('chai');
 const makeHandlers = require('../../../src/plugin/handlers');
 const getErrorsAndDiffs = require('../../../src/plugin/getErrorsAndDiffs');
